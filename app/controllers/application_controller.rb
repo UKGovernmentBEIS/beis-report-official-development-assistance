237878a2da1c7e087f4c3ab35283c5fcc0bc6c98
Robert Lee-Cann <leeky@leeky.org.uk>
2019-10-29 16:15:34 +0000

    Allow signed-in users to sign out
    When clicking the 'sign out' link on the user's dashboard, their session
    is cleared and they are redirected back to the home page.
    
    NB: This doesn't yet redirect the user through Auth0 in order to log out
    there as well.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   def health_check
     render json: {rails: "OK"}, status: :ok
   end
+
+  def sign_out
+    reset_session
+    redirect_to root_path
+  end
 end
--- spec/features/users_can_sign_in_spec.rb
+++ spec/features/users_can_sign_in_spec.rb
@@ -8,6 +8,7 @@ RSpec.feature "Users can sign in with Auth0" do
     click_on "Start now"
 
     expect(page).to have_content(I18n.t("page_title.dashboard"))
+    expect(page).to have_content(I18n.t("generic.link.sign_out"))
   end
 
   scenario "protected pages cannot be visited unless signed in" do
