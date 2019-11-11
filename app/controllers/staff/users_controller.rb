df937ce6e461a51401cdab2379fe252413b7b633
Tom Hipkin <tomhipkin@gmail.com>
2019-11-11 18:15:47 +0000

    Users can create other users
    * Roles aren't available at the moment to restrict this feature to a type of user so the feature test is named generically. This should be updated when role behaviour is added.

---
+++ app/controllers/staff/users_controller.rb
@@ -0,0 +1,25 @@
+class Staff::UsersController < Staff::BaseController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      flash[:notice] = I18n.t("form.user.create.success")
+      redirect_to users_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
