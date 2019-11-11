b9ea3a91648ac5a9e80c945202b81f5860c6ae71
Laura Porter <laura@dxw.com>
2019-11-11 11:27:57 +0000

    Use real IATI codelists to test OrganisationHelper
    Using a fixture here was not appropriate, as the codelists are not going to change

--- spec/helpers/organisation_helper_spec.rb
+++ spec/helpers/organisation_helper_spec.rb
@@ -3,18 +3,19 @@
 require "rails_helper"
 
 RSpec.describe OrganisationHelper, type: :helper do
-  describe "#yaml_to_options" do
-    it "formats the data in a yaml file to a nested array for use in options_for_select" do
-      yaml_snippet = YAML.safe_load(File.read("spec/fixtures/default_currency.yml"))
-      allow(helper).to receive(:load_yaml).and_return(yaml_snippet)
-
-      expect(helper.yaml_to_options("default_currency")).to eq [
-        ["UAE Dirham", "AED"],
-        ["Afghani", "AFN"],
-        ["Lek", "ALL"],
-        ["Armenian Dram", "AMD"],
-        ["Netherlands Antillian Guilder", "ANG"],
-      ]
+  describe "version 2_03" do
+    let(:version) { "2_03" }
+    describe "#yaml_to_options" do
+      it "formats the data in a yaml file to a nested array for use in options_for_select" do
+        expect(helper.yaml_to_options("default_currency")).to include(
+          ["UAE Dirham", "AED"],
+          ["Afghani", "AFN"],
+          ["Lek", "ALL"],
+          ["Armenian Dram", "AMD"],
+          ["Netherlands Antillian Guilder", "ANG"],
+          ["Kwanza", "AOA"]
+        )
+      end
     end
   end
 end
