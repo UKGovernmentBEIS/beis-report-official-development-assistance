cc503efb6047cf7a71495e88a4faf9f35718f0f4
Laura Porter <laura@dxw.com>
2019-11-12 13:17:36 +0000

    Temporarily re-add stub_authenticated_session to fix tests

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -14,4 +14,8 @@ module AuthenticationHelpers
     visit dashboard_path
     click_on "Start now"
   end
+
+  def stub_authenticated_session(name: "Alex", email: "alex@example.com")
+    page.set_rack_session(userinfo: {uid: "123456789", info: {name: name, email: email}})
+  end
 end
