04abd9a6e25d29a0b71fdf5da9814ed21fb73bf7
Laura Porter <laura@dxw.com>
2019-11-14 13:20:27 +0000

    Sign users out of Auth0 when they log out of the application
    1. Redirect users to Auth0 when they sign out
    2. Remove the Sign out feature test as it has been superceded by a request spec
    3. Set default values for Auth0 in env.test
    4. Move the sign out route to ApplicationController as it can be used anywhere

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  def sign_out
+    reset_session
+    redirect_to logout_url.to_s
+  end
+
+  private
+
+  def logout_url
+    domain = ENV["AUTH0_DOMAIN"]
+    client_id = ENV["AUTH0_CLIENT_ID"]
+    request_params = {
+      returnTo: root_url,
+      client_id: client_id,
+    }
+
+    URI::HTTPS.build(host: domain, path: "/v2/logout", query: to_query(request_params))
+  end
+
+  def to_query(hash)
+    hash.map { |k, v| "#{k}=#{CGI.escape(v)}" unless v.nil? }.reject(&:nil?).join("&")
+  end
 end
