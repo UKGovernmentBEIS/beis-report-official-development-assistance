7ab64d54873e962b9365a2af0275fc80128e2814
Tom Hipkin <tomhipkin@gmail.com>
2019-11-18 12:40:16 +0000

    Users are shown a not authorised page when they do not exist
    * Given that Auth0 and this app needs to have a local copy of each user record, we should handle the case where the common identifier does not match.
    * This could happen if either source is updated without successfully telling the other of the change. Given we are using identifiers and not names or emails, this should be a very small case.
    * Raising our own `UserNotAuthorised` allows us to interupt the chain and also to describe the error in a more meaningful way. This class can be reused by other actions in the future to prevent the use seeing content they shouldn't. It also includes a reusable 401 error page.

--- app/controllers/staff/base_controller.rb
+++ app/controllers/staff/base_controller.rb
@@ -1,12 +1,22 @@
+class UserNotAuthorised < StandardError; end
 class Staff::BaseController < ApplicationController
   include Secured
 
+  rescue_from(UserNotAuthorised) do
+    render "pages/errors/not_authorised", status: 401
+  end
+
   helper_method :current_user
   def current_user
-    @current_user ||= User.find_by!(identifier: signed_in_user_identifier) { |user|
-      user.name = session.dig(:userinfo, "info", "name")
-      user.email = session.dig(:userinfo, "info", "email")
-    }
+    @current_user ||= if session.dig(:userinfo)
+      User.find_by!(identifier: signed_in_user_identifier) do |user|
+        user.name = session.dig(:userinfo, "info", "name")
+        user.email = session.dig(:userinfo, "info", "email")
+      end
+    end
+  rescue ActiveRecord::RecordNotFound
+    repudiate!
+    raise UserNotAuthorised
   end
 
   def sign_out
@@ -16,5 +26,10 @@ class Staff::BaseController < ApplicationController
 
   def signed_in_user_identifier
     session.dig(:userinfo, "uid")
+  end
+
+  private def repudiate!
+    session.delete(:userinfo)
+    @current_user = nil
   end
 end
