0e3140b619642a0005ae785f2f25727f3158edf8
Laura Porter <laura@dxw.com>
2019-11-18 10:11:16 +0000

    Add basic fund model and tests

---
+++ spec/factories/fund.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :fund do
+    name { Faker::Company.name }
+  end
+end
---
+++ spec/models/fund_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe Fund, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe "relations" do
+    it { should belong_to(:organisation) }
+    it { should have_one(:activity) }
+  end
+end
