1a514dac967d30173f3a9c8c308e27328cc73e22
Tom Hipkin <tomhipkin@gmail.com>
2019-11-18 12:42:22 +0000

    Creating a user creates them in Auth0

--- spec/features/staff/users_can_invite_new_users_spec.rb
+++ spec/features/staff/users_can_invite_new_users_spec.rb
@@ -6,6 +6,15 @@ RSpec.feature "users can invite new users to the service" do
   end
 
   scenario "a new user can be created" do
+    new_user_name = "Foo Bar"
+    new_user_email = "email@example.com"
+    auth0_identifier = "auth0|00991122"
+
+    stub_auth0_create_user_request(
+      email: new_user_email,
+      auth0_identifier: auth0_identifier
+    )
+
     # Navigate from the landing page
     visit root_path
     click_on(I18n.t("generic.link.start_now"))
@@ -19,8 +28,8 @@ RSpec.feature "users can invite new users to the service" do
 
     # Fill out the form
     expect(page).to have_content(I18n.t("page_title.users.new"))
-    fill_in "user[name]", with: "Foo Bar"
-    fill_in "user[email]", with: "email@example.com"
+    fill_in "user[name]", with: new_user_name
+    fill_in "user[email]", with: new_user_email
 
     # Submit the form
     click_button I18n.t("form.user.submit")
