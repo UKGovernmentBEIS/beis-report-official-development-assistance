723e5adcfa56b0fdb7b1f83fbf8e90b94da82d8e
Tom Hipkin <tomhipkin@gmail.com>
2019-11-18 12:40:16 +0000

    Users are shown a not authorised page when they do not exist
    * Given that Auth0 and this app needs to have a local copy of each user record, we should handle the case where the common identifier does not match.
    * This could happen if either source is updated without successfully telling the other of the change. Given we are using identifiers and not names or emails, this should be a very small case.
    * Raising our own `UserNotAuthorised` allows us to interupt the chain and also to describe the error in a more meaningful way. This class can be reused by other actions in the future to prevent the use seeing content they shouldn't. It also includes a reusable 401 error page.

--- spec/features/staff/users_can_sign_in_spec.rb
+++ spec/features/staff/users_can_sign_in_spec.rb
@@ -22,4 +22,19 @@ RSpec.feature "Users can sign in with Auth0" do
 
     expect(page).to have_content(I18n.t("page_title.welcome"))
   end
+
+  context "when the Auth0 identifier does not match a user record" do
+    scenario "informs the user their invitation has failed and the team has been notified" do
+      user = create(:user, identifier: "a-local-identifier")
+      mock_successful_authentication(
+        uid: "an-unknown-identifier", name: user.name, email: user.email
+      )
+
+      visit dashboard_path
+      click_on I18n.t("generic.link.start_now")
+
+      expect(page).to have_content(I18n.t("page_title.errors.not_authorised"))
+      expect(page).to have_content(I18n.t("page_content.errors.not_authorised.explanation"))
+    end
+  end
 end
