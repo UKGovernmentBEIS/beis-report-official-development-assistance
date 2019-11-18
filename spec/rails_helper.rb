d56d92bd0cf30e36183fde09be62f7fffd1ecc7a
Tom Hipkin <tomhipkin@gmail.com>
2019-11-18 12:42:22 +0000

    Creating a user creates them in Auth0

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include AuthenticationHelpers
+  config.include Auth0Helpers
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
