e7af14922a393eb66575c3295b1205811de15442
Tom Hipkin <tomhipkin@gmail.com>
2019-11-19 10:10:35 +0000

    Organisation Factory creates different names

--- spec/factories/organisation.rb
+++ spec/factories/organisation.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :organisation do
-    name { "My Organisation" }
+    name { Faker::Company.name }
     organisation_type { "10" }
     default_currency { "gbp" }
     language_code { "en" }
