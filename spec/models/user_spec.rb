8ff58bae249379cddbb060f8bf6df9ecd6804de9
Tom Hipkin <tomhipkin@gmail.com>
2019-11-19 10:11:46 +0000

    Join Users and Organisations
    This will enable an association to be made between both records. Users can have many organisations, and organisations can have many users.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
   end
+
+  describe "associations" do
+    it { should have_and_belong_to_many(:organisations) }
+  end
 end
