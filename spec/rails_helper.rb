675d7e931d8a896494f1220c939cba80285086e8
Tom Hipkin <tomhipkin@gmail.com>
2019-11-19 17:03:22 +0000

    Remove duplicate OmniAuth test mode config
    This is also loaded in a support file that is loaded.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,8 +64,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  OmniAuth.config.test_mode = true
-
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
