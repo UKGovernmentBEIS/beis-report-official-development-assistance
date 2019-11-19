6f60e51a1f0bcbc90271aed8e77d0e3e4a537abd
Tom Hipkin <tomhipkin@gmail.com>
2019-11-19 12:15:34 +0000

    Refactor creating users in auth0 into own service
    * This decouples the responisbility of creating users in  1 place into 2. This should make it easier to manage and work with later if we want to send more information to auth0.

--- app/services/create_user.rb
+++ app/services/create_user.rb
@@ -1,5 +1,3 @@
-require "./lib/auth0_api"
-
 class CreateUser
   attr_accessor :user, :organisations
 
@@ -15,14 +13,7 @@ class CreateUser
       user.organisations = organisations
       user.save
       begin
-        auth0_response = Auth0Api.new.client.create_user(
-          user.name,
-          email: user.email,
-          email_verified: true,
-          password: "#{SecureRandom.urlsafe_base64}aA1!",
-          connection: "Username-Password-Authentication",
-        )
-        user.update(identifier: auth0_response.fetch("user_id"))
+        CreateUserInAuth0.new(user: user).call
       rescue Auth0::Exception => e
         result.success = false
         Rails.logger.error("Error adding user #{user.email} to Auth0 during CreateUser with #{e.message}.")
