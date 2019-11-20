bc67f2e2dec2c593bb5290b59cfdf52cd4ec4057
Laura Porter <laura@dxw.com>
2019-11-20 12:35:29 +0000

    Add a scope to get only the funds belonging to a user

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -1,5 +1,9 @@
 class Fund < ApplicationRecord
-  validates_presence_of :name
+  validates_presence_of :name, :organisation_id
   belongs_to :organisation
   has_one :activity
+
+  scope :for_user, lambda { |user|
+    where(organisation: [user.organisations])
+  }
 end
