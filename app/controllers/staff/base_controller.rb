57001bcc18c77f744c731b148671e082bb7b7377
Robert Lee-Cann <leeky@leeky.org.uk>
2019-11-25 11:16:56 +0000

    Refactor Pundit::NotAuthorizedError handler
    Handle `Pundit::NotAuthorizedError` exceptions in the same way as
    `UserNotAuthorised` ones in the `Authorisation` concern.
    
    I've also amended the default Pundit error message to be similar to the
    one used for `UserNotAuthorised` exceptions.
    
    NB: I've left the Pundit `after_action` in `Staff::BaseController` so
    that it's obvious that it's being called without too much digging.

--- app/controllers/staff/base_controller.rb
+++ app/controllers/staff/base_controller.rb
@@ -1,17 +1,8 @@
 class Staff::BaseController < ApplicationController
-  rescue_from Pundit::NotAuthorizedError, with: :pundit_not_authorized
-
   include Secured
   include Authorisation
   include Pundit
 
   # Ensure that Pundit 'authorize' and scopes are used
   after_action :verify_authorized, :verify_policy_scoped
-
-  def pundit_not_authorized(exception)
-    policy_name = exception.policy.class.to_s.underscore
-
-    flash[:error] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
-    redirect_to dashboard_path
-  end
 end
