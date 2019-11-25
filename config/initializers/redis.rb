c6e883e1b3004ea0c7df106769952bec345e9b7d
Tom Hipkin <tomhipkin@gmail.com>
2019-11-25 15:46:50 +0000

    Mock Redis when in test
    So that it does not contaminate or destroy the development database.
    
    Use namespacing to ensure that we can flush each independently if we need to, to aid debugging.

---
+++ config/initializers/redis.rb
@@ -0,0 +1,12 @@
+require "connection_pool"
+
+REDIS = if Rails.env.test?
+  Redis::Namespace.new(:test, redis: MockRedis.new)
+else
+  ConnectionPool.new(size: 5, timeout: 5) do
+    Redis::Namespace.new(
+      :roda,
+      redis: Redis.new(url: "#{ENV["REDIS_URL"]}/0")
+    )
+  end
+end
