0d787996a656b216ff8629c0daa37708e40c2b21
Laura Porter <laura@dxw.com>
2019-11-26 11:26:47 +0000

    Improve Fund feature spec to reflect that Funds are now listed on their Org's show page

--- spec/features/staff/users_can_view_funds_spec.rb
+++ spec/features/staff/users_can_view_funds_spec.rb
@@ -3,9 +3,9 @@ RSpec.feature "Users can view funds" do
     authenticate!(user: user)
   end
 
-  let(:organisation) { create(:organisation) }
-  let(:organisation_2) { create(:organisation) }
-  let(:user) { create(:user, organisations: [organisation]) }
+  let(:user_organisation) { create(:organisation) }
+  let(:other_organisation) { create(:organisation) }
+  let(:user) { create(:user, organisations: [user_organisation]) }
 
   context "when the user is not logged in" do
     it "redirects the user to the root path" do
@@ -16,23 +16,23 @@ RSpec.feature "Users can view funds" do
   end
 
   context "when there are funds belonging to the user's organisation" do
-    scenario "the user will see them on the funds index page" do
-      fund = create(:fund, organisation: organisation)
+    scenario "the user will see them on the organisation show page" do
+      fund = create(:fund, organisation: user_organisation)
+      visit organisations_path
+      click_link user_organisation.name
 
-      visit funds_path
-
-      expect(page).to have_content(I18n.t("page_title.fund.index"))
+      expect(page).to have_content(I18n.t("page_content.organisation.funds"))
       expect(page).to have_content fund.name
     end
   end
 
   context "when there are funds belonging to another organisation" do
-    scenario "the user will not see them on the funds index page" do
-      fund = create(:fund, organisation: organisation_2)
+    scenario "the user will not see them on the show page for their organisation" do
+      fund = create(:fund, organisation: other_organisation)
+      visit organisations_path
+      click_link user_organisation.name
 
-      visit funds_path
-
-      expect(page).to have_content(I18n.t("page_title.fund.index"))
+      expect(page).to have_content(I18n.t("page_content.organisation.funds"))
       expect(page).not_to have_content fund.name
     end
   end
