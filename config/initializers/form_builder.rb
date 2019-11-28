827f82c4a2ada908e6b495d3a807f3bd52c12b72
Robert Lee-Cann <leeky@leeky.org.uk>
2019-11-28 16:17:10 +0000

    Use ActiveRecord I18n scope for form labels
    By default, GOVUKDesignSystemFormBuilder uses the 'helpers.label' scope
    for finding translations. This only affects the form fields themselves,
    and not the related validation errors, which might lead to confusion -
    e.g. a field named "Full name" and a validation error "Name can't be
    blank".
    
    This change uses the default ActiveRecord model attribute keys instead,
    which maintains consistency between form labels and validation messages.

--- config/initializers/form_builder.rb
+++ config/initializers/form_builder.rb
@@ -2,3 +2,18 @@ Rails.application.config.action_view.default_form_builder = GOVUKDesignSystemFor
 
 # Don't use XHR when submitting forms
 Rails.application.config.action_view.form_with_generates_remote_forms = false
+
+# Use activerecord.attributes.model.attribute_name for label scope
+module GOVUKDesignSystemFormBuilder
+  class Base
+    private def localisation_key(context)
+      return nil unless @object_name.present? && @attribute_name.present?
+
+      if context == "label"
+        ["activerecord", "attributes", @object_name, @attribute_name].join(".")
+      else
+        ["helpers", context, @object_name, @attribute_name].join(".")
+      end
+    end
+  end
+end
