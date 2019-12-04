fd9842136f4ab9dbf475807869c171c9f82672d6
Robert Lee-Cann <leeky@leeky.org.uk>
2019-12-04 11:46:50 +0000

    Clean up activity form steps definition
    Co-authored-by: Dominic Baggott <dominic.baggott@gmail.com>

--- app/controllers/staff/activity_forms_controller.rb
+++ app/controllers/staff/activity_forms_controller.rb
@@ -1,7 +1,18 @@
 class Staff::ActivityFormsController < Staff::BaseController
   include Wicked::Wizard
 
-  steps :identifier, :purpose, :sector, :status, :dates, :country, :flow, :finance, :aid_type, :tied_status
+  steps(
+    :identifier,
+    :purpose,
+    :sector,
+    :status,
+    :dates,
+    :country,
+    :flow,
+    :finance,
+    :aid_type,
+    :tied_status,
+  )
 
   def index
     skip_policy_scope
