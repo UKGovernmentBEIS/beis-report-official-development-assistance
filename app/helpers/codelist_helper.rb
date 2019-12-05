3fba95199711ac2c8a99b1c43e39781ecdeace2b
Laura Porter <laura@dxw.com>
2019-12-05 10:02:51 +0000

    Update CodelistHelper to return an array of objects, for govuk form builder
    govuk_design_system_formbuilder requires an array for OpenStruct objects for
    select boxes, as opposed to simple_form only needing a multidimensional array.

--- app/helpers/codelist_helper.rb
+++ app/helpers/codelist_helper.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
 module CodelistHelper
+  # TODO: Remove `yaml_to_options` when all forms are migrated from simple form to the govuk form builder,
+  # The govuk form builder expects an array of objects, not an array of arrays.
   def yaml_to_options(entity, type)
-    yaml = load_yaml(entity, type)
-    data = yaml["data"]
+    data = load_yaml(entity, type)
     data.collect { |item| [item["name"], item["code"]] }.sort
   end
 
+  def yaml_to_objects(entity, type, with_empty_item = true)
+    data = load_yaml(entity, type)
+    objects = data.collect { |item| OpenStruct.new(name: item["name"], code: item["code"]) }.sort_by(&:name)
+    if with_empty_item
+      empty_item = OpenStruct.new(name: "", code: "")
+      objects.unshift(empty_item)
+    end
+    objects
+  end
+
   def load_yaml(entity, type)
-    YAML.safe_load(File.read("#{Rails.root}/vendor/data/codelists/IATI/#{IATI_VERSION}/#{entity}/#{type}.yml"))
+    yaml = YAML.safe_load(File.read("#{Rails.root}/vendor/data/codelists/IATI/#{IATI_VERSION}/#{entity}/#{type}.yml"))
+    yaml["data"]
   end
 end
