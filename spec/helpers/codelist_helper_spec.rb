227b4b33b5ace86f3b63c4fd85b2ac43c84abdf9
Laura Porter <laura@dxw.com>
2019-12-05 10:02:51 +0000

    Update CodelistHelper to return an array of objects, for govuk form builder
    govuk_design_system_formbuilder requires an array for OpenStruct objects for
    select boxes, as opposed to simple_form only needing a multidimensional array.

--- spec/helpers/codelist_helper_spec.rb
+++ spec/helpers/codelist_helper_spec.rb
@@ -22,5 +22,30 @@ RSpec.describe CodelistHelper, type: :helper do
         expect(helper.yaml_to_options("organisation", "default_currency").last).to eq(["Zloty", "PLN"])
       end
     end
+
+    describe "#yaml_to_objects" do
+      it "formats the data in a yaml file to an array of objects for use in govuk form builder" do
+        expect(helper.yaml_to_objects("organisation", "default_currency")).to include(
+          OpenStruct.new(name: "Afghani", code: "AFN"),
+          OpenStruct.new(name: "Lek", code: "ALL"),
+          OpenStruct.new(name: "Armenian Dram", code: "AMD"),
+          OpenStruct.new(name: "Netherlands Antillian Guilder", code: "ANG"),
+          OpenStruct.new(name: "Kwanza", code: "AOA")
+        )
+      end
+
+      it "adds a blank first item by default" do
+        expect(helper.yaml_to_objects("organisation", "default_currency").first).to eq(OpenStruct.new(name: "", code: ""))
+      end
+
+      it "removes the blank first item if you need it to" do
+        expect(helper.yaml_to_objects("organisation", "default_currency", false).first).to_not eq(OpenStruct.new(name: "", code: ""))
+      end
+
+      it "sorts the resulting objects by name order" do
+        expect(helper.yaml_to_objects("organisation", "default_currency", false).first).to eq(OpenStruct.new(name: "Afghani", code: "AFN"))
+        expect(helper.yaml_to_objects("organisation", "default_currency", false).last).to eq(OpenStruct.new(name: "Zloty", code: "PLN"))
+      end
+    end
   end
 end
