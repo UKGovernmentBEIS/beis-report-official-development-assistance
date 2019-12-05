48925ac50bf4492b93871ab1f62be88e99d61e7d
Laura Porter <laura@dxw.com>
2019-12-05 10:56:07 +0000

    Add validations to Transaction#new form
    * Currently all fields are required.
    * Also move the form from `simple_form` to `govuk_design_system_formbuilder`

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Transaction, type: :model do
   describe "relations" do
     it { should belong_to(:fund) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:reference) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:transaction_type) }
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:currency) }
+    it { should validate_presence_of(:value) }
+    it { should validate_presence_of(:disbursement_channel) }
+  end
 end
