1607cf29a74667dc1a5556da7a57d7a9ef89f05a
Laura Porter <laura@dxw.com>
2019-12-06 14:33:20 +0000

    Add a TransactionPresenter to downcase and internationalise Transaction attributes

---
+++ app/presenters/transaction_presenter.rb
@@ -0,0 +1,16 @@
+class TransactionPresenter < SimpleDelegator
+  def transaction_type
+    return if super.blank?
+    I18n.t("transaction.transaction_type.#{super}")
+  end
+
+  def currency
+    return if super.blank?
+    I18n.t("generic.default_currency.#{super.downcase}")
+  end
+
+  def disbursement_channel
+    return if super.blank?
+    I18n.t("transaction.disbursement_channel.#{super}")
+  end
+end
