894d3abb76e35be24b2272fe54fb521fda51ef0a
Robert Lee-Cann <leeky@leeky.org.uk>
2019-12-09 14:59:17 +0000

    Refactor activity form to use GOVUK form builder
    - Use GOVUK design system form builder and helpers
    - Move setting defaults into the Activity model
    - Move model translations to models_and_forms.en.yml

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,10 @@ class Activity < ApplicationRecord
   validates_presence_of :identifier
   validates_uniqueness_of :identifier
 
+  attribute :recipient_region, :string, default: "998"
+  attribute :flow, :string, default: "10"
+  attribute :tied_status, :string, default: "5"
+
   def default_currency
     organisation.default_currency
   end
