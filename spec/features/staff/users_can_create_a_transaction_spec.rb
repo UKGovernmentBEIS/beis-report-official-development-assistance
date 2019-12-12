1a96aec28e14c3643129d49576450420dd8d6075
Tom Hipkin <tomhipkin@gmail.com>
2019-12-12 15:04:57 +0000

    Refactor transaction form filling for reuse
    This will allow us to change the way we will out the transaction form centrally and reduce repetition where we only want to test one field, eg. value.

--- spec/features/staff/users_can_create_a_transaction_spec.rb
+++ spec/features/staff/users_can_create_a_transaction_spec.rb
@@ -25,27 +25,9 @@ RSpec.feature "Users can create a transaction" do
 
     click_on(I18n.t("page_content.transactions.button.create"))
 
-    fill_in "transaction[reference]", with: "123"
-    fill_in "transaction[description]", with: "This money will be purchasing a new school roof"
-    select "Outgoing Pledge", from: "transaction[transaction_type]"
-    fill_in "transaction[date(3i)]", with: "1"
-    fill_in "transaction[date(2i)]", with: "1"
-    fill_in "transaction[date(1i)]", with: "2020"
-    fill_in "transaction[value]", with: "1000.01"
-    select "Money is disbursed through central Ministry of Finance or Treasury", from: "transaction[disbursement_channel]"
-    select "Pound Sterling", from: "transaction[currency]"
-    click_on(I18n.t("generic.button.submit"))
+    fill_in_transaction_form
 
     expect(page).to have_content(I18n.t("form.transaction.create.success"))
-    within ".transactions" do
-      expect(page).to have_content("123")
-      expect(page).to have_content("This money will be purchasing a new school roof")
-      expect(page).to have_content(I18n.t("transaction.transaction_type.12"))
-      expect(page).to have_content("2020-01-01")
-      expect(page).to have_content("1000.01")
-      expect(page).to have_content(I18n.t("transaction.disbursement_channel.1"))
-      expect(page).to have_content(I18n.t("generic.default_currency.gbp"))
-    end
   end
 
   scenario "validations" do
@@ -76,6 +58,7 @@ RSpec.feature "Users can create a transaction" do
       click_on(fund.name)
 
       click_on(I18n.t("page_content.transactions.button.create"))
+
       fill_in "transaction[reference]", with: "123"
       fill_in "transaction[description]", with: "This money will be purchasing a new school roof"
       select "Outgoing Pledge", from: "transaction[transaction_type]"
