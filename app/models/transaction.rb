a9dfe8770f5786c494bc6bd284bbf465ee2b9b67
Tom Hipkin <tomhipkin@gmail.com>
2019-12-16 14:39:47 +0000

    Break long list of validations into vertical list
    Reducing line length to < 80

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,13 @@
 class Transaction < ApplicationRecord
   belongs_to :fund
-  validates_presence_of :reference, :description, :transaction_type, :date, :currency, :value, :disbursement_channel
   belongs_to :provider, foreign_key: :provider_id, class_name: "Organisation"
   belongs_to :receiver, foreign_key: :receiver_id, class_name: "Organisation"
+  validates_presence_of :reference,
+    :description,
+    :transaction_type,
+    :date,
+    :currency,
+    :value,
+    :disbursement_channel
   validates :value, inclusion: 1..99_999_999_999.00
 end
