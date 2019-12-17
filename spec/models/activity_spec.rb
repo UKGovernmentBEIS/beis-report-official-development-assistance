01c1f3a84ffcfde664750025656267a2ece1632b
Laura Porter <laura@dxw.com>
2019-12-17 12:50:38 +0000

    Refactor Activity defaults to be extendable for other hierarchy types
    The defaults on the Activity model are Fund-specific. This change refactors
    the setting of default attributes so when we add other hierarchy types, we can
    add other defaults.

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -8,4 +8,36 @@ RSpec.describe Activity, type: :model do
   describe "constraints" do
     it { should validate_uniqueness_of(:identifier) }
   end
+
+  describe "#set_hierarchy_defaults" do
+    let(:fund) { build_stubbed(:fund) }
+
+    context "when the hierarchy is unknown" do
+      it "should not set any defaults" do
+        activity = Activity.new(hierarchy: nil)
+        activity.set_hierarchy_defaults
+        expect(activity.changed?).to eq(false)
+      end
+    end
+
+    context "when it's a fund" do
+      it "should set the recipient_region to 998" do
+        activity = build_stubbed(:activity, hierarchy: fund)
+        activity.set_hierarchy_defaults
+        expect(activity.recipient_region).to eq("998")
+      end
+
+      it "should set the flow to 10" do
+        activity = build_stubbed(:activity, hierarchy: fund)
+        activity.set_hierarchy_defaults
+        expect(activity.flow).to eq("10")
+      end
+
+      it "should set the tied_status to 5" do
+        activity = build_stubbed(:activity, hierarchy: fund)
+        activity.set_hierarchy_defaults
+        expect(activity.tied_status).to eq("5")
+      end
+    end
+  end
 end
