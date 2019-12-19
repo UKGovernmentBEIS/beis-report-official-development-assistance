af8a07c462301af603b74ee2f3e02ab93214ba6b
Tom Hipkin <tomhipkin@gmail.com>
2019-12-19 18:14:40 +0000

    Users see a styled and informative sign in error message
    * When Oauth redirects the user to the standard /failure endpoint we now give the user a styled page that has an error message and a prompt as to what to do next.
    * We don't have a support contact yet so omit it. We will need this before going live.

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -10,6 +10,6 @@ class Auth0Controller < ApplicationController
 
   def failure
     # show a failure page or redirect to an error page
-    @error_msg = request.params["message"]
+    @error_message = request.params["message"]
   end
 end
