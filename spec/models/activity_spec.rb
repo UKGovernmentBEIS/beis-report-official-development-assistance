ba9cbb84c2ef88917fe8dfe912ac020a42c2b9da
Tom Hipkin <tomhipkin@gmail.com>
2019-12-19 16:55:32 +0000

    Refactor activity unit tests to use shoulda matchers
    Use a more concise syntax which will help us add more validations.

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -11,30 +11,18 @@ RSpec.describe Activity, type: :model do
 
   describe "validations" do
     context "when recipient_region is blank" do
-      it "raises an error" do
-        activity = build(:activity, recipient_region: nil, wizard_status: :country)
-        activity.valid?
-        expect(activity.errors.messages)
-          .to include(recipient_region: ["Recipient region can't be blank"])
-      end
+      subject { build(:activity, recipient_region: nil, wizard_status: :country) }
+      it { should validate_presence_of(:recipient_region) }
     end
 
     context "when flow is blank" do
-      it "raises an error" do
-        activity = build(:activity, flow: nil, wizard_status: :flow)
-        activity.valid?
-        expect(activity.errors.messages)
-          .to include(flow: ["Flow can't be blank"])
-      end
+      subject { build(:activity, flow: nil, wizard_status: :flow) }
+      it { should validate_presence_of(:flow) }
     end
 
     context "when tied_status is blank" do
-      it "raises an error" do
-        activity = build(:activity, tied_status: nil, wizard_status: :tied_status)
-        activity.valid?
-        expect(activity.errors.messages)
-          .to include(tied_status: ["Tied status can't be blank"])
-      end
+      subject { build(:activity, tied_status: nil, wizard_status: :tied_status) } 
+      it { should validate_presence_of(:tied_status) }
     end
   end
 end
