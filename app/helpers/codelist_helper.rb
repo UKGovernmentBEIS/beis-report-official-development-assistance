1c65975db824fff7788883be3debcb4f0c376f4b
Tom Hipkin <tomhipkin@gmail.com>
2019-12-19 15:32:36 +0000

    Refactor codelist helper to use named params
    * I had to go and check what "generic" and the boolean meant when trying to reuse this method. Switched to named params for readability.

--- app/helpers/codelist_helper.rb
+++ app/helpers/codelist_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module CodelistHelper
-  def yaml_to_objects(entity, type, with_empty_item = true)
-    data = load_yaml(entity, type)
+  def yaml_to_objects(entity:, type:, with_empty_item: true)
+    data = load_yaml(entity: entity, type: type)
     return [] if data.empty?
 
     objects = data.collect { |item| OpenStruct.new(name: item["name"], code: item["code"]) }.sort_by(&:name)
@@ -14,11 +14,11 @@ module CodelistHelper
   end
 
   def currency_select_options
-    objects = yaml_to_objects("generic", "default_currency", false)
+    objects = yaml_to_objects(entity: "generic", type: "default_currency", with_empty_item: false)
     objects.unshift(OpenStruct.new(name: "Pound Sterling", code: "GBP")).uniq
   end
 
-  def load_yaml(entity, type)
+  def load_yaml(entity:, type:)
     yaml = YAML.safe_load(File.read("#{Rails.root}/vendor/data/codelists/IATI/#{IATI_VERSION}/#{entity}/#{type}.yml"))
     yaml["data"]
   rescue
