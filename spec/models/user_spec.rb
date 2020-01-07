2ddf869ce866394560d123c981cc23bd772d06ff
meyric <meyric@dxw.com>
2020-01-07 17:10:38 +0000

    Display users role on index and show pages

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe User, type: :model do
   describe "associations" do
     it { should have_and_belong_to_many(:organisations) }
   end
+
+  describe "#role_name" do
+    it "returns the human readable role name of the user" do
+      user = User.new(role: :fund_manager)
+
+      expect(user.role_name).to eql "Fund manager"
+    end
+  end
 end
