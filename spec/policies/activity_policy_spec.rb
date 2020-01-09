dfdadbc8c63075203addc0772e400f0a83ca469e
Tom Hipkin <tomhipkin@gmail.com>
2020-01-09 17:38:45 +0000

    Only fund managers can create and edit activities related to a fund
    * It isn't ideal that the ActivityPolicy has to have awareness of the different classes of hierarchy. It was the only way I could imagine chaining the conditionals together to ensure fund managers can edit funds, and delivery partners can edit projects, without allowing either to edit each others.

--- spec/policies/activity_policy_spec.rb
+++ spec/policies/activity_policy_spec.rb
@@ -1,49 +1,56 @@
 require "rails_helper"
 
 RSpec.describe ActivityPolicy do
+  let(:organisation) { create(:organisation) }
+
   subject { described_class.new(user, activity) }
 
-  let(:organisation) { create(:organisation) }
-  let(:fund) { create(:fund, organisation: organisation) }
-  let(:activity) { create(:activity, hierarchy: fund) }
+  context "for a fund" do
+    let(:fund) { create(:fund, organisation: organisation) }
+    let(:activity) { create(:activity, hierarchy: fund) }
 
-  context "as an administrator" do
-    let(:user) { build_stubbed(:administrator) }
+    context "as an administrator" do
+      let(:user) { build_stubbed(:administrator) }
 
-    it { is_expected.to permit_action(:index) }
-    it { is_expected.to permit_action(:show) }
-    it { is_expected.to permit_new_and_create_actions }
-    it { is_expected.to permit_edit_and_update_actions }
-    it { is_expected.to permit_action(:destroy) }
-  end
-
-  context "as a delivery partner" do
-    let(:user) { build_stubbed(:user) }
-    let(:resolved_scope) do
-      described_class::Scope.new(user, Activity.all).resolve
-    end
-
-    it { is_expected.to permit_action(:index) }
-    it { is_expected.to permit_action(:show) }
-    it { is_expected.to permit_new_and_create_actions }
-    it { is_expected.to permit_edit_and_update_actions }
-    it { is_expected.to permit_action(:destroy) }
-
-    context "with activities from funds in my own organisation" do
-      let(:user) { create(:administrator, organisations: [organisation]) }
+      it { is_expected.to permit_action(:index) }
+      it { is_expected.to permit_action(:show) }
+      it { is_expected.to permit_new_and_create_actions }
+      it { is_expected.to permit_edit_and_update_actions }
+      it { is_expected.to permit_action(:destroy) }
 
       it "includes activity in resolved scope" do
+        resolved_scope = described_class::Scope.new(user, Activity.all).resolve
         expect(resolved_scope).to include(activity)
       end
     end
 
-    context "with activities from funds in another organisation" do
-      let(:other_organisation) { create(:organisation) }
-      let(:forbidden_fund) { create(:fund, organisation: other_organisation) }
-      let(:activity) { create(:activity, hierarchy: forbidden_fund) }
+    context "as a fund_manager" do
+      let(:user) { build_stubbed(:fund_manager, organisations: [organisation]) }
+
+      it { is_expected.to permit_action(:index) }
+      it { is_expected.to permit_action(:show) }
+      it { is_expected.to permit_new_and_create_actions }
+      it { is_expected.to permit_edit_and_update_actions }
+      it { is_expected.to permit_action(:destroy) }
+
+      it "includes activity in resolved scope" do
+        resolved_scope = described_class::Scope.new(user, Activity.all).resolve
+        expect(resolved_scope).to include(activity)
+      end
+    end
+
+    context "as a delivery_partner" do
+      let(:user) { build_stubbed(:delivery_partner) }
+
+      it { is_expected.to permit_action(:index) }
+      it { is_expected.to forbid_action(:show) }
+      it { is_expected.to forbid_new_and_create_actions }
+      it { is_expected.to forbid_edit_and_update_actions }
+      it { is_expected.to forbid_action(:destroy) }
 
       it "does not include activity in resolved scope" do
-        expect(resolved_scope).to_not include(activity)
+        resolved_scope = described_class::Scope.new(user, Activity.all).resolve
+        expect(resolved_scope).not_to include(activity)
       end
     end
   end
