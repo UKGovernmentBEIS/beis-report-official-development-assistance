c962c1b6c28b065e063b31932d6320950cc4a22d
Tom Hipkin <tomhipkin@gmail.com>
2020-01-10 13:26:36 +0000

    Only fund managers can create transactions on a fund

--- spec/features/staff/users_can_create_a_transaction_spec.rb
+++ spec/features/staff/users_can_create_a_transaction_spec.rb
@@ -1,56 +1,22 @@
 RSpec.feature "Users can create a transaction" do
-  before do
-    authenticate!(user: user)
-  end
-
   let(:organisation) { create(:organisation, name: "UKSA") }
-  let!(:fund) { create(:fund, organisation: organisation, name: "My Space Fund") }
-  let!(:activity) { create(:activity, hierarchy: fund) }
-  let(:user) { create(:administrator, organisations: [organisation]) }
+  let(:activity) { create(:activity, hierarchy: fund) }
 
   context "when the user is not logged in" do
     it "redirects the user to the root path" do
+      fund = create(:fund, organisation: organisation)
       page.set_rack_session(userinfo: nil)
       visit organisation_fund_path(organisation, fund)
       expect(current_path).to eq(root_path)
     end
   end
 
-  scenario "an incoming fund transaction" do
-    visit dashboard_path
-    click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
+  context "when the user is a fund_manager" do
+    before { authenticate!(user: build_stubbed(:fund_manager, organisations: [organisation]))}
 
-    click_on(organisation.name)
-    click_on(fund.name)
+    scenario "successfully creates a transaction on a fund" do
+      fund = create(:fund, organisation: organisation)
 
-    click_on(I18n.t("page_content.transactions.button.create"))
-
-    fill_in_transaction_form
-
-    expect(page).to have_content(I18n.t("form.transaction.create.success"))
-  end
-
-  scenario "validations" do
-    visit dashboard_path
-    click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
-
-    click_on(organisation.name)
-    click_on(fund.name)
-
-    click_on(I18n.t("page_content.transactions.button.create"))
-    click_on(I18n.t("generic.button.submit"))
-
-    expect(page).to_not have_content(I18n.t("form.transaction.create.success"))
-    expect(page).to have_content("Reference can't be blank")
-    expect(page).to have_content("Description can't be blank")
-    expect(page).to have_content("Transaction type can't be blank")
-    expect(page).to have_content("Date can't be blank")
-    expect(page).to have_content("Value must be between 1 and 99,999,999,999.00")
-    expect(page).to have_content("Disbursement channel can't be blank")
-  end
-
-  context "Value number validation" do
-    scenario "Value must be between 1 and 99,999,999,999" do
       visit dashboard_path
       click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
 
@@ -59,74 +25,137 @@ RSpec.feature "Users can create a transaction" do
 
       click_on(I18n.t("page_content.transactions.button.create"))
 
-      fill_in "transaction[reference]", with: "123"
-      fill_in "transaction[description]", with: "This money will be purchasing a new school roof"
-      select "Outgoing Pledge", from: "transaction[transaction_type]"
-      fill_in "transaction[date(3i)]", with: "1"
-      fill_in "transaction[date(2i)]", with: "1"
-      fill_in "transaction[date(1i)]", with: "2020"
-      fill_in "transaction[value]", with: "100000000000"
-      select "Money is disbursed through central Ministry of Finance or Treasury", from: "transaction[disbursement_channel]"
-      select "Pound Sterling", from: "transaction[currency]"
+      fill_in_transaction_form
+
+      expect(page).to have_content(I18n.t("form.transaction.create.success"))
+    end
+
+    scenario "validations" do
+      fund = create(:fund, organisation: organisation)
+
+      visit dashboard_path
+      click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
+
+      click_on(organisation.name)
+      click_on(fund.name)
+
+      click_on(I18n.t("page_content.transactions.button.create"))
       click_on(I18n.t("generic.button.submit"))
 
+      expect(page).to_not have_content(I18n.t("form.transaction.create.success"))
+      expect(page).to have_content("Reference can't be blank")
+      expect(page).to have_content("Description can't be blank")
+      expect(page).to have_content("Transaction type can't be blank")
+      expect(page).to have_content("Date can't be blank")
       expect(page).to have_content("Value must be between 1 and 99,999,999,999.00")
+      expect(page).to have_content("Disbursement channel can't be blank")
     end
 
-    scenario "When the value includes a pound sign" do
-      visit dashboard_path
-      click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
+    context "Value number validation" do
+      scenario "Value must be between 1 and 99,999,999,999" do
+        fund = create(:fund, organisation: organisation)
 
-      click_on(organisation.name)
-      click_on(fund.name)
+        visit dashboard_path
+        click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
 
-      click_on(I18n.t("page_content.transactions.button.create"))
+        click_on(organisation.name)
+        click_on(fund.name)
 
-      fill_in_transaction_form(value: "£123", expectations: false)
+        click_on(I18n.t("page_content.transactions.button.create"))
 
-      expect(page).to have_content "123"
+        fill_in "transaction[reference]", with: "123"
+        fill_in "transaction[description]", with: "This money will be purchasing a new school roof"
+        select "Outgoing Pledge", from: "transaction[transaction_type]"
+        fill_in "transaction[date(3i)]", with: "1"
+        fill_in "transaction[date(2i)]", with: "1"
+        fill_in "transaction[date(1i)]", with: "2020"
+        fill_in "transaction[value]", with: "100000000000"
+        select "Money is disbursed through central Ministry of Finance or Treasury", from: "transaction[disbursement_channel]"
+        select "Pound Sterling", from: "transaction[currency]"
+        click_on(I18n.t("generic.button.submit"))
+
+        expect(page).to have_content("Value must be between 1 and 99,999,999,999.00")
+      end
+
+      scenario "When the value includes a pound sign" do
+        fund = create(:fund, organisation: organisation)
+
+        visit dashboard_path
+        click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
+
+        click_on(organisation.name)
+        click_on(fund.name)
+
+        click_on(I18n.t("page_content.transactions.button.create"))
+
+        fill_in_transaction_form(value: "£123", expectations: false)
+
+        expect(page).to have_content "123"
+      end
+
+      scenario "When the value includes alphabetical characters" do
+        fund = create(:fund, organisation: organisation)
+
+        visit dashboard_path
+        click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
+
+        click_on(organisation.name)
+        click_on(fund.name)
+
+        click_on(I18n.t("page_content.transactions.button.create"))
+
+        fill_in_transaction_form(value: "abc123def", expectations: false)
+
+        expect(page).to have_content "123"
+      end
+
+      scenario "When the value includes decimal places" do
+        fund = create(:fund, organisation: organisation)
+
+        visit dashboard_path
+        click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
+
+        click_on(organisation.name)
+        click_on(fund.name)
+
+        click_on(I18n.t("page_content.transactions.button.create"))
+
+        fill_in_transaction_form(value: "100.12", expectations: false)
+
+        expect(page).to have_content "100.12"
+      end
+
+      scenario "When the value includes commas" do
+        fund = create(:fund, organisation: organisation)
+
+        visit dashboard_path
+        click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
+
+        click_on(organisation.name)
+        click_on(fund.name)
+
+        click_on(I18n.t("page_content.transactions.button.create"))
+
+        fill_in_transaction_form(value: "123,000,000", expectations: false)
+
+        expect(page).to have_content "123000000"
+      end
     end
 
-    scenario "When the value includes alphabetical characters" do
-      visit dashboard_path
-      click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
+    # TODO: When we come to create transactions for different types of activity
+    # for projects and programmes etc, we will want to test that users who are
+    # deliver partners can create transactions for project activities too.
+    context "when the user is a delivery_partner" do
+      before { authenticate!(user: build_stubbed(:delivery_partner, organisations: [organisation]))}
 
-      click_on(organisation.name)
-      click_on(fund.name)
+      scenario "cannot create an transaction that belongs to a fund activity" do
+        fund = create(:fund, organisation: organisation)
+        activity = create(:activity, hierarchy: fund)
 
-      click_on(I18n.t("page_content.transactions.button.create"))
+        visit new_fund_transaction_path(fund)
 
-      fill_in_transaction_form(value: "abc123def", expectations: false)
-
-      expect(page).to have_content "123"
-    end
-
-    scenario "When the value includes decimal places" do
-      visit dashboard_path
-      click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
-
-      click_on(organisation.name)
-      click_on(fund.name)
-
-      click_on(I18n.t("page_content.transactions.button.create"))
-
-      fill_in_transaction_form(value: "100.12", expectations: false)
-
-      expect(page).to have_content "100.12"
-    end
-
-    scenario "When the value includes commas" do
-      visit dashboard_path
-      click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
-
-      click_on(organisation.name)
-      click_on(fund.name)
-
-      click_on(I18n.t("page_content.transactions.button.create"))
-
-      fill_in_transaction_form(value: "123,000,000", expectations: false)
-
-      expect(page).to have_content "123000000"
+        expect(page).to have_content(I18n.t("page_title.errors.not_authorised"))
+      end
     end
   end
 end
