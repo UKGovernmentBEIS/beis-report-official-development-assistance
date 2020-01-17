698cebc1bcf5f491a62460e44ea5d748c955da0e
Laura Porter <laura@dxw.com>
2020-01-17 10:18:48 +0000

    Allow the user to add Activity information to a Programme
    In much the same way as Activity information can be added to a Fund, add the same
    information to an Activity attached to a Programme

--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -1,6 +1,24 @@
 module ActivityHelper
   def hierarchy_path_for(activity:)
-    url_for([activity.hierarchy.organisation, activity.hierarchy])
+    # TODO there must be a better way
+    # organisation_fund_path when hierarchy is a fund
+    # fund_programme_path when hierarchy is a programme
+    case activity.hierarchy.class.name
+    when "Fund"
+      url_for([activity.hierarchy.organisation, activity.hierarchy])
+    when "Programme"
+      url_for([activity.hierarchy.fund, activity.hierarchy])
+    end
+  end
+
+  def edit_hierarchy_path_for(activity:)
+    # TODO there must be a better way
+    case activity.hierarchy.class.name
+    when "Fund"
+      url_for([:edit, activity.hierarchy.organisation, activity.hierarchy])
+    when "Programme"
+      url_for([:edit, activity.hierarchy.fund, activity.hierarchy])
+    end
   end
 
   def activity_path_for(activity:)
--- spec/features/staff/users_can_create_an_activity_spec.rb
+++ spec/features/staff/users_can_create_an_activity_spec.rb
@@ -14,125 +14,155 @@ RSpec.feature "Users can create an activity" do
   context "when the user is a fund_manager" do
     before { authenticate!(user: build_stubbed(:fund_manager, organisation: organisation)) }
 
-    scenario "successfully creates a fund activity with all optional information" do
-      fund = create(:fund, organisation: organisation)
+    context "when the activity's hierarchy is a fund" do
+      scenario "successfully creates a fund activity with all optional information" do
+        fund = create(:fund, organisation: organisation)
 
-      visit dashboard_path
-      click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
-      click_on(organisation.name)
-      click_on(fund.name)
+        visit dashboard_path
+        click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
+        click_on(organisation.name)
+        click_on(fund.name)
 
-      click_on I18n.t("page_content.fund.button.create_activity", activity: "fund")
+        click_on I18n.t("page_content.fund.button.create_activity", activity: "fund")
 
-      fill_in_activity_form
-    end
+        fill_in_activity_form
+      end
 
-    scenario "the activity form has some defaults for funds" do
-      fund = create(:fund, organisation: organisation)
-      visit organisation_fund_path(organisation, fund)
+      scenario "the activity form has some defaults for funds" do
+        fund = create(:fund, organisation: organisation)
+        visit organisation_fund_path(organisation, fund)
 
-      click_on I18n.t("page_content.fund.button.create_activity", activity: "fund")
-      activity = Activity.last
+        click_on I18n.t("page_content.fund.button.create_activity", activity: "fund")
+        activity = Activity.last
 
-      visit fund_activity_steps_path(fund_id: fund, activity_id: activity, id: :country)
-      expect(page.find("option[@selected = 'selected']").text).to eq("Developing countries, unspecified")
+        visit fund_activity_steps_path(fund_id: fund, activity_id: activity, id: :country)
+        expect(page.find("option[@selected = 'selected']").text).to eq("Developing countries, unspecified")
 
-      visit fund_activity_steps_path(fund_id: fund, activity_id: activity, id: :flow)
-      expect(page.find("option[@selected = 'selected']").text).to eq("ODA")
+        visit fund_activity_steps_path(fund_id: fund, activity_id: activity, id: :flow)
+        expect(page.find("option[@selected = 'selected']").text).to eq("ODA")
 
-      visit fund_activity_steps_path(fund_id: fund, activity_id: activity, id: :tied_status)
-      expect(page.find("option[@selected = 'selected']").text).to eq("Untied")
-    end
+        visit fund_activity_steps_path(fund_id: fund, activity_id: activity, id: :tied_status)
+        expect(page.find("option[@selected = 'selected']").text).to eq("Untied")
+      end
 
-    context "validations" do
-      scenario "validation errors work as expected" do
+      context "validations" do
+        scenario "validation errors work as expected" do
+          fund = create(:fund, organisation: organisation)
+          visit organisation_fund_path(organisation, fund)
+          click_on I18n.t("page_content.fund.button.create_activity", activity: "fund")
+
+          # Don't provide an identifier
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content "can't be blank"
+
+          fill_in "activity[identifier]", with: "foo"
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content I18n.t("page_title.activity_form.show.purpose")
+
+          # Don't provide a title and description
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content "Title can't be blank"
+          expect(page).to have_content "Description can't be blank"
+
+          fill_in "activity[title]", with: Faker::Lorem.word
+          fill_in "activity[description]", with: Faker::Lorem.paragraph
+          click_button I18n.t("form.activity.submit")
+
+          expect(page).to have_content I18n.t("page_title.activity_form.show.sector")
+
+          # Don't provide a sector
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content "Sector can't be blank"
+
+          select "Education policy and administrative management", from: "activity[sector]"
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content I18n.t("page_title.activity_form.show.status")
+
+          # Don't provide a status
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content "Status can't be blank"
+
+          select "Implementation", from: "activity[status]"
+          click_button I18n.t("form.activity.submit")
+
+          expect(page).to have_content I18n.t("page_title.activity_form.show.dates")
+
+          # Dates are not mandatory so we can move through this step
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content I18n.t("page_title.activity_form.show.country")
+
+          # Region has a default and can't be set to blank so we skip
+          select "Developing countries, unspecified", from: "activity[recipient_region]"
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content I18n.t("page_title.activity_form.show.flow")
+
+          # Flow has a default and can't be set to blank so we skip
+          select "ODA", from: "activity[flow]"
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content I18n.t("page_title.activity_form.show.finance")
+
+          # Don't select a finance
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content "Finance can't be blank"
+
+          select "Standard grant", from: "activity[finance]"
+          click_button I18n.t("form.activity.submit")
+
+          expect(page).to have_content I18n.t("page_title.activity_form.show.aid_type")
+
+          # Don't select an aid type
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content "Aid type can't be blank"
+
+          select "General budget support", from: "activity[aid_type]"
+          click_button I18n.t("form.activity.submit")
+
+          expect(page).to have_content I18n.t("page_title.activity_form.show.tied_status")
+
+          # Tied status has a default and can't be set to blank so we skip
+          select "Untied", from: "activity[tied_status]"
+          click_button I18n.t("form.activity.submit")
+          expect(page).to have_content fund.name
+        end
+      end
+
+      scenario "can go back to the previous page" do
         fund = create(:fund, organisation: organisation)
         visit organisation_fund_path(organisation, fund)
         click_on I18n.t("page_content.fund.button.create_activity", activity: "fund")
 
-        # Don't provide an identifier
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content "can't be blank"
+        click_on I18n.t("generic.link.back")
 
-        fill_in "activity[identifier]", with: "foo"
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content I18n.t("page_title.activity_form.show.purpose")
-
-        # Don't provide a title and description
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content "Title can't be blank"
-        expect(page).to have_content "Description can't be blank"
-
-        fill_in "activity[title]", with: Faker::Lorem.word
-        fill_in "activity[description]", with: Faker::Lorem.paragraph
-        click_button I18n.t("form.activity.submit")
-
-        expect(page).to have_content I18n.t("page_title.activity_form.show.sector")
-
-        # Don't provide a sector
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content "Sector can't be blank"
-
-        select "Education policy and administrative management", from: "activity[sector]"
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content I18n.t("page_title.activity_form.show.status")
-
-        # Don't provide a status
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content "Status can't be blank"
-
-        select "Implementation", from: "activity[status]"
-        click_button I18n.t("form.activity.submit")
-
-        expect(page).to have_content I18n.t("page_title.activity_form.show.dates")
-
-        # Dates are not mandatory so we can move through this step
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content I18n.t("page_title.activity_form.show.country")
-
-        # Region has a default and can't be set to blank so we skip
-        select "Developing countries, unspecified", from: "activity[recipient_region]"
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content I18n.t("page_title.activity_form.show.flow")
-
-        # Flow has a default and can't be set to blank so we skip
-        select "ODA", from: "activity[flow]"
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content I18n.t("page_title.activity_form.show.finance")
-
-        # Don't select a finance
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content "Finance can't be blank"
-
-        select "Standard grant", from: "activity[finance]"
-        click_button I18n.t("form.activity.submit")
-
-        expect(page).to have_content I18n.t("page_title.activity_form.show.aid_type")
-
-        # Don't select an aid type
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content "Aid type can't be blank"
-
-        select "General budget support", from: "activity[aid_type]"
-        click_button I18n.t("form.activity.submit")
-
-        expect(page).to have_content I18n.t("page_title.activity_form.show.tied_status")
-
-        # Tied status has a default and can't be set to blank so we skip
-        select "Untied", from: "activity[tied_status]"
-        click_button I18n.t("form.activity.submit")
-        expect(page).to have_content fund.name
+        expect(page).to have_current_path(organisation_fund_path(fund.id, organisation_id: organisation.id))
       end
     end
 
-    scenario "can go back to the previous page" do
-      fund = create(:fund, organisation: organisation)
-      visit organisation_fund_path(organisation, fund)
-      click_on I18n.t("page_content.fund.button.create_activity", activity: "fund")
+    context "when the activity's hierarchy is a programme" do
+      scenario "successfully creates a programme activity with all optional information" do
+        fund = create(:fund, organisation: organisation)
+        programme = create(:programme, organisation: organisation, fund: fund)
 
-      click_on I18n.t("generic.link.back")
+        visit dashboard_path
+        click_on(I18n.t("page_content.dashboard.button.manage_organisations"))
+        click_on(organisation.name)
+        click_on(fund.name)
+        click_on(programme.name)
 
-      expect(page).to have_current_path(organisation_fund_path(fund.id, organisation_id: organisation.id))
+        click_on I18n.t("page_content.fund.button.create_activity", activity: "programme")
+
+        fill_in_activity_form
+      end
+
+      scenario "can go back to the previous page" do
+        fund = create(:fund, organisation: organisation)
+        programme = create(:programme, fund: fund)
+        visit fund_programme_path(fund, programme)
+        click_on I18n.t("page_content.fund.button.create_activity", activity: "programme")
+
+        click_on I18n.t("generic.link.back")
+
+        expect(page).to have_current_path(fund_programme_path(fund, programme))
+      end
     end
   end
 
