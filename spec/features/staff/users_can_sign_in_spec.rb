af1f30cdbe0d9c8b6f6091dd18aa4fa0d0c1fefe
Tom Hipkin <tomhipkin@gmail.com>
2020-01-21 12:40:56 +0000

    Users are redirected to their organisation page when they log in
    * We are hoping to remove the dashboard page next, but as many tests
    will depend upon it we want to tackle this as 2 steps.
    
    Co-authored=by: Agz <a@deberny.com>
    Co-authored-by: Lorna <lorna@dxw.com>

--- spec/features/staff/users_can_sign_in_spec.rb
+++ spec/features/staff/users_can_sign_in_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Users can sign in with Auth0" do
       uid: user.identifier, name: user.name, email: user.email
     )
 
-    visit dashboard_path
+    visit root_path
     expect(page).to have_content(I18n.t("page_title.welcome"))
 
     within ".app-header__user-links" do
@@ -37,6 +37,45 @@ RSpec.feature "Users can sign in with Auth0" do
     expect(page).to have_content(I18n.t("page_title.dashboard"))
     expect(page).to have_content(user.name)
     expect(page).to have_content(I18n.t("generic.link.sign_out"))
+  end
+
+  scenario "any user lands on their organisation page" do
+    user = create(:administrator)
+
+    mock_successful_authentication(
+      uid: user.identifier, name: user.name, email: user.email
+    )
+
+    visit root_path
+    expect(page).to have_content(I18n.t("page_title.welcome"))
+
+    within ".app-header__user-links" do
+      expect(page).to have_content(I18n.t("generic.link.sign_in"))
+      click_on I18n.t("generic.link.sign_in")
+    end
+
+    expect(page).to have_content(user.organisation.name)
+  end
+
+  context "when the user doesn't have an organisation" do
+    scenario "they see a 401 not authorised page" do
+      user = build(:administrator, organisation: nil)
+      user.save(validate: false)
+
+      mock_successful_authentication(
+        uid: user.identifier, name: user.name, email: user.email
+      )
+
+      visit root_path
+
+      within ".app-header__user-links" do
+        expect(page).to have_content(I18n.t("generic.link.sign_in"))
+        click_on I18n.t("generic.link.sign_in")
+      end
+
+      expect(page).to have_content(I18n.t("page_title.errors.not_authorised"))
+      expect(page).to have_content(I18n.t("page_content.errors.not_authorised.explanation"))
+    end
   end
 
   scenario "protected pages cannot be visited unless signed in" do
