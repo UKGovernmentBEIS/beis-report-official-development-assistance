9357fdd1a9e0d1951af1ba82847fb6ba77d246d9
Lorna Harwood <lorna@dxw.com>
2020-01-30 16:29:53 +0000

    Provide a way to flag an organisation as BEIS
    We only imagine this to be done very infrequently by a developer in the console, so there is no UI for this.
    
    co-authored-by: Tom <tomhipkin@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_01_28_155455) do
     t.string "default_currency"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "service_owner", default: false
   end
 
   create_table "transactions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
