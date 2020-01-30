d4c9a55fbdba3c9cccb09513e72a926c49998533
meyric <meyric@dxw.com>
2020-01-30 16:54:56 +0000

    wip
    - showing programmes on fund level activity
    - adding programmes to fund level activities

--- spec/features/staff/fund_managers_can_create_a_programme_level_activity_spec.rb
+++ spec/features/staff/fund_managers_can_create_a_programme_level_activity_spec.rb
@@ -1,13 +1,16 @@
 RSpec.feature "Create a programme level activity" do
-  let!(:organisation) { create(:organisation, name: "UKSA") }
+  let!(:organisation) { create(:organisation, name: "BEIS") }
 
   context "when the user is a fund_manager" do
     before { authenticate!(user: create(:fund_manager, organisation: organisation)) }
 
     scenario "successfully create a activity" do
+      fund = create(:activity, level: :fund, organisation: organisation)
+
       visit organisation_path(organisation)
       click_link(I18n.t("page_content.dashboard.button.manage_organisations"))
       click_on(organisation.name)
+      click_on(fund.title)
       click_on(I18n.t("page_content.organisation.button.create_programme"))
 
       fill_in_activity_form
