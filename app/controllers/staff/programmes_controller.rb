d6bc32a08af34cfeb4437c5e7d4e7046ae32f433
Laura Porter <laura@dxw.com>
2020-02-03 14:49:34 +0000

    Add Fund- and Programme-specific defaults for funding organisations to Activities

--- app/controllers/staff/programmes_controller.rb
+++ app/controllers/staff/programmes_controller.rb
@@ -10,6 +10,9 @@ class Staff::ProgrammesController < Staff::ActivitiesController
 
     @activity.wizard_status = "identifier"
     @activity.level = :programme
+    @activity.funding_organisation_name = "Department for Business, Energy and Industrial Strategy"
+    @activity.funding_organisation_reference = "GB-GOV-13"
+    @activity.funding_organisation_type = "10"
     @activity.save(validate: false)
 
     redirect_to activity_step_path(@activity.id, @activity.wizard_status)
