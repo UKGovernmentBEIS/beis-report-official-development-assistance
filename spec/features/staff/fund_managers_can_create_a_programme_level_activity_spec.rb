fd54ca44d093ff9498bbd2b13a9d743e7085e6cd
Laura Porter <laura@dxw.com>
2020-02-04 13:26:41 +0000

    Add Accountable Organisation details to Activities and set defaults

--- spec/features/staff/fund_managers_can_create_a_programme_level_activity_spec.rb
+++ spec/features/staff/fund_managers_can_create_a_programme_level_activity_spec.rb
@@ -19,8 +19,8 @@ RSpec.feature "Fund managers can create programme level activities" do
     end
 
     scenario "the activity has the appropriate funding organisation defaults" do
-      identifier = "a-programme-has-a-funding-organisation"
       fund = create(:activity, level: :fund, organisation: organisation)
+      identifier = "a-programme-has-a-funding-organisation"
 
       visit organisation_path(organisation)
       click_on fund.title
@@ -32,6 +32,22 @@ RSpec.feature "Fund managers can create programme level activities" do
       expect(activity.funding_organisation_name).to eq("Department for Business, Energy and Industrial Strategy")
       expect(activity.funding_organisation_reference).to eq("GB-GOV-13")
       expect(activity.funding_organisation_type).to eq("10")
+    end
+
+    scenario "the activity has the appropriate accountable organisation defaults" do
+      fund = create(:activity, level: :fund, organisation: organisation)
+      identifier = "a-fund-has-an-accountable-organisation"
+
+      visit organisation_path(organisation)
+      click_on fund.title
+      click_on(I18n.t("page_content.organisation.button.create_programme"))
+
+      fill_in_activity_form(identifier: identifier)
+
+      activity = Activity.find_by(identifier: identifier)
+      expect(activity.accountable_organisation_name).to eq("Department for Business, Energy and Industrial Strategy")
+      expect(activity.accountable_organisation_reference).to eq("GB-GOV-13")
+      expect(activity.accountable_organisation_type).to eq("10")
     end
   end
 end
