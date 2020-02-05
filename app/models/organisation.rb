2312b0e69e9dad019e48c575d4d160077e85396a
meyric <meyric@dxw.com>
2020-02-05 12:08:21 +0000

    Add IATI reference to organisations
    The IATI reference is used to uniquely identify an organisation and is
    published alongside any references to the organisation.
    
    As there is not a formal standard for the reference we can only follow the
    IATI recommendations for creating the reference. This also makes the
    reference difficult to validate, at this point we collect it from a text
    field without validation.
    
    See the IATAI documentation:
    
    https://reference.iatistandard.org/organisation-identifiers/
    
    As the IATI reference is required to publish data to IATI we require it
    on organisations.

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,5 +3,7 @@ class Organisation < ApplicationRecord
   has_many :funds
 
   validates_presence_of :name, :organisation_type, :language_code, :default_currency
+  validates :iati_reference, uniqueness: {case_sensitive: false}, presence: true
+
   scope :sorted_by_name, -> { order(name: :asc) }
 end
