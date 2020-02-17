0ee83be23f0644d4e46e850b452a6da928651bb3
meyric <meyric@dxw.com>
2020-02-17 09:26:43 +0000

    Implimenting organisations presenter
    We store the `organisation_type` as a code number but want to display it
    as the human readable text to the user - this presenter enables that.

---
+++ spec/presenters/implementing_organisation_presenter_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe ImplementingOrganisationPresenter do
+  describe "#organisation_type" do
+    it "takes the code and changes it to the name of the organisation type" do
+      organisation = ImplementingOrganisation.new(name: "This organisation", organisation_type: "70", reference: "GB-COH-1234566")
+      implementing_organisation_presenter = ImplementingOrganisationPresenter.new(organisation)
+
+      expect(implementing_organisation_presenter.organisation_type).to eq "Private Sector"
+    end
+  end
+end
