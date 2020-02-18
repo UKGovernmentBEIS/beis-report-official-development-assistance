f2d17da1109872abdcc7ee3678f05c40628acabe
meyric <meyric@dxw.com>
2020-02-18 12:10:23 +0000

    Budgets can be added to project test coverage
    The previous commit allowed budgets to be added to project level
    activities. This commit adds test coverage for that behaviour but also
    re-organises the spec to try and better document the behaviour of the
    application.
    
    As there is a lot of change here, I've committed it separately.

--- spec/features/staff/users_can_create_a_budget_spec.rb
+++ spec/features/staff/users_can_create_a_budget_spec.rb
@@ -1,87 +1,34 @@
 RSpec.describe "Users can create a budget" do
-  let(:organisation) { create(:organisation, name: "UKSA") }
+  let(:organisation) { create(:delivery_partner_organisation, name: "UKSA") }
 
-  context "when the user is not logged in" do
-    it "redirects the user to the root path" do
-      activity = create(:activity)
-      page.set_rack_session(userinfo: nil)
-      visit new_activity_budget_path(activity)
-      expect(current_path).to eq(root_path)
-    end
-  end
+  let!(:fund_activity) { create(:activity, organisation: organisation) }
+  let!(:programme_activity) { create(:programme_activity, activity: fund_activity, organisation: organisation) }
+  let!(:project_activity) { create(:project_activity, activity: programme_activity, organisation: organisation) }
 
-  context "on a programme" do
-    let!(:fund_activity) { create(:activity, organisation: organisation) }
-    let!(:programme_activity) do
-      create(:programme_activity,
-        activity: fund_activity,
-        organisation: organisation)
-    end
+  context "when signed in as an administrator" do
+    let(:fund_manager) { create(:administrator, organisation: organisation) }
 
-    context "as a fund manager" do
-      let(:fund_manager) { create(:fund_manager, organisation: organisation) }
+    before { authenticate!(user: fund_manager) }
 
-      before { authenticate!(user: fund_manager) }
+    scenario "sees validation errors for missing attributes" do
+      visit organisation_path(organisation)
 
-      scenario "successfully creates a budget" do
-        visit organisation_path(organisation)
-        click_on(fund_activity.title)
-        click_on(programme_activity.title)
+      click_on(fund_activity.title)
+      click_on(programme_activity.title)
 
-        click_on(I18n.t("page_content.budgets.button.create"))
+      click_on(I18n.t("page_content.budgets.button.create"))
 
-        choose("budget[budget_type]", option: "original")
-        choose("budget[status]", option: "indicative")
-        fill_in "budget[period_start_date(3i)]", with: "01"
-        fill_in "budget[period_start_date(2i)]", with: "01"
-        fill_in "budget[period_start_date(1i)]", with: "2020"
-        fill_in "budget[period_end_date(3i)]", with: "01"
-        fill_in "budget[period_end_date(2i)]", with: "01"
-        fill_in "budget[period_end_date(1i)]", with: "2021"
-        fill_in "budget[value]", with: "1000.00"
-        click_button I18n.t("generic.button.submit")
+      click_button I18n.t("generic.button.submit")
 
-        expect(page).to have_content(I18n.t("form.budget.create.success"))
-      end
-
-      scenario "sees validation errors for missing attributes" do
-        visit organisation_path(organisation)
-
-        click_on(fund_activity.title)
-        click_on(programme_activity.title)
-
-        click_on(I18n.t("page_content.budgets.button.create"))
-
-        click_button I18n.t("generic.button.submit")
-
-        expect(page).to have_content("There is a problem")
-        expect(page).to have_content("Budget type can't be blank")
-        expect(page).to have_content("Status can't be blank")
-        expect(page).to have_content("Period start date can't be blank")
-        expect(page).to have_content("Period end date can't be blank")
-        expect(page).to have_content("Value is not included in the list")
-      end
+      expect(page).to have_content("There is a problem")
+      expect(page).to have_content("Budget type can't be blank")
+      expect(page).to have_content("Status can't be blank")
+      expect(page).to have_content("Period start date can't be blank")
+      expect(page).to have_content("Period end date can't be blank")
+      expect(page).to have_content("Value is not included in the list")
     end
 
-    context "as a delivery partner" do
-      let(:delivery_partner) { create(:delivery_partner, organisation: organisation) }
-
-      before { authenticate!(user: delivery_partner) }
-
-      scenario "cannot create a budget on a programme" do
-        visit new_activity_budget_path(programme_activity)
-
-        expect(page).to have_content(I18n.t("page_title.errors.not_authorised"))
-      end
-    end
-  end
-
-  context "on a fund" do
-    context "as a fund manager" do
-      let(:fund_manager) { create(:fund_manager, organisation: organisation) }
-
-      before { authenticate!(user: fund_manager) }
-
+    context "on a fund level project" do
       scenario "cannot create a budget on a fund" do
         fund_activity = create(:fund_activity, organisation: organisation)
         visit new_activity_budget_path(fund_activity)
@@ -90,17 +37,71 @@ RSpec.describe "Users can create a budget" do
       end
     end
 
-    context "as a delivery partner" do
-      let(:delivery_partner) { create(:delivery_partner, organisation: organisation) }
+    context "on a programme level activity" do
+      scenario "successfully creates a budget" do
+        visit organisation_path(organisation)
+        click_on(fund_activity.title)
+        click_on(programme_activity.title)
 
-      before { authenticate!(user: delivery_partner) }
+        click_on(I18n.t("page_content.budgets.button.create"))
 
-      scenario "cannot create a budget on a fund" do
-        fund_activity = create(:fund_activity, organisation: organisation)
-        visit new_activity_budget_path(fund_activity)
+        fill_in_and_submit_budget_form
 
-        expect(page).to have_content(I18n.t("page_title.errors.not_authorised"))
+        expect(page).to have_content(I18n.t("form.budget.create.success"))
       end
     end
+
+    context "on a project level activity" do
+      scenario "successfully creates a budget" do
+        visit organisation_path(organisation)
+        click_on(fund_activity.title)
+        click_on(programme_activity.title)
+        click_on(project_activity.title)
+
+        click_on(I18n.t("page_content.budgets.button.create"))
+
+        fill_in_and_submit_budget_form
+
+        expect(page).to have_content(I18n.t("form.budget.create.success"))
+      end
+    end
+  end
+
+  context "when signed in as a delivery partner" do
+    let(:delivery_partner) { create(:delivery_partner, organisation: organisation) }
+
+    before { authenticate!(user: delivery_partner) }
+
+    scenario "cannot create a budget on a fund" do
+      fund_activity = create(:fund_activity, organisation: organisation)
+      visit new_activity_budget_path(fund_activity)
+
+      expect(page).to have_content(I18n.t("page_title.errors.not_authorised"))
+    end
+
+    scenario "cannot create a budget on a programme" do
+      visit new_activity_budget_path(programme_activity)
+
+      expect(page).to have_content(I18n.t("page_title.errors.not_authorised"))
+    end
+
+    scenario "cannot create a budget on a project" do
+      visit new_activity_budget_path(project_activity)
+
+      expect(page).to have_content(I18n.t("page_title.errors.not_authorised"))
+    end
+  end
+
+  def fill_in_and_submit_budget_form
+    choose("budget[budget_type]", option: "original")
+    choose("budget[status]", option: "indicative")
+    fill_in "budget[period_start_date(3i)]", with: "01"
+    fill_in "budget[period_start_date(2i)]", with: "01"
+    fill_in "budget[period_start_date(1i)]", with: "2020"
+    fill_in "budget[period_end_date(3i)]", with: "01"
+    fill_in "budget[period_end_date(2i)]", with: "01"
+    fill_in "budget[period_end_date(1i)]", with: "2021"
+    fill_in "budget[value]", with: "1000.00"
+    click_button I18n.t("generic.button.submit")
   end
 end
