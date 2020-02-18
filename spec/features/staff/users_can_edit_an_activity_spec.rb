81432d884c04e069c29aea4c0ff2397308f3ff24
Tom Hipkin <tomhipkin@gmail.com>
2020-02-18 16:37:21 +0000

    Authorise who can see and interact with activities
    Before this change there was nothing stopping users changing the URL and being able to jump to activities they shouldn't be able to. This was because everyone was an administrator! Now we've hidden the buttons in the view, we also need to protect the end points from being changed and restrict access.
    
    Instead of using lots of feature tests for these unhappy paths I'm using lots of faster policy unit tests.

--- spec/features/staff/users_can_edit_an_activity_spec.rb
+++ spec/features/staff/users_can_edit_an_activity_spec.rb
@@ -1,96 +1,97 @@
 RSpec.feature "Users can edit an activity" do
   include ActivityHelper
 
-  let(:organisation) { create(:organisation, name: "UKSA") }
+  context "when the activity is a fund" do
+    let(:user) { create(:beis_user) }
+    before { authenticate!(user: user) }
 
-  before { authenticate!(user: create(:administrator, organisation: organisation)) }
+    context "when the activity only has an identifier (and is incomplete)" do
+      it "shows edit link on the identifier, and add link on only the next step" do
+        activity = create(:fund_activity, :at_purpose_step, organisation: user.organisation)
 
-  context "when the activity only has an identifier (and is incomplete)" do
-    it "shows edit link on the identifier, and add link on only the next step" do
-      activity = create(:activity, :at_purpose_step, organisation: organisation)
+        visit organisation_activity_path(activity.organisation, activity)
 
-      visit organisation_activity_path(activity.organisation, activity)
+        # Click the first edit link that opens the form on step 1
+        within(".identifier") do
+          expect(page).to have_content(I18n.t("generic.link.edit"))
+        end
 
-      # Click the first edit link that opens the form on step 1
-      within(".identifier") do
-        expect(page).to have_content(I18n.t("generic.link.edit"))
-      end
+        within(".title") do
+          expect(page).to have_content(I18n.t("generic.link.add"))
+        end
 
-      within(".title") do
-        expect(page).to have_content(I18n.t("generic.link.add"))
-      end
-
-      within(".sector") do
-        expect(page).to_not have_content(I18n.t("generic.link.add"))
-      end
-    end
-  end
-
-  context "when the activity is complete" do
-    it "editing and saving a step returns the user to the activity page" do
-      activity = create(:activity, organisation: organisation)
-      identifier = "AB-CDE-1234"
-      visit organisation_activity_path(activity.organisation, activity)
-
-      within(".identifier") do
-        click_on(I18n.t("generic.link.edit"))
-      end
-
-      fill_in "activity[identifier]", with: identifier
-      click_button I18n.t("form.activity.submit")
-
-      expect(page).to have_content I18n.t("form.fund.update.success")
-      expect(page.current_path).to eq organisation_activity_path(activity.organisation, activity)
-    end
-
-    it "all edit links are available to take the user to the right step" do
-      activity = create(:activity, organisation: organisation)
-
-      visit organisation_activity_path(activity.organisation, activity)
-
-      assert_all_edit_links_go_to_the_correct_form_step(activity: activity)
-    end
-  end
-
-  context "when the activity is incomplete" do
-    it "editing and saving a step goes to the next step in the form" do
-      activity = create(:activity, :at_country_step, organisation: organisation)
-      recipient_region = "Oceania, regional"
-
-      visit organisation_activity_path(activity.organisation, activity)
-
-      within(".recipient_region") do
-        click_on(I18n.t("generic.link.edit"))
-      end
-
-      select recipient_region, from: "activity[recipient_region]"
-      click_button I18n.t("form.activity.submit")
-
-      expect(page).to have_content I18n.t("page_title.activity_form.show.flow")
-      expect(page).not_to have_content activity.title
-    end
-  end
-
-  context "when a title attribute is present" do
-    it "the call to action is 'Edit'" do
-      activity = create(:activity, organisation: organisation, wizard_status: :sector)
-
-      visit organisation_activity_path(activity.organisation, activity)
-
-      within(".title") do
-        expect(page).to have_content(I18n.t("generic.link.edit"))
+        within(".sector") do
+          expect(page).to_not have_content(I18n.t("generic.link.add"))
+        end
       end
     end
-  end
 
-  context "when an activity attribute is not present" do
-    it "the call to action is 'Add'" do
-      activity = create(:activity, :at_purpose_step, organisation: organisation)
+    context "when the activity is complete" do
+      it "editing and saving a step returns the user to the activity page" do
+        activity = create(:fund_activity, organisation: user.organisation)
+        identifier = "AB-CDE-1234"
+        visit organisation_activity_path(activity.organisation, activity)
 
-      visit organisation_activity_path(activity.organisation, activity)
+        within(".identifier") do
+          click_on(I18n.t("generic.link.edit"))
+        end
 
-      within(".title") do
-        expect(page).to have_content(I18n.t("generic.link.add"))
+        fill_in "activity[identifier]", with: identifier
+        click_button I18n.t("form.activity.submit")
+
+        expect(page).to have_content I18n.t("form.fund.update.success")
+        expect(page.current_path).to eq organisation_activity_path(activity.organisation, activity)
+      end
+
+      it "all edit links are available to take the user to the right step" do
+        activity = create(:fund_activity, organisation: user.organisation)
+
+        visit organisation_activity_path(activity.organisation, activity)
+
+        assert_all_edit_links_go_to_the_correct_form_step(activity: activity)
+      end
+    end
+
+    context "when the activity is incomplete" do
+      it "editing and saving a step goes to the next step in the form" do
+        activity = create(:fund_activity, :at_country_step, organisation: user.organisation)
+        recipient_region = "Oceania, regional"
+
+        visit organisation_activity_path(activity.organisation, activity)
+
+        within(".recipient_region") do
+          click_on(I18n.t("generic.link.edit"))
+        end
+
+        select recipient_region, from: "activity[recipient_region]"
+        click_button I18n.t("form.activity.submit")
+
+        expect(page).to have_content I18n.t("page_title.activity_form.show.flow")
+        expect(page).not_to have_content activity.title
+      end
+    end
+
+    context "when a title attribute is present" do
+      it "the call to action is 'Edit'" do
+        activity = create(:fund_activity, organisation: user.organisation, wizard_status: :sector)
+
+        visit organisation_activity_path(activity.organisation, activity)
+
+        within(".title") do
+          expect(page).to have_content(I18n.t("generic.link.edit"))
+        end
+      end
+    end
+
+    context "when an activity attribute is not present" do
+      it "the call to action is 'Add'" do
+        activity = create(:fund_activity, :at_purpose_step, organisation: user.organisation)
+
+        visit organisation_activity_path(activity.organisation, activity)
+
+        within(".title") do
+          expect(page).to have_content(I18n.t("generic.link.add"))
+        end
       end
     end
   end
--- spec/policies/activity_policy_spec.rb
+++ spec/policies/activity_policy_spec.rb
@@ -3,21 +3,67 @@ require "rails_helper"
 RSpec.describe ActivityPolicy do
   subject { described_class.new(user, activity) }
 
-  let(:organisation) { create(:organisation) }
-  let(:activity) { create(:activity, organisation: organisation) }
+  context "when the user belongs to BEIS" do
+    let(:user) { build_stubbed(:beis_user) }
 
-  context "as an administrator" do
-    let(:user) { build_stubbed(:administrator) }
+    context "when the activity is a fund" do
+      let(:activity) { create(:fund_activity, organisation: user.organisation) }
 
-    it { is_expected.to permit_action(:index) }
-    it { is_expected.to permit_action(:show) }
-    it { is_expected.to permit_new_and_create_actions }
-    it { is_expected.to permit_edit_and_update_actions }
-    it { is_expected.to permit_action(:destroy) }
+      it { is_expected.to permit_action(:show) }
+      it { is_expected.to permit_new_and_create_actions }
+      it { is_expected.to permit_edit_and_update_actions }
+      it { is_expected.to forbid_action(:destroy) }
 
-    it "includes activity in resolved scope" do
-      resolved_scope = described_class::Scope.new(user, Activity.all).resolve
-      expect(resolved_scope).to include(activity)
+      it "includes activity in resolved scope" do
+        resolved_scope = described_class::Scope.new(user, Activity.all).resolve
+        expect(resolved_scope).to include(activity)
+      end
+    end
+
+    context "when the activity is a programme" do
+      let(:activity) { create(:programme_activity, organisation: user.organisation) }
+
+      it { is_expected.to permit_action(:show) }
+      it { is_expected.to permit_new_and_create_actions }
+      it { is_expected.to permit_edit_and_update_actions }
+      it { is_expected.to forbid_action(:destroy) }
+
+      it "includes activity in resolved scope" do
+        resolved_scope = described_class::Scope.new(user, Activity.all).resolve
+        expect(resolved_scope).to include(activity)
+      end
+    end
+
+    context "when the user does NOT belong to BEIS" do
+      let(:user) { build_stubbed(:delivery_partner_user) }
+
+      context "when the activity is a fund" do
+        let(:activity) { create(:fund_activity, organisation: user.organisation) }
+
+        it { is_expected.to forbid_action(:show) }
+        it { is_expected.to forbid_new_and_create_actions }
+        it { is_expected.to forbid_edit_and_update_actions }
+        it { is_expected.to forbid_action(:destroy) }
+
+        it "includes does not include the activity in resolved scope" do
+          resolved_scope = described_class::Scope.new(user, Activity.all).resolve
+          expect(resolved_scope).to be_empty
+        end
+      end
+
+      context "when the activity is a programme" do
+        let(:activity) { create(:programme_activity, organisation: user.organisation) }
+
+        it { is_expected.to permit_action(:show) }
+        it { is_expected.to forbid_new_and_create_actions }
+        it { is_expected.to forbid_edit_and_update_actions }
+        it { is_expected.to forbid_action(:destroy) }
+
+        it "includes activity in resolved scope" do
+          resolved_scope = described_class::Scope.new(user, Activity.all).resolve
+          expect(resolved_scope).to include(activity)
+        end
+      end
     end
   end
 end
