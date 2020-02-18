a3cc4ce44cd2c2af7dca7bdbc4584d74880d4a32
Tom Hipkin <tomhipkin@gmail.com>
2020-02-18 11:54:16 +0000

    Delivery partners see programmes on their home page
    * Fund managers do not see this list of programmes, they have to click through to funds first in order to create programmes. They could have a list of funds and programmes but that would be a change in user experience that isn't the subject of this work.
    * For now allow all users to see all programmes. We know this list won't make sense to all users but it should be good enough for a single private beta user to use the service with. I imagine we would want to filter this list of programmes down by those that have been allocated to them through the 'extending organisation' flow in the future.

---
+++ spec/policies/programme_policy_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe ProgrammePolicy do
+  subject { described_class.new(user, activity) }
+
+  let(:organisation) { create(:organisation) }
+  let(:activity) { create(:programme_activity, organisation: organisation) }
+
+  context "as a user that belongs to BEIS" do
+    let(:user) { build_stubbed(:beis_user) }
+
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_new_and_create_actions }
+    it { is_expected.to permit_edit_and_update_actions }
+    it { is_expected.to permit_action(:destroy) }
+
+    it "includes activity in resolved scope" do
+      resolved_scope = described_class::Scope.new(user, Activity.all).resolve
+      expect(resolved_scope).to include(activity)
+    end
+  end
+
+  context "as a user that does NOT belong to BEIS" do
+    let(:user) { build_stubbed(:delivery_partner_user) }
+
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to forbid_edit_and_update_actions }
+    it { is_expected.to forbid_action(:destroy) }
+
+    it "includes activity in resolved scope" do
+      resolved_scope = described_class::Scope.new(user, Activity.all).resolve
+      expect(resolved_scope).to eq(Activity.all)
+    end
+  end
+end
