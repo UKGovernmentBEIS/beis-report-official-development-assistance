a8934ac674f5ad0254ade0d7fe3d084e929eee23
Tom Hipkin <tomhipkin@gmail.com>
2020-02-24 17:26:01 +0000

    Updating a budget happens in a dedicated and tested service

---
+++ app/services/update_budget.rb
@@ -0,0 +1,26 @@
+class UpdateBudget
+  attr_accessor :budget
+
+  def initialize(budget:)
+    self.budget = budget
+  end
+
+  def call(attributes: {})
+    budget.assign_attributes(attributes)
+    budget.value = sanitize_monetary_string(value: attributes[:value])
+
+    result = if budget.valid?
+      Result.new(budget.save, budget)
+    else
+      Result.new(false, budget)
+    end
+
+    result
+  end
+
+  private
+
+  def sanitize_monetary_string(value:)
+    Monetize.parse(value)
+  end
+end
