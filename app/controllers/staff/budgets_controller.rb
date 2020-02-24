49468e57d6ba588777676bf741ee1fe936c62dbe
Tom Hipkin <tomhipkin@gmail.com>
2020-02-24 17:26:01 +0000

    Updating a budget happens in a dedicated and tested service

--- app/controllers/staff/budgets_controller.rb
+++ app/controllers/staff/budgets_controller.rb
@@ -34,16 +34,13 @@ class Staff::BudgetsController < Staff::BaseController
 
   def update
     @budget = Budget.find(id)
-    @activity = Activity.find(activity_id)
-    @budget.activity = @activity
     authorize @budget
 
-    @budget.assign_attributes(budget_params)
-    @budget.value = monetary_value
-    @budget.period_start_date = format_date(period_start_date)
-    @budget.period_end_date = format_date(period_end_date)
+    @activity = Activity.find(activity_id)
+    result = UpdateBudget.new(budget: @budget)
+      .call(attributes: budget_params)
 
-    if @budget.save
+    if result.success?
       flash[:notice] = I18n.t("form.budget.update.success")
       redirect_to organisation_activity_path(@activity.organisation, @activity)
     else
@@ -70,22 +67,5 @@ class Staff::BudgetsController < Staff::BaseController
       :period_end_date,
       :currency
     )
-   end
-
-  def monetary_value
-    @monetary_value ||= begin
-                          string_value = params.require(:budget).permit(:value)
-                          Monetize.parse(string_value).to_f
-                        end
-  end
-
-  def period_start_date
-    date_fields = params.require(:budget).permit("period_start_date(3i)", "period_start_date(2i)", "period_start_date(1i)")
-    {day: date_fields["period_start_date(3i)"], month: date_fields["period_start_date(2i)"], year: date_fields["period_start_date(1i)"]}
-  end
-
-  def period_end_date
-    date_fields = params.require(:budget).permit("period_end_date(3i)", "period_end_date(2i)", "period_end_date(1i)")
-    {day: date_fields["period_end_date(3i)"], month: date_fields["period_end_date(2i)"], year: date_fields["period_end_date(1i)"]}
   end
 end
---
+++ spec/services/update_budget_spec.rb
@@ -0,0 +1,108 @@
+require "rails_helper"
+
+RSpec.describe UpdateBudget do
+  let(:budget) { create(:budget) }
+
+  describe "#call" do
+    it "returns a successful result" do
+      allow(budget).to receive(:valid?).and_return(true)
+      allow(budget).to receive(:save).and_return(true)
+
+      result = described_class.new(budget: budget).call(attributes: {})
+
+      expect(result.success?).to be true
+    end
+
+    context "when the budget isn't valid" do
+      it "returns a failed result" do
+        allow(budget).to receive(:valid?).and_return(false)
+
+        result = described_class.new(budget: budget).call(attributes: {})
+
+        expect(result.success?).to be false
+      end
+    end
+
+    context "when attributes are passed in" do
+      it "sets the attributes passed in as budget attributes" do
+        attributes = ActionController::Parameters.new(status: "foo").permit!
+
+        result = described_class.new(budget: budget).call(attributes: attributes)
+
+        expect(result.object.status).to eq("foo")
+      end
+    end
+
+    context "when unknown attributes are passed in" do
+      it "raises an error" do
+        attributes = ActionController::Parameters.new(foo: "bar").permit!
+
+        expect { described_class.new(budget: budget).call(attributes: attributes) }
+          .to raise_error(ActiveModel::UnknownAttributeError)
+      end
+    end
+
+    context "when a value is passed in" do
+      context "and that value contains alphabetical characters" do
+        it "sets a value without these characters" do
+          attributes = ActionController::Parameters.new(value: "abc 123.00 xyz").permit!
+
+          result = described_class.new(budget: budget).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("123.00"))
+        end
+      end
+
+      context "and that value contains currency characters" do
+        it "sets a value without these characters" do
+          attributes = ActionController::Parameters.new(value: "£123.00").permit!
+
+          result = described_class.new(budget: budget).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("123.00"))
+        end
+      end
+
+      context "and that value contains commas" do
+        it "sets a value without these characters" do
+          attributes = ActionController::Parameters.new(value: "1,230.90").permit!
+
+          result = described_class.new(budget: budget).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("1230.90"))
+        end
+      end
+
+      context "and that value contains a single decimal place" do
+        it "sets a value to 1 decimal place and omits the trailing zero" do
+          attributes = ActionController::Parameters.new(value: "1.1").permit!
+
+          result = described_class.new(budget: budget).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("1.1"))
+          expect(result.object.value).to eq(BigDecimal("1.10"))
+        end
+      end
+
+      context "and that value contains 2 decimal places" do
+        it "sets a value to 2 decimal places" do
+          attributes = ActionController::Parameters.new(value: "1.11").permit!
+
+          result = described_class.new(budget: budget).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("1.11"))
+        end
+      end
+
+      context "and that value contains more than 2 decimal places" do
+        it "rounds the value back up to 2 decimal places" do
+          attributes = ActionController::Parameters.new(value: "1.115").permit!
+
+          result = described_class.new(budget: budget).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("1.12"))
+        end
+      end
+    end
+  end
+end
