950fc8055266ed2f9d85a8c6bd61e1dcc3fc8857
Tom Hipkin <tomhipkin@gmail.com>
2020-02-24 13:18:02 +0000

    Refactor transaction creation into a service
    * Centralising the money and date formatting logic used in the controller will make it easier to reuse this code with budget creation
    * I found we could remove the custom date formatting we had since assigning ActionController::Parameters straight to an object will help us by translating the mapping of date(3i), date(2i) etc to the correct date field
    * There is some feature test duplication after this however since the financial values are such a core part of the service, and a core piece of information to get correct, I think there is value in making sure the user sees the correct error message in the edge cases.
    * Change link to use transaction.activity_id instead of .activity as Bullet detected eager loading. Not sure which part of this refactor tripped the need for this but I'm happy to trust in Bullet to keep things optimised
    * Remove unnessary .to_f which was chomping down the value before Rails was doing the same through it's decimal data field with a scale set to 2.

---
+++ spec/services/create_transaction_spec.rb
@@ -0,0 +1,113 @@
+require "rails_helper"
+
+RSpec.describe CreateTransaction do
+  let(:activity) { create(:activity) }
+
+  describe "#call" do
+    it "sets the activity as the one this transaction belongs to" do
+      result = described_class.new(activity: activity).call
+      expect(result.object.activity).to eq(activity)
+    end
+
+    it "returns a successful result" do
+      allow_any_instance_of(Transaction).to receive(:valid?).and_return(true)
+      allow_any_instance_of(Transaction).to receive(:save).and_return(true)
+
+      result = described_class.new(activity: activity).call(attributes: {})
+
+      expect(result.success?).to be true
+    end
+
+    context "when the transaction isn't valid" do
+      it "returns a failed result" do
+        allow_any_instance_of(Transaction).to receive(:valid?).and_return(false)
+
+        result = described_class.new(activity: activity).call(attributes: {})
+
+        expect(result.success?).to be false
+      end
+    end
+
+    context "when attributes are passed in" do
+      it "sets the attributes passed in as transaction attributes" do
+        attributes = ActionController::Parameters.new(reference: "foo").permit!
+
+        result = described_class.new(activity: activity).call(attributes: attributes)
+
+        expect(result.object.reference).to eq("foo")
+      end
+    end
+
+    context "when unknown attributes are passed in" do
+      it "raises an error" do
+        attributes = ActionController::Parameters.new(foo: "bar").permit!
+
+        expect { described_class.new(activity: activity).call(attributes: attributes) }
+          .to raise_error(ActiveModel::UnknownAttributeError)
+      end
+    end
+
+    context "when a value is passed in" do
+      context "and that value contains alphabetical characters" do
+        it "sets a value without these characters" do
+          attributes = ActionController::Parameters.new(value: "abc 123.00 xyz").permit!
+
+          result = described_class.new(activity: activity).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("123.00"))
+        end
+      end
+
+      context "and that value contains currency characters" do
+        it "sets a value without these characters" do
+          attributes = ActionController::Parameters.new(value: "£123.00").permit!
+
+          result = described_class.new(activity: activity).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("123.00"))
+        end
+      end
+
+      context "and that value contains commas" do
+        it "sets a value without these characters" do
+          attributes = ActionController::Parameters.new(value: "1,230.90").permit!
+
+          result = described_class.new(activity: activity).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("1230.90"))
+        end
+      end
+
+      context "and that value contains a single decimal place" do
+        it "sets a value to 1 decimal place and omits the trailing zero" do
+          attributes = ActionController::Parameters.new(value: "1.1").permit!
+
+          result = described_class.new(activity: activity).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("1.1"))
+          expect(result.object.value).to eq(BigDecimal("1.10"))
+        end
+      end
+
+      context "and that value contains 2 decimal places" do
+        it "sets a value to 2 decimal places" do
+          attributes = ActionController::Parameters.new(value: "1.11").permit!
+
+          result = described_class.new(activity: activity).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("1.11"))
+        end
+      end
+
+      context "and that value contains more than 2 decimal places" do
+        it "rounds the value back up to 2 decimal places" do
+          attributes = ActionController::Parameters.new(value: "1.115").permit!
+
+          result = described_class.new(activity: activity).call(attributes: attributes)
+
+          expect(result.object.value).to eq(BigDecimal("1.12"))
+        end
+      end
+    end
+  end
+end
