43c057c0307c5469a69a35ada5401747b7f33884
bob walker <bob@dxw.com>
2020-03-03 10:40:44 +0000

    Add terraform to deploy the worker
    This is being deployed as a separate cloudfoundry app due to limitations
    in the cloudfoundry provider.
    Ideally this would be a worker process of the main app.

---
+++ terraform/worker.tf
@@ -0,0 +1,29 @@
+resource "cloudfoundry_app" "beis-roda-worker" {
+  name              = "beis-roda-${var.environment}-worker"
+  space             = cloudfoundry_space.space.id
+  instances         = 1
+  strategy          = "blue-green-v2"
+  command           = "bundle exec sidekiq"
+  health_check_type = "none"
+  memory            = 1024
+  disk_quota        = 3072
+  timeout           = 120
+  docker_image      = "thedxw/beis-report-official-development-assistance:${var.docker_image}"
+  service_binding { service_instance = cloudfoundry_service_instance.beis-roda-redis.id }
+  service_binding { service_instance = cloudfoundry_service_instance.beis-roda-postgres.id }
+  service_binding { service_instance = cloudfoundry_user_provided_service.papertrail.id }
+  environment = {
+    "RAILS_LOG_TO_STDOUT"           = "true"
+    "RAILS_SERVE_STATIC_FILES"      = "enabled"
+    "RAILS_ENV"                     = "production"
+    "SECRET_KEY_BASE"               = var.secret_key_base
+    "DOMAIN"                        = "https://beis-roda-${var.environment}.london.cloudapps.digital"
+    "AUTH0_CLIENT_ID"               = var.auth0_client_id
+    "AUTH0_CLIENT_SECRET"           = var.auth0_client_secret
+    "AUTH0_DOMAIN"                  = var.auth0_domain
+    "NOTIFY_KEY"                    = var.notify_key
+    "NOTIFY_WELCOME_EMAIL_TEMPLATE" = var.notify_welcome_email_template
+    "ROLLBAR_ENV"                   = "paas-${var.environment}"
+    "ROLLBAR_ACCESS_TOKEN"          = var.rollbar_access_token
+  }
+}
