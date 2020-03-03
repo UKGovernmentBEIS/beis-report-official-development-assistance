1fdf3806be79093fe75d80acccb9f38fddeebca0
Laura Porter <laura@dxw.com>
2020-03-03 14:51:24 +0000

    BEIS users can mark users as inactive
    Inactive users cannot log in.
    
    BEIS users can see a summary of which users are active or inactive. They can
    toggle the user as active/inactive on the user#edit page.

--- spec/features/staff/beis_users_can_view_other_users_spec.rb
+++ spec/features/staff/beis_users_can_view_other_users_spec.rb
@@ -15,7 +15,7 @@ RSpec.feature "BEIS users can can view other users" do
     authenticate!(user: user)
   end
 
-  scenario "a user can be viewed" do
+  scenario "an active user can be viewed" do
     another_user = create(:administrator)
 
     # Navigate from the landing page
@@ -26,6 +26,7 @@ RSpec.feature "BEIS users can can view other users" do
     expect(page).to have_content(I18n.t("page_title.users.index"))
     expect(page).to have_content(another_user.name)
     expect(page).to have_content(another_user.email)
+    expect(page).to have_content(I18n.t("user.active.#{another_user.active}"))
 
     # Navigate to the individual user page
     within(".users") do
@@ -35,6 +36,24 @@ RSpec.feature "BEIS users can can view other users" do
     expect(page).to have_content(I18n.t("page_title.users.show"))
     expect(page).to have_content(another_user.name)
     expect(page).to have_content(another_user.email)
+  end
+
+  scenario "an inactive user can be viewed" do
+    another_user = create(:inactive_user)
+
+    # Navigate from the landing page
+    visit organisation_path(user.organisation)
+    click_on(I18n.t("page_content.dashboard.button.manage_users"))
+
+    # The details include whether the user is active
+    expect(page).to have_content(I18n.t("user.active.#{another_user.active}"))
+
+    # Navigate to the individual user page
+    within(".users") do
+      find("tr", text: another_user.name).click_link("Show")
+    end
+
+    expect(page).to have_content(I18n.t("user.active.#{another_user.active}"))
   end
 
   scenario "can go back to the previous page" do
