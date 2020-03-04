3e84d479e3aef5d34919f5b9b3d25d514012fe36
Laura Porter <laura@dxw.com>
2020-03-04 12:20:06 +0000

    Use Yes/No for active status and Activate/Deactivate for form
    After discussion with our content designer we have decided to amend the text slightly:
    
    - User active status is now displayed as Yes/No
    - The CTA on the form is Activate/Deactivate user

--- spec/features/staff/beis_users_can_view_other_users_spec.rb
+++ spec/features/staff/beis_users_can_view_other_users_spec.rb
@@ -26,7 +26,7 @@ RSpec.feature "BEIS users can can view other users" do
     expect(page).to have_content(I18n.t("page_title.users.index"))
     expect(page).to have_content(another_user.name)
     expect(page).to have_content(another_user.email)
-    expect(page).to have_content(I18n.t("user.active.#{another_user.active}"))
+    expect(page).to have_content(I18n.t("form.user.active.true"))
 
     # Navigate to the individual user page
     within(".users") do
@@ -46,14 +46,14 @@ RSpec.feature "BEIS users can can view other users" do
     click_on(I18n.t("page_content.dashboard.button.manage_users"))
 
     # The details include whether the user is active
-    expect(page).to have_content(I18n.t("user.active.#{another_user.active}"))
+    expect(page).to have_content(I18n.t("form.user.active.false"))
 
     # Navigate to the individual user page
     within(".users") do
       find("tr", text: another_user.name).click_link("Show")
     end
 
-    expect(page).to have_content(I18n.t("user.active.#{another_user.active}"))
+    expect(page).to have_content(I18n.t("form.user.active.false"))
   end
 
   scenario "can go back to the previous page" do
