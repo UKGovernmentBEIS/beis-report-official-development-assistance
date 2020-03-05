fe5ae449208f7adfe06230507323046b2dd18b85
Laura Porter <laura@dxw.com>
2020-03-05 13:09:51 +0000

    Group & sort users on users#index page by their organisation name

--- spec/features/staff/beis_users_can_view_other_users_spec.rb
+++ spec/features/staff/beis_users_can_view_other_users_spec.rb
@@ -39,6 +39,30 @@ RSpec.feature "BEIS users can can view other users" do
     expect(page).to have_content(another_user.email)
   end
 
+  scenario "users are grouped by their organisation name in alphabetical order" do
+    a_organisation = create(:organisation, name: "A Organisation")
+    b_organisation = create(:organisation, name: "B Organisation")
+
+    a1_user = create(:administrator, organisation: a_organisation)
+    a2_user = create(:administrator, organisation: a_organisation)
+    b1_user = create(:administrator, organisation: b_organisation)
+    b2_user = create(:administrator, organisation: b_organisation)
+
+    # Navigate from the landing page
+    visit organisation_path(user.organisation)
+    click_on(I18n.t("page_content.dashboard.button.manage_users"))
+
+    expected_array = [
+      a1_user.organisation.name,
+      a2_user.organisation.name,
+      b1_user.organisation.name,
+      b2_user.organisation.name,
+      user.organisation.name,
+    ].sort
+
+    expect(page.all("td.organisation").collect(&:text)).to match_array(expected_array)
+  end
+
   scenario "an inactive user can be viewed" do
     another_user = create(:inactive_user)
 
