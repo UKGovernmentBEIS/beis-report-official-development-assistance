a4bba660f597dbf89ec024218595197dc5801c0e
Laura Porter <laura@dxw.com>
2020-03-05 14:57:49 +0000

    Allow BEIS users to view projects (but not create them)
    So that BEIS users can download the XML for a project, they will need read-only
    access to the project data

--- spec/features/staff/users_can_view_a_project_spec.rb
+++ spec/features/staff/users_can_view_a_project_spec.rb
@@ -33,4 +33,31 @@ RSpec.feature "Users can view a project" do
       end
     end
   end
+
+  context "when the user belongs to BEIS" do
+    let(:user) { create(:beis_user) }
+    before { authenticate!(user: user) }
+
+    scenario "can view a project but not create one" do
+      project = create(:project_activity)
+
+      visit organisation_activity_path(project.organisation, project)
+
+      expect(page).to have_content project.title
+      expect(page).to_not have_content I18n.t("page_content.organisation.button.create_project")
+    end
+
+    scenario "can download a project as XML" do
+      fund = create(:fund_activity)
+      programme = create(:programme_activity)
+      fund.activities << programme
+      project = create(:project_activity, organisation: user.organisation)
+      programme.activities << project
+
+      visit organisation_activity_path(project.organisation, project)
+
+      expect(page).to have_content project.title
+      expect(page).to_not have_content I18n.t("page_content.organisation.button.create_project")
+    end
+  end
 end
