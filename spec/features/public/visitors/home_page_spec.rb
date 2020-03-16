232785b76bd7ae508c1dee6a1bebf9037ea0f536
meyric <meyric@dxw.com>
2020-03-16 14:51:01 +0000

    Fix root path behaviour for signed in users
    Users who are signed in and visit the root path should be redirected to
    their organisation page and not see an error.

--- spec/features/public/visitors/home_page_spec.rb
+++ spec/features/public/visitors/home_page_spec.rb
@@ -3,4 +3,34 @@ feature "Home page" do
     visit root_path
     expect(page).to have_content(I18n.t("app.title"))
   end
+
+  context "when signed in as a BEIS user" do
+    let(:user) { create(:beis_user) }
+    before { authenticate!(user: user) }
+
+    scenario "they are redirected to their organisation show page" do
+      visit root_path
+      expect(page.current_path).to eq organisation_path(user.organisation)
+    end
+  end
+
+  context "when signed in as a delivery partner user" do
+    let(:user) { create(:delivery_partner_user) }
+    before { authenticate!(user: user) }
+
+    scenario "they are redirected to their organisation show page" do
+      visit root_path
+      expect(page.current_path).to eq organisation_path(user.organisation)
+    end
+  end
+
+  context "when signed in as a user who is not active" do
+    let(:user) { create(:delivery_partner_user, active: false) }
+    before { authenticate!(user: user) }
+
+    scenario "they are shown the start page" do
+      visit root_path
+      expect(page).to have_button(I18n.t("generic.link.sign_in"))
+    end
+  end
 end
