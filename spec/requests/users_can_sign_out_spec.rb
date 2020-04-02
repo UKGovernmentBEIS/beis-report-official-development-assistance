3418f1fe28f7d48450cabaa18bf364c66c352b14
meyric <meyric@dxw.com>
2020-04-02 12:53:53 +0100

    CI and local tests use the same env vars
    This work came out of seeing the tests fail in CI with seed 10466,
    running the same seed locally, the tests passed.
    
    Trying to figure out what was different it became apparent that in CI
    the `.env.example` variables are loaded and locally the `.env.test`
    variables are loaded.
    
    One of the differences was that REDIS_URL is not defined in .env.test
    whereas it is in .env.example.
    
    Adding the REDIS_URL to the .env.test file results in seed 10466 failing
    locally, which confirms the issue.
    
    This could be fixed two ways but the decision was made to change CI to
    use the .env.test variables and add the REDIS_URL in. This means
    developers will need to run redis locally now in order to run the test
    suite, but also means CI and the local tests run in the same environment
    - this feels like a good trade-off.
    
    With both local and CI test failing the same the actual failing test was
    examined:
    
    `spec/requests/users_can_sign_out_spec.rb`
    
    This failure seems to relate to test order as the spec will pass with
    other seeds. Seed 10466 runs this spec first.
    
    This looks to be the only spec that stubs the env variables.
    
    Stubbing the entire ENV means that other parts of the code that might
    use the ENV will fail, and indeed this was the case.
    
    As the the Code Climate gem is already used, it make sense to use it
    here to only stub the env variables needed for the test rather than the
    whole thing.

--- spec/requests/users_can_sign_out_spec.rb
+++ spec/requests/users_can_sign_out_spec.rb
@@ -2,14 +2,12 @@ require "rails_helper"
 
 RSpec.describe "signing out of Auth0", type: :request do
   scenario "success" do
-    allow(ENV).to receive(:[]).with("BULLET_DEBUG").and_return("false")
-    allow(ENV).to receive(:[]).with("AUTH0_CLIENT_ID").and_return("123456")
-    allow(ENV).to receive(:[]).with("AUTH0_DOMAIN").and_return("test.auth0")
-    host! "test.local"
-    mock_successful_authentication
+    ClimateControl.modify AUTH0_DOMAIN: "test.auth0", AUTH0_CLIENT_ID: "123456", BULLET_DEBUG: "false" do
+      host! "test.local"
+      mock_successful_authentication
+      get "/sign_out"
 
-    get "/sign_out"
-
-    expect(request).to redirect_to("https://test.auth0/v2/logout?returnTo=http%3A%2F%2Ftest.local%2F&client_id=123456")
+      expect(request).to redirect_to("https://test.auth0/v2/logout?returnTo=http%3A%2F%2Ftest.local%2F&client_id=123456")
+    end
   end
 end
