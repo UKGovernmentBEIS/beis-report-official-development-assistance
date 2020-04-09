cd87098f8fe50fdd33bdd0b610b227850b999a72
Tom Hipkin <tomhipkin@gmail.com>
2020-04-09 17:21:11 +0100

    Run data migrations on start before schema migrations
    We found that deploying a batch of code to production that the master branch was much further behind in terms of outstanding schema changes. This meant that a data migration that required a much earlier schema change worked locally and on staging, but failed on production.
    
    As schema changes should include the required and relevant data changes we think we can switch the ordering.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,13 +5,13 @@ echo "Starting docker entrypoint…"
 
 setup_database()
 {
-  echo "Preparing database…"
-  bundle exec rake db:prepare
-  echo "Finished database setup."
-
   echo "Running data migrations…"
   bundle exec rake data:migrate
   echo "Finished running data migrations."
+
+  echo "Preparing database…"
+  bundle exec rake db:prepare
+  echo "Finished database setup."
 }
 
 if [ -z ${DATABASE_URL+x} ]; then echo "Skipping database setup"; else setup_database; fi
