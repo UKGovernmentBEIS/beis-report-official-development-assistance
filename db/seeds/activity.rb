f18b9b2012c315916349d9d576555e57b8feb17c
Maria Herrera <maria@dxw.com>
2020-04-14 16:13:18 +0100

    Seeds file creates valid activities
    While working on a different feature, I noticed our seeds file does not create
    activities that pass the IATI validator, as they were missing important bits of
    information in regards to extending organisations and reference numbers.
    
    This commit introduces some changes to the activity seeds file, where now all the
    activities created will pass the IATI public validator. In addition to a fund,
    I have created two programmes that belong to the fund, and one project that belongs
    to one of the programme. This should help us to test the app faster, since we do
    not have to create programmes manually every time.

--- db/seeds/activity.rb
+++ db/seeds/activity.rb
@@ -1,3 +1,33 @@
 beis = Organisation.find_by(service_owner: true)
-activity_params = FactoryBot.build(:activity, title: "GCRF", organisation: beis).attributes
-Activity.find_or_create_by(activity_params)
+
+fund_params = FactoryBot.build(:fund_activity,
+  title: "GCRF",
+  organisation: beis).attributes
+
+fund = Activity.find_or_create_by(fund_params)
+
+delivery_partner = Organisation.find_by!(service_owner: false)
+
+first_programme_params = FactoryBot.build(:programme_activity,
+  title: "International Partnerships",
+  organisation: beis,
+  activity: fund,
+  extending_organisation: delivery_partner).attributes
+
+programme = Activity.find_or_create_by(first_programme_params)
+
+second_programme_params = FactoryBot.build(:programme_activity,
+  title: "Africa Catalyst Programme",
+  organisation: beis,
+  activity: fund,
+  extending_organisation: delivery_partner).attributes
+
+Activity.find_or_create_by(second_programme_params)
+
+project_params = FactoryBot.build(:project_activity,
+  title: "Airbus Flood and Drought",
+  organisation: delivery_partner,
+  activity: programme,
+  extending_organisation: delivery_partner).attributes
+
+Activity.find_or_create_by(project_params)
