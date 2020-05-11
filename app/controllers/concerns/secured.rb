048c0fd2ffde499a483ed78f7d08ac1b4f6afa43
Tom Hipkin <tomhipkin@gmail.com>
2020-05-11 16:04:28 +0100

    Temporarily log extra information when the session userinfo is missing
    This is only designed to help us debug an issue on the pentest environment.

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -6,6 +6,17 @@ module Secured
   end
 
   def logged_in_using_omniauth?
+    # DEBUG: Session info can be missing
+    if Rails.env.production? && session[:userinfo].blank?
+      # Is there a session object?
+      Rails.logger.info(session)
+
+      # Are there any keys?
+      Rails.logger.info(session.keys)
+
+      # Is Redis available as a session_store?
+      Rails.logger.info(Redis.new(url: ENV["REDIS_URL"]).ping)
+    end
     redirect_to "/" if session[:userinfo].blank?
   end
 end
