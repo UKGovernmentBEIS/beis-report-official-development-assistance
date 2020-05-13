64428c89b643d010363f08890bb2b4ed3f37659d
Laura Porter <laura@dxw.com>
2020-05-13 15:02:20 +0100

    Add download? policy to OrganisationPolicy
    Only BEIS users may download XML of an organisation (and all its Activities)

--- spec/policies/organisation_policy_spec.rb
+++ spec/policies/organisation_policy_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe OrganisationPolicy do
     it { is_expected.to permit_new_and_create_actions }
     it { is_expected.to permit_edit_and_update_actions }
     it { is_expected.to permit_action(:destroy) }
+    it { is_expected.to permit_action(:download) }
   end
 
   context "as user that does NOT belong to BEIS" do
@@ -24,6 +25,7 @@ RSpec.describe OrganisationPolicy do
       it { is_expected.to forbid_new_and_create_actions }
       it { is_expected.to permit_edit_and_update_actions }
       it { is_expected.to forbid_action(:destroy) }
+      it { is_expected.to forbid_action(:download) }
     end
 
     context "when the user does NOT belong to that organisation" do
@@ -34,6 +36,7 @@ RSpec.describe OrganisationPolicy do
       it { is_expected.to forbid_new_and_create_actions }
       it { is_expected.to forbid_edit_and_update_actions }
       it { is_expected.to forbid_action(:destroy) }
+      it { is_expected.to forbid_action(:download) }
     end
   end
 end
