2d2976a973010177f511f6859b70142b05c19313
Tom Hipkin <tomhipkin@gmail.com>
2020-05-14 17:19:19 +0100

    Redis is required when the application boots
    * We found that live environments were still  using the cookie store for sessions rather than the intended redis store.
    * The application is initialised during the docker build in order to generates assets. Redis is not strictly required for this to succeed.
    * The previous implementation was allowing redis url to be omitted to allow the builds to run. This was effective but caused the unexpected result that we weren't aware that there was a redis problem when setting the session store. Set a dummy redis url in the dockerbuild for generating assets only.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,13 +1,4 @@
 redis_url = ENV.fetch("REDIS_URL", nil)
-if redis_url.nil?
-  # If REDIS_URL is unexpectedly unavailable Rails will use the default
-  # cookie_store. We declare this explicity so the fallback is clear.
-  Rails.application.config.session_store :cookie_store,
-    key: "_roda_session",
-    expire_after: 12.hours
-  return
-end
-
 redis_uri = URI(redis_url)
 redis_store_params = {
   servers: {
@@ -21,5 +12,4 @@ redis_store_params = {
   threadsafe: true,
 }
 redis_store_params[:secure] = true if Rails.env.production?
-
 Rails.application.config.session_store :redis_store, redis_store_params
