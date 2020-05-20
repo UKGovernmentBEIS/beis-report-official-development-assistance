5894bc6dfca1c33fb0f968e4d42d9c6f604ba4a2
meyric <meyric@dxw.com>
2020-05-20 12:46:42 +0100

    Pre-fill planned disbursement providing org
    User research showed that users did not understand why they have to
    enter details that the service should already 'know'.
    
    Now that an Activity can derive it's own providing organisation we can
    pre-fill the planned disbursement form with the details of the correct
    organisation, aligning with users expectations.
    
    If there is a case we do not yet know about, the user can change the
    providing organisation details and save the planned disbursement.

--- spec/features/staff/users_can_create_a_planned_disbursement_spec.rb
+++ spec/features/staff/users_can_create_a_planned_disbursement_spec.rb
@@ -33,6 +33,76 @@ RSpec.describe "Users can create a planned disbursement" do
       expect(page).to have_current_path organisation_activity_path(user.organisation, project)
       expect(page).to have_content I18n.t("form.planned_disbursement.create.success")
     end
+
+    context "when the delivery partner is a government organisation" do
+      context "and the activity is a project" do
+        it "pre fills the providing organisation details with those of BEIS" do
+          beis = create(:beis_organisation)
+          government_devlivery_partner = create(:delivery_partner_organisation, organisation_type: "10")
+          user.update(organisation: government_devlivery_partner)
+          project = create(:project_activity, organisation: user.organisation)
+
+          visit organisation_path(user.organisation)
+          click_on project.title
+          click_on I18n.t("page_content.planned_disbursements.button.create")
+
+          expect(page).to have_field(I18n.t("activerecord.attributes.planned_disbursement.providing_organisation_name"), with: beis.name)
+          expect(page).to have_field(I18n.t("activerecord.attributes.planned_disbursement.providing_organisation_type"), with: beis.organisation_type)
+          expect(page).to have_field(I18n.t("form.planned_disbursement.providing_organisation_reference.label"), with: beis.iati_reference)
+        end
+      end
+
+      context "and the activity is a third-party project" do
+        it "pre fills the providing organisation details with those of BEIS" do
+          beis = create(:beis_organisation)
+          government_devlivery_partner = create(:delivery_partner_organisation, organisation_type: "10")
+          user.update(organisation: government_devlivery_partner)
+          project = create(:third_party_project_activity, organisation: user.organisation)
+
+          visit organisation_path(user.organisation)
+          click_on project.title
+          click_on I18n.t("page_content.planned_disbursements.button.create")
+
+          expect(page).to have_field(I18n.t("activerecord.attributes.planned_disbursement.providing_organisation_name"), with: beis.name)
+          expect(page).to have_field(I18n.t("activerecord.attributes.planned_disbursement.providing_organisation_type"), with: beis.organisation_type)
+          expect(page).to have_field(I18n.t("form.planned_disbursement.providing_organisation_reference.label"), with: beis.iati_reference)
+        end
+      end
+    end
+
+    context "when the delivery partner is a non-government organisation" do
+      context "and the activity is a project" do
+        it "pre fills the providing organisation details with those of BEIS" do
+          beis = create(:beis_organisation)
+          government_devlivery_partner = create(:delivery_partner_organisation, organisation_type: "10")
+          user.update(organisation: government_devlivery_partner)
+          project = create(:project_activity, organisation: user.organisation)
+
+          visit organisation_path(user.organisation)
+          click_on project.title
+          click_on I18n.t("page_content.planned_disbursements.button.create")
+
+          expect(page).to have_field(I18n.t("activerecord.attributes.planned_disbursement.providing_organisation_name"), with: beis.name)
+          expect(page).to have_field(I18n.t("activerecord.attributes.planned_disbursement.providing_organisation_type"), with: beis.organisation_type)
+          expect(page).to have_field(I18n.t("form.planned_disbursement.providing_organisation_reference.label"), with: beis.iati_reference)
+        end
+      end
+      context "and the activity is a third-party project" do
+        it "pre fills the providing organisation detauls with those of the delivery partner" do
+          non_government_devlivery_partner = create(:delivery_partner_organisation, organisation_type: "22")
+          user.update(organisation: non_government_devlivery_partner)
+          project = create(:third_party_project_activity, organisation: user.organisation)
+
+          visit organisation_path(user.organisation)
+          click_on project.title
+          click_on I18n.t("page_content.planned_disbursements.button.create")
+
+          expect(page).to have_field(I18n.t("activerecord.attributes.planned_disbursement.providing_organisation_name"), with: non_government_devlivery_partner.name)
+          expect(page).to have_field(I18n.t("activerecord.attributes.planned_disbursement.providing_organisation_type"), with: non_government_devlivery_partner.organisation_type)
+          expect(page).to have_field(I18n.t("form.planned_disbursement.providing_organisation_reference.label"), with: non_government_devlivery_partner.iati_reference)
+        end
+      end
+    end
   end
 
   context "when signed in as a beis user" do
