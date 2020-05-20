c73e4e6c8abd456e5060ca99043090c5dd3352d7
Lorna Harwood <lorna@dxw.com>
2020-05-20 11:09:39 +0100

    Switch our version of the form builder gem to the latest
    - During a separate piece of work we have been trying to take advantage
    of a new feature of this gem.
    - Since we were using a dxw fork, we were unable to do this, so had to
    either switch away from it or update our fork. Given that it looked like
    the gem was now providing the feature that caused us to originally fork,
    we think that it is safe to switch back.
    - On updating the gem we found that there were able to remove the monkey
    patching for Internationalisation and replace it with gem configuration
    https://govuk-form-builder.netlify.app/building-blocks/localisation/#customising-locale-structure
    - We found that we needed to continue to provide an override so that we
    could continue to use a single set of local values rather than have it
    duplicated. This is consist with the initial approach our application
    was using.
    - Whilst by default the gem looks for locale values under helpers.legend
    we were already using helpers.fieldset, so we made another configuration
    change to allow us to continue to use helpers.fieldset
    - We have changed html_safe? to html_safe without a question mark after
    a test failure made us aware of the fact that this hint text was never
    being used. Since the update to the gem we found that we do need to have
    this line for the hint text appear
    
    Co-authored-by: Tom Hipkin <tomhipkin@gmail.com>

--- config/initializers/form_builder.rb
+++ config/initializers/form_builder.rb
@@ -4,28 +4,13 @@ Rails.application.config.action_view.default_form_builder = GOVUKDesignSystemFor
 Rails.application.config.action_view.form_with_generates_remote_forms = false
 
 # Use activerecord.attributes.model.attribute_name for label scope
+
 module GOVUKDesignSystemFormBuilder
+  GOVUKDesignSystemFormBuilder.configure do |conf|
+    conf.localisation_schema_label = %i[activerecord attributes]
+    conf.localisation_schema_legend = %i[helpers fieldset]
+  end
   class Base
-    private def localised_text(context)
-      key = localisation_key(context)
-
-      return nil unless I18n.exists?(key)
-
-      translation = I18n.translate(key)
-      translation = translation.fetch(:html).html_safe if translation.is_a?(Hash) && translation.key?(:html)
-      translation
-    end
-
-    private def localisation_key(context)
-      return nil unless @object_name.present? && @attribute_name.present?
-
-      if context == "label"
-        ["activerecord", "attributes", @object_name, @attribute_name].join(".")
-      else
-        ["helpers", context, @object_name, @attribute_name].join(".")
-      end
-    end
-
     def has_errors?
       @builder.object.errors.any? &&
         @builder.object.errors.messages.dig(
