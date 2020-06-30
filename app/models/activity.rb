2c180153fd4074b4e07944d948d093b76e51bf4c
Robert Lee-Cann <leeky@leeky.org.uk>
2020-06-30 15:41:43 +0100

    Activity identifiers are only unique between sibling activities
    Activities can now have the same identifier, but only when they belong
    to different parent activities.
    
    We are loosening up the uniqueness validation on activity identifiers
    because we believe it is likely that the project and third-party project
    identifiers currently used by delivery partners have the possibility of
    being the same.
    
    When using this reference to publish to IATI we think this approach
    would continue to work. The fund, programme and project identifiers are
    all combined which should guarantee uniqueness across all activities.
    
    NB: We originally added this because we found in practice that programme
    identifiers were being reused by BEIS. However, because they would still
    share a common fund activity, this change wouldn't actual have fixed
    this issue. In that specific case, we chose to change the identifier to
    suffix the delivery partner's initials.
    
    Co-authored-by: Tom Hipkin <tomhipkin@gmail.com>

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -31,7 +31,7 @@ class Activity < ApplicationRecord
   validates :flow, presence: true, on: :flow_step
   validates :aid_type, presence: true, on: :aid_type_step
 
-  validates_uniqueness_of :identifier, allow_nil: true
+  validates :identifier, uniqueness: {scope: :parent_id}, allow_nil: true
   validates :planned_start_date, presence: {message: I18n.t("activerecord.errors.models.activity.attributes.dates")}, on: :dates_step, unless: proc { |a| a.actual_start_date.present? }
   validates :actual_start_date, presence: {message: I18n.t("activerecord.errors.models.activity.attributes.dates")}, on: :dates_step, unless: proc { |a| a.planned_start_date.present? }
   validates :planned_start_date, :planned_end_date, :actual_start_date, :actual_end_date, date_within_boundaries: true
