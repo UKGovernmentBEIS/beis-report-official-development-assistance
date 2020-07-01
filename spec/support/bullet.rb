cf0e439d5d430787566e6aec117d537fdd99d221
Tom Hipkin <tomhipkin@gmail.com>
2020-07-01 15:04:32 +0100

    Chore to group related Bullet whitelists together
    I stumbled on a second place whitelists were being configured for Bullet. This change simply merges the 2 rather than questions if it's correct or optimal.

--- spec/support/bullet.rb
+++ spec/support/bullet.rb
@@ -1,8 +1,6 @@
 RSpec.configure do |config|
   if Bullet.enable?
     config.before(:each) do
-      Bullet.add_whitelist type: :unused_eager_loading, class_name: "User", association: :organisation
-      Bullet.add_whitelist type: :unused_eager_loading, class_name: "Activity", association: :organisation
       Bullet.start_request
     end
 
