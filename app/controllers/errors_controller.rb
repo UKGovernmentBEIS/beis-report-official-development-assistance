4090c08e92c5e8b2bdb0940a6e5f4a349a5b68c7
Laura Porter <laura@dxw.com>
2020-07-15 15:44:21 +0100

    Replace default Rails error pages with styled ones

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,21 @@
+class ErrorsController < ApplicationController
+  include Auth
+  include Pundit
+
+  layout "errors"
+
+  def not_found
+    render "pages/errors/not_found",
+      status: 404
+  end
+
+  def internal_server_error
+    render "pages/errors/internal_server_error",
+      status: 500
+  end
+
+  def unacceptable
+    render "pages/errors/unacceptable",
+      status: 422
+  end
+end
---
+++ spec/requests/errors_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "the custom error pages" do
+  it "responds with a custom 404 page" do
+    get "/404"
+
+    expect(response).to have_http_status(404)
+    expect(response.body).to include I18n.t("page_title.errors.not_found")
+  end
+
+  it "responds with a custom 500 page" do
+    get "/500"
+
+    expect(response).to have_http_status(500)
+    expect(response.body).to include I18n.t("page_title.errors.internal_server_error")
+  end
+
+  it "responds with a custom 422 page" do
+    get "/422"
+
+    expect(response).to have_http_status(422)
+    expect(response.body).to include I18n.t("page_title.errors.unacceptable")
+  end
+end
