7f470e36502a9ea531c54663bee7aaf2682393cd
Laura Porter <laura@dxw.com>
2020-08-05 16:10:47 +0100

    Service to turn an Activity into a CSV row with attributes in expected order

---
+++ app/services/export_activity_to_csv.rb
@@ -0,0 +1,30 @@
+require "csv"
+
+class ExportActivityToCsv
+  attr_accessor :activity
+
+  def initialize(activity:)
+    @activity = activity
+  end
+
+  def call
+    activity_presenter = ActivityPresenter.new(activity)
+    [
+      activity_presenter.identifier,
+      activity_presenter.transparency_identifier,
+      activity_presenter.sector,
+      activity_presenter.title,
+      activity_presenter.description,
+      activity_presenter.status,
+      activity_presenter.planned_start_date,
+      activity_presenter.actual_start_date,
+      activity_presenter.planned_end_date,
+      activity_presenter.actual_end_date,
+      activity_presenter.recipient_region,
+      activity_presenter.recipient_country,
+      activity_presenter.aid_type,
+      activity_presenter.level,
+      "https://#{ENV["DOMAIN"]}#{Rails.application.routes.url_helpers.organisation_activity_details_path(activity_presenter.organisation, activity_presenter)}",
+    ].to_csv
+  end
+end
