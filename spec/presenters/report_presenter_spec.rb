cd5238f9b697ffbab72bad70436bb19c85a8564a
meyric <meyric@dxw.com>
2020-08-13 17:09:32 +0100

    Show the financial quarter of the report
    On the reports tables.
    
    At the moment all reports shown are for the same financial quarter so
    this may not be the most helpful way to use this information, but as a
    fist step on the path to a complete report - review - submit journey it
    gives me confidence that the financial quarters can be shown correctly.

--- spec/presenters/report_presenter_spec.rb
+++ spec/presenters/report_presenter_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe ReportPresenter do
       expect(result).to eql I18n.l(Date.today)
     end
   end
+
+  describe "#financial_quarter_and_year" do
+    it "returns the formatted financial quarter and year e.g. Q1 2020-2021" do
+      report = build(:report, financial_quarter: 1, financial_year: 2020)
+      result = described_class.new(report).financial_quarter_and_year
+
+      expect(result).to eql "Q1 2020-2021"
+    end
+
+    it "returns nil when the report has no financial quarter or year" do
+      report = build(:report, financial_quarter: nil, financial_year: nil)
+      result = described_class.new(report).financial_quarter_and_year
+
+      expect(result).to be_nil
+    end
+  end
 end
