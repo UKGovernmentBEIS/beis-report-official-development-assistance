b9d3af4fbb62c8ef9bdb4c5ee8ccbce99c23b555
James Coglan <james.coglan@dxw.com>
2020-09-09 16:30:07 +0100

    Refactor CreatePlannedDisbursement#report and CreateTransaction#report
    These two classes both infer the currently active report that a planned
    disbursement or transaction should be added to. During bulk transaction
    upload, this report will be provided explicitly, because we'll launch
    the upload process from the page for a specific report.
    
    When ImportTransactions is calling CreateTransaction, I would rather
    pass the Report in, rather than have CreateTransaction infer the current
    Report and possibly disagree with the caller. Therefore I've factored
    the logic shared by these two classes into
    `Report#editable_for_activity`, and made both classes call this if
    they're not given a Report explicitly.
    
    The one difference introduced here is that the previous implementation
    looked only for `active` reports. We actually want to find any report
    the delivery partner can currently edit, which means it may be `active`
    or `awaiting_changes`.

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -13,6 +13,46 @@ RSpec.describe Report, type: :model do
     it { should belong_to(:organisation) }
   end
 
+  describe ".editable_for_activity" do
+    let!(:organisation) { create(:organisation) }
+    let!(:project) { create(:project_activity, organisation: organisation) }
+    let!(:project_in_another_fund) { create(:project_activity, organisation: organisation) }
+
+    let! :approved_report do
+      create(:report, fund: project.associated_fund, organisation: organisation, state: :approved)
+    end
+
+    let! :report_for_another_fund do
+      create(:report, fund: project_in_another_fund.associated_fund, organisation: organisation, state: :active)
+    end
+
+    context "when there is an active report" do
+      let! :active_report do
+        create(:report, fund: project.associated_fund, organisation: organisation, state: :active)
+      end
+
+      it "returns the editable report for the activity's fund" do
+        expect(Report.editable_for_activity(project)).to eq(active_report)
+      end
+    end
+
+    context "when there is a report awaiting changes" do
+      let! :report_awaiting_changes do
+        create(:report, fund: project.associated_fund, organisation: organisation, state: :awaiting_changes)
+      end
+
+      it "returns the editable report for the activity's fund" do
+        expect(Report.editable_for_activity(project)).to eq(report_awaiting_changes)
+      end
+    end
+
+    context "when there is no editable report" do
+      it "returns nothing" do
+        expect(Report.editable_for_activity(project)).to be_nil
+      end
+    end
+  end
+
   it "sets the financial_quarter and financial_year when created" do
     travel_to(Date.parse("01-04-2020")) do
       report = Report.new
