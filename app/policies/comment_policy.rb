6fb9093fed06746773c8b145259ccf496e8278be
Laura Porter <laura@dxw.com>
2020-09-10 15:31:29 +0100

    Add Comment policy
        Comments are editable according to the report they belong to.
    
        If a DP is creating/editing a comment, they can only do so if the report the
        comment belongs to is also editable.
    
        If a DP is editing a comment, they can only do so if the comment was made by
        themselves or someone in the same organisation.
    
    BEIS users can never create or edit a comment.

---
+++ app/policies/comment_policy.rb
@@ -0,0 +1,39 @@
+class CommentPolicy < ApplicationPolicy
+  attr_reader :user, :record
+
+  def index?
+    true
+  end
+
+  def show?
+    return true if beis_user?
+    user.organisation == record.report.organisation
+  end
+
+  def create?
+    return false if beis_user?
+    !editable_report_for_organisation.nil?
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    return false if beis_user?
+    editable_report_for_organisation_and_fund && record.owner.organisation == user.organisation
+  end
+
+  def destroy?
+    false
+  end
+
+  private def editable_report_for_organisation
+    Report.find_by(organisation: user.organisation, state: [:active, :awaiting_changes])
+  end
+  
+  private def editable_report_for_organisation_and_fund
+    fund = record.activity.associated_fund
+    Report.find_by(organisation: record.owner.organisation, fund: fund, state: [:active, :awaiting_changes])
+  end
+end
