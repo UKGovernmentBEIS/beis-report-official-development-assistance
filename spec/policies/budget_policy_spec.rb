b1c460a802a77177ad551ab9554987ecbee246fa
meyric <meyric@dxw.com>
2020-09-15 12:50:47 +0100

    Update Budget policy
    BEIS users can add budget to fund and programme (level A and B)
    activities at anytim
    
    Delivery partner users can add budgets when the activity to which the
    budget is added has a editable report.
    
    Delivery partner users can edit a budget as long as the editable report
    is the one in which it was created.
    
    The policy is a straight copy of the transaction one, we might look to
    dry these out at some point but for now we want to have the policy in
    place to onboard users to the service.

--- spec/policies/budget_policy_spec.rb
+++ spec/policies/budget_policy_spec.rb
@@ -1,42 +1,186 @@
 require "rails_helper"
 
 RSpec.describe BudgetPolicy do
-  let(:user) { create(:beis_user) }
-  let(:activity) { create(:activity, organisation: user.organisation) }
   let(:budget) { create(:budget, parent_activity: activity) }
 
   subject { described_class.new(user, budget) }
 
-  describe "#create?" do
-    context "when the user belongs to the authoring organisation" do
-      let(:budget) { create(:budget, parent_activity: activity) }
-      it { is_expected.to permit_action(:create) }
-    end
+  context "when signed in as a BEIS user" do
+    let(:user) { create(:beis_user) }
 
-    context "when the user does NOT belong to the authoring organisation" do
-      let(:another_organisation) { create(:organisation) }
-      let(:activity) { create(:activity, organisation: another_organisation) }
-      let(:budget) { create(:budget, parent_activity: activity) }
+    context "when the activity has no level" do
+      let(:activity) { CreateActivity.new(organisation_id: user.organisation.id).call }
+
+      it { is_expected.to permit_action(:show) }
+
       it { is_expected.to forbid_action(:create) }
-    end
-  end
-
-  describe "#update?" do
-    context "when the user belongs to the authoring organisation" do
-      let(:budget) { create(:budget, parent_activity: activity) }
-      it { is_expected.to permit_action(:update) }
-    end
-
-    context "when the user does NOT belong to the authoring organisation" do
-      let(:another_organisation) { create(:organisation) }
-      let(:activity) { create(:activity, organisation: another_organisation) }
-      let(:budget) { create(:budget, parent_activity: activity) }
+      it { is_expected.to forbid_action(:edit) }
       it { is_expected.to forbid_action(:update) }
+      it { is_expected.to forbid_action(:destroy) }
+    end
+
+    context "when the activity is a fund" do
+      let(:activity) { create(:fund_activity, organisation: user.organisation) }
+
+      it { is_expected.to permit_action(:show) }
+      it { is_expected.to permit_action(:create) }
+      it { is_expected.to permit_action(:edit) }
+      it { is_expected.to permit_action(:update) }
+
+      it { is_expected.to forbid_action(:destroy) }
+    end
+
+    context "when the activity is a programme" do
+      let(:activity) { create(:programme_activity, organisation: user.organisation) }
+
+      it { is_expected.to permit_action(:show) }
+      it { is_expected.to permit_action(:create) }
+      it { is_expected.to permit_action(:edit) }
+      it { is_expected.to permit_action(:update) }
+
+      it { is_expected.to forbid_action(:destroy) }
+    end
+
+    context "when the activity is a project" do
+      let(:activity) { create(:project_activity, organisation: user.organisation) }
+
+      it { is_expected.to permit_action(:show) }
+
+      it { is_expected.to forbid_action(:create) }
+      it { is_expected.to forbid_action(:edit) }
+      it { is_expected.to forbid_action(:update) }
+      it { is_expected.to forbid_action(:destroy) }
+    end
+
+    context "when the activity is a third party project" do
+      let(:activity) { create(:third_party_project_activity, organisation: user.organisation) }
+
+      it { is_expected.to permit_action(:show) }
+
+      it { is_expected.to forbid_action(:create) }
+      it { is_expected.to forbid_action(:edit) }
+      it { is_expected.to forbid_action(:update) }
+      it { is_expected.to forbid_action(:destroy) }
     end
   end
 
-  describe "#destroy?" do
-    let(:budget) { create(:budget, parent_activity: activity) }
-    it { is_expected.to forbid_action(:destroy) }
+  context "when signed in as a Delivery partner user" do
+    let(:user) { create(:delivery_partner_user) }
+
+    context "when the activity has no level" do
+      let(:activity) { CreateActivity.new(organisation_id: user.organisation.id).call }
+
+      it { is_expected.to permit_action(:show) }
+
+      it { is_expected.to forbid_action(:create) }
+      it { is_expected.to forbid_action(:edit) }
+      it { is_expected.to forbid_action(:update) }
+      it { is_expected.to forbid_action(:destroy) }
+    end
+
+    context "when the activity is a fund" do
+      let(:activity) { create(:fund_activity, organisation: user.organisation) }
+
+      it { is_expected.to permit_action(:show) }
+
+      it { is_expected.to forbid_action(:create) }
+      it { is_expected.to forbid_action(:edit) }
+      it { is_expected.to forbid_action(:update) }
+      it { is_expected.to forbid_action(:destroy) }
+    end
+
+    context "when the activity is a programme" do
+      let(:activity) { create(:programme_activity, organisation: user.organisation) }
+
+      it { is_expected.to permit_action(:show) }
+
+      it { is_expected.to forbid_action(:create) }
+      it { is_expected.to forbid_action(:edit) }
+      it { is_expected.to forbid_action(:update) }
+      it { is_expected.to forbid_action(:destroy) }
+    end
+
+    context "when the activity is a project" do
+      let(:activity) { create(:project_activity) }
+
+      context "and the activity does not belong to the users organiastion" do
+        it { is_expected.to forbid_action(:show) }
+        it { is_expected.to forbid_action(:create) }
+        it { is_expected.to forbid_action(:edit) }
+        it { is_expected.to forbid_action(:update) }
+        it { is_expected.to forbid_action(:destroy) }
+      end
+
+      context "and the activity does belong to the users organisation" do
+        before do
+          activity.update(organisation: user.organisation)
+        end
+
+        context "when there is no editable report" do
+          let(:report) { create(:report, state: :inactive) }
+
+          it { is_expected.to permit_action(:show) }
+
+          it { is_expected.to forbid_action(:create) }
+          it { is_expected.to forbid_action(:edit) }
+          it { is_expected.to forbid_action(:update) }
+          it { is_expected.to forbid_action(:destroy) }
+        end
+
+        context "when there is an editiable report" do
+          let(:report) { create(:report, state: :active) }
+
+          context "and the report is not for the organisation or fund of the activity" do
+            it { is_expected.to permit_action(:show) }
+
+            it { is_expected.to forbid_action(:create) }
+            it { is_expected.to forbid_action(:edit) }
+            it { is_expected.to forbid_action(:update) }
+            it { is_expected.to forbid_action(:destroy) }
+          end
+
+          context "and the report is for the organisation but not the fund of the activity" do
+            before do
+              report.update(organisation: activity.organisation)
+            end
+
+            it { is_expected.to permit_action(:show) }
+
+            it { is_expected.to forbid_action(:create) }
+            it { is_expected.to forbid_action(:edit) }
+            it { is_expected.to forbid_action(:update) }
+            it { is_expected.to forbid_action(:destroy) }
+          end
+
+          context "and the report is for the organisation and fund of the activity" do
+            before do
+              report.update(organisation: activity.organisation, fund: activity.associated_fund)
+            end
+
+            context "when the report is not the one in which the budtet was created" do
+              it { is_expected.to permit_action(:show) }
+              it { is_expected.to permit_action(:create) }
+
+              it { is_expected.to forbid_action(:edit) }
+              it { is_expected.to forbid_action(:update) }
+              it { is_expected.to forbid_action(:destroy) }
+            end
+
+            context "when the report is the one in which the budget was created" do
+              before do
+                budget.update(report: report)
+              end
+
+              it { is_expected.to permit_action(:show) }
+              it { is_expected.to permit_action(:create) }
+              it { is_expected.to permit_action(:edit) }
+              it { is_expected.to permit_action(:update) }
+
+              it { is_expected.to forbid_action(:destroy) }
+            end
+          end
+        end
+      end
+    end
   end
 end
