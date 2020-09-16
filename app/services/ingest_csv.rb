6d138f6c0e950ec376711f1af231f40a2f8b2e7f
Robert Lee-Cann <leeky@leeky.org.uk>
2020-09-16 14:58:15 +0100

    IngestCsv tags Rails.logger entries
    In order to make it easier to spot warning in the output from IngestCsv,
    tag each log entry with "IngestCsv" and the activity's id

--- app/services/ingest_csv.rb
+++ app/services/ingest_csv.rb
@@ -22,24 +22,26 @@ class IngestCsv
           next
         end
 
-        attributes = IngestCsvRow.new(row).call
+        Rails.logger.tagged(["IngestCsv", "activity:#{activity.id}"]) do
+          attributes = IngestCsvRow.new(row).call
 
-        # Ignore attributes that have been set to :skip
-        attributes.delete_if { |_key, value| value == :skip }
+          # Ignore attributes that have been set to :skip
+          attributes.delete_if { |_key, value| value == :skip }
 
-        activity.assign_attributes(attributes)
+          activity.assign_attributes(attributes)
 
-        if activity.valid?
-          # Clean save
-          activity.save!
-        elsif (activity.errors.keys - ACCEPTABLE_INVALID_ATTRIBUTES).empty?
-          # Force validation if the only invalid fields are ones we're ok with
-          activity.save!(validate: false)
-        else
-          # Skip row and write message
-          write_log("Skipping Activity #{row}: #{activity.errors.full_messages}")
-          write_log("attributes: #{attributes.inspect}")
-          # binding.pry
+          if activity.valid?
+            # Clean save
+            activity.save!
+          elsif (activity.errors.keys - ACCEPTABLE_INVALID_ATTRIBUTES).empty?
+            # Force validation if the only invalid fields are ones we're ok with
+            activity.save!(validate: false)
+          else
+            # Skip row and write message
+            write_log("Skipping Activity #{row}: #{activity.errors.full_messages}")
+            write_log("attributes: #{attributes.inspect}")
+            # binding.pry
+          end
         end
       end
     end
