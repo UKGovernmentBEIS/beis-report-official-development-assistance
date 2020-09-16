26a31fef74f885da8b597fd40055928051ed9265
Maria Herrera <maria@dxw.com>
2020-09-16 15:48:27 +0100

    `collaboration type` added to create activity
    This is a new field required when users create new activities through RODA.
    It is required for levels B, C, and D. It is optional for ingested activities.
    The client requested to have this option as radio buttons with a pre-selected
    value of `Bilateral`, since it is the most common value for this field on
    the existing activities.
    
    `collaboration_type` gets reported to IATI through the XMLs, so a later commit
    will contain the logic necessary to make this happen.

--- spec/factories/activity.rb
+++ spec/factories/activity.rb
@@ -56,6 +56,7 @@ FactoryBot.define do
     factory :programme_activity do
       parent factory: :fund_activity
       level { :programme }
+      collaboration_type { "1" }
       funding_organisation_name { "Department for Business, Energy and Industrial Strategy" }
       funding_organisation_reference { "GB-GOV-13" }
       funding_organisation_type { "10" }
@@ -74,6 +75,7 @@ FactoryBot.define do
       call_present { "true" }
       call_open_date { Date.yesterday }
       call_close_date { Date.tomorrow }
+      collaboration_type { "1" }
       total_applications { "25" }
       total_awards { "12" }
       funding_organisation_name { "Department for Business, Energy and Industrial Strategy" }
@@ -103,6 +105,7 @@ FactoryBot.define do
       call_present { "true" }
       call_open_date { Date.yesterday }
       call_close_date { Date.tomorrow }
+      collaboration_type { "1" }
       total_applications { "25" }
       total_awards { "12" }
       funding_organisation_name { "Department for Business, Energy and Industrial Strategy" }
@@ -134,6 +137,7 @@ FactoryBot.define do
     geography { nil }
     recipient_region { nil }
     recipient_country { nil }
+    collaboration_type { nil }
     flow { nil }
     aid_type { nil }
   end
@@ -156,6 +160,7 @@ FactoryBot.define do
     recipient_country { nil }
     intended_beneficiaries { nil }
     gdi { nil }
+    collaboration_type { nil }
     flow { nil }
     aid_type { nil }
   end
@@ -176,6 +181,7 @@ FactoryBot.define do
     recipient_country { nil }
     intended_beneficiaries { nil }
     gdi { nil }
+    collaboration_type { nil }
     flow { nil }
     aid_type { nil }
   end
@@ -185,6 +191,7 @@ FactoryBot.define do
     recipient_country { nil }
     intended_beneficiaries { nil }
     gdi { nil }
+    collaboration_type { nil }
     flow { nil }
     aid_type { nil }
   end
@@ -195,12 +202,20 @@ FactoryBot.define do
     recipient_country { nil }
     intended_beneficiaries { nil }
     gdi { nil }
+    collaboration_type { nil }
     flow { nil }
     aid_type { nil }
   end
 
   trait :nil_form_state do
     form_state { nil }
+  end
+
+  trait :at_collaboration_type_step do
+    form_state { "collaboration_type" }
+    collaboration_type { nil }
+    flow { nil }
+    aid_type { nil }
   end
 
   trait :blank_form_state do
@@ -219,6 +234,7 @@ FactoryBot.define do
     recipient_country { nil }
     intended_beneficiaries { nil }
     gdi { nil }
+    collaboration_type { nil }
     flow { nil }
     aid_type { nil }
     extending_organisation_id { nil }
@@ -244,6 +260,7 @@ FactoryBot.define do
     recipient_country { nil }
     intended_beneficiaries { nil }
     gdi { nil }
+    collaboration_type { nil }
     flow { nil }
     aid_type { nil }
     extending_organisation_id { nil }
@@ -267,6 +284,7 @@ FactoryBot.define do
     recipient_country { nil }
     intended_beneficiaries { nil }
     gdi { nil }
+    collaboration_type { nil }
     flow { nil }
     aid_type { nil }
     extending_organisation_id { nil }
