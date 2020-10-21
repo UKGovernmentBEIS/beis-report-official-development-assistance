f306aacda8ff78600969084784d6cbe3a30cb587
Maria Herrera <maria@dxw.com>
2020-10-21 13:35:50 +0100

    Rake task for invalid activities
    The original plan to flag invalid activities was to introduce a temporary change
    in the UI to inform the user what activities were invalid. Once the activities
    had been amended, we could undo the changes on the code and leave the app as it
    was previously.
    
    After discussion with the wider dev team, we agreed that a better approach for
    this feature would be creating a rake task that we could run in production in
    order to get a list of invalid activities. This list will contain the organisation
    name, the title of the activity, the level, and the URL of the activity. We can
    then supply this list to the client, so they will be able to amend those
    activities.
    
    The reason for this approach is because it doesn't require to make changes to the
    codebase that later will need to be reverted, as this takes time. It is straight
    forward enough that a rake task could give us the same outcome, with less risks
    and being less time-consuming.
    
    This rake task stores a csv file with the invalid activities in the tmp folder.
    We have tested this on local environments and also the spec asserts we get what
    we need.

---
+++ spec/lib/tasks/invalid_activities_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+require "csv"
+Rails.application.load_tasks
+
+describe "invalid_activities.rake" do
+  it "creates a csv file on tmp folder with a list of invalid activities in RODA" do
+    activities = create_list(:project_activity, 5)
+    activities.first.update_columns(gdi: nil, collaboration_type: nil)
+    activities.last.update_columns(title: nil, programme_status: nil)
+
+    run_task(task_name: "invalid_activities")
+    invalid_activities_from_csv = CSV.read("tmp/invalid_activities.csv")
+
+    expect(invalid_activities_from_csv.count).to eql(2)
+    expect(invalid_activities_from_csv.first).to include(activities.first.title)
+    expect(invalid_activities_from_csv.first).to include(activities.first.organisation.name)
+    expect(invalid_activities_from_csv.first).to include(activities.first.level)
+    expect(invalid_activities_from_csv.first).to include(Rails.application.routes.url_helpers.organisation_activity_details_url(activities.first.organisation, activities.first.id, host: ENV["DOMAIN"]))
+  end
+end
+
+def run_task(task_name:)
+  Rake::Task[task_name].invoke
+end
