uk-gov-mirror/UKGovernmentBEIS.beis-report-official-development-assistance

Financial quarter and year on planned disbursements
Now that we have the columns we should collect the financial quarter and
year on planned disbursements in the application.

There is a lot going on here so I have kept the start and end date in
the UI for this commit, which makes it feel a bit odd, but they will go
next.

I make financial_quarter and year required fields in this commit but we
pre-fill the add new planned disbursement form with the current values
so when using the UI there is no issue.

This ingest already populates the values from the start date, so the
only place where a planned disbursement can exist without the values is
in out tests.

Once I remove the fields from the UI, I expect to have to update some
specs, but for now the create planned disbursement service also sets the
value if there isn't financial_quarter and year, to keep the test suite
green until I remove the fields.

app/controllers/staff/planned_disbursements_controller.rb
@@ -6,6 +6,7 @@ class Staff::PlannedDisbursementsController < Staff::BaseController
     @planned_disbursement = PlannedDisbursement.new
     @planned_disbursement.parent_activity = @activity
     pre_fill_providing_organisation
+    pre_fill_financial_quarter_and_year
 
     authorize @planned_disbursement
   end
@@ -50,9 +51,7 @@ class Staff::PlannedDisbursementsController < Staff::BaseController
     end
   end
 
-  private
-
-  def planned_disbursement_params
+  private def planned_disbursement_params
     params.require(:planned_disbursement).permit(
       :planned_disbursement_type,
       :period_start_date,
@@ -64,13 +63,20 @@ class Staff::PlannedDisbursementsController < Staff::BaseController
       :providing_organisation_reference,
       :receiving_organisation_name,
       :receiving_organisation_type,
-      :receiving_organisation_reference
+      :receiving_organisation_reference,
+      :financial_quarter,
+      :financial_year,
     )
   end
 
-  def pre_fill_providing_organisation
+  private def pre_fill_providing_organisation
     @planned_disbursement.providing_organisation_name = @activity.providing_organisation.name
     @planned_disbursement.providing_organisation_type = @activity.providing_organisation.organisation_type
     @planned_disbursement.providing_organisation_reference = @activity.providing_organisation.iati_reference
+  end
+
+  private def pre_fill_financial_quarter_and_year
+    @planned_disbursement.financial_quarter = FinancialPeriods.current_financial_quarter_string
+    @planned_disbursement.financial_year = FinancialPeriods.current_financial_year_string
   end
 end

app/helpers/form_helper.rb
@@ -38,6 +38,18 @@ module FormHelper
     end
   end
 
+  def list_of_financial_quarters
+    @list_of_financial_quarters ||= begin
+      FinancialPeriods::FINANCIAL_QUARTERS.map { |id| OpenStruct.new(id: id, name: "Q#{id}") }
+    end
+  end
+
+  def list_of_financial_years
+    @list_of_financial_years ||= begin
+      FinancialPeriods.next_ten_financial_years.map { |year| OpenStruct.new(id: year.to_s, name: "#{year}-#{year + 1}") }
+    end
+  end
+
   def user_active_options
     [
       OpenStruct.new(id: "true", name: t("form.user.active.active")),

UKGovernmentBEIS/beis-report-official-development-assistance

app/controllers/staff/planned_disbursements_controller.rb
@@ -6,6 +6,7 @@ class Staff::PlannedDisbursementsController < Staff::BaseController
     @planned_disbursement = PlannedDisbursement.new
     @planned_disbursement.parent_activity = @activity
     pre_fill_providing_organisation
+    pre_fill_financial_quarter_and_year
 
     authorize @planned_disbursement
   end
@@ -50,9 +51,7 @@ class Staff::PlannedDisbursementsController < Staff::BaseController
     end
   end
 
-  private
-
-  def planned_disbursement_params
+  private def planned_disbursement_params
     params.require(:planned_disbursement).permit(
       :planned_disbursement_type,
       :period_start_date,
@@ -64,13 +63,20 @@ class Staff::PlannedDisbursementsController < Staff::BaseController
       :providing_organisation_reference,
       :receiving_organisation_name,
       :receiving_organisation_type,
-      :receiving_organisation_reference
+      :receiving_organisation_reference,
+      :financial_quarter,
+      :financial_year,
     )
   end
 
-  def pre_fill_providing_organisation
+  private def pre_fill_providing_organisation
     @planned_disbursement.providing_organisation_name = @activity.providing_organisation.name
     @planned_disbursement.providing_organisation_type = @activity.providing_organisation.organisation_type
     @planned_disbursement.providing_organisation_reference = @activity.providing_organisation.iati_reference
+  end
+
+  private def pre_fill_financial_quarter_and_year
+    @planned_disbursement.financial_quarter = FinancialPeriod.current_quarter_string
+    @planned_disbursement.financial_year = FinancialPeriod.current_year_string
   end
 end

app/helpers/form_helper.rb
@@ -38,6 +38,18 @@ module FormHelper
     end
   end
 
+  def list_of_financial_quarters
+    @list_of_financial_quarters ||= begin
+      FinancialPeriod::FINANCIAL_QUARTERS.map { |id| OpenStruct.new(id: id, name: "Q#{id}") }
+    end
+  end
+
+  def list_of_financial_years
+    @list_of_financial_years ||= begin
+      FinancialPeriod.next_ten_years.map { |year| OpenStruct.new(id: year.to_s, name: "#{year}-#{year + 1}") }
+    end
+  end
+
   def user_active_options
     [
       OpenStruct.new(id: "true", name: t("form.user.active.active")),