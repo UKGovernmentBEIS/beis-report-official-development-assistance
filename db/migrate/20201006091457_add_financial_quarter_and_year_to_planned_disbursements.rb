uk-gov-mirror/UKGovernmentBEIS.beis-report-official-development-assistance

Add financial quarter and year to PlannedDisbursements
The long term goal is to only collect the financial quarter (1-4) and
financial year on planned disbursements and calculate both
period_start_date and period_end_date based on the values.

The first step towards this is to add the columns to PlannedDisbursement
and to backfill existing records with values using the period_start_date
to make the calculation in reverse.

I felt a concern or helper wasn't the right place to put methods to
calculate values for financial quarters and years as I know they will be
used across models, controllers and services in the next few commits
that will introduce more around the changes we want to make to
PlannedDisbursement.

I settled on a simple module in the lib directory, so I changed the
application configuration to eager load from here.

I also don't want to remove the existing period_start and period_end
dates as we have values in them in production. We have the option to
remove them later (and calculate start and end dates for IATI publishing)
if we want to but that feels like a separate decision and piece of work
to this.

As the schema is changed to make this change I included the backfill as
part of the migration.

I also skipped validation on PlannedDisbursement when adding the
backfilled values as I don't want to worry about invalid records that
might exist, only ensure we have the financial quarter and year.

db/migrate/20201006091457_add_financial_quarter_and_year_to_planned_disbursements.rb
@@ -1,0 +1,21 @@
+class AddFinancialQuarterAndYearToPlannedDisbursements < ActiveRecord::Migration[6.0]
+  def change
+    add_column :planned_disbursements, :financial_quarter, :integer, index: true
+    add_column :planned_disbursements, :financial_year, :integer, index: true
+
+    reversible do |change|
+      change.up do
+        planned_disbursements = PlannedDisbursement.all
+
+        planned_disbursements.each do |planned_disbursement|
+          financial_quarter = FinancialPeriods.financial_quarter_from_date(planned_disbursement.period_start_date)
+          financial_year = FinancialPeriods.financial_year_from_date(planned_disbursement.period_start_date)
+
+          planned_disbursement.financial_quarter = financial_quarter
+          planned_disbursement.financial_year = financial_year
+          planned_disbursement.save(validate: false)
+        end
+      end
+    end
+  end
+end

UKGovernmentBEIS/beis-report-official-development-assistance

db/migrate/20201006091457_add_financial_quarter_and_year_to_planned_disbursements.rb
@@ -1,0 +1,21 @@
+class AddFinancialQuarterAndYearToPlannedDisbursements < ActiveRecord::Migration[6.0]
+  def change
+    add_column :planned_disbursements, :financial_quarter, :integer, index: true
+    add_column :planned_disbursements, :financial_year, :integer, index: true
+
+    reversible do |change|
+      change.up do
+        planned_disbursements = PlannedDisbursement.all
+
+        planned_disbursements.each do |planned_disbursement|
+          financial_quarter = FinancialPeriod.quarter_from_date(planned_disbursement.period_start_date)
+          financial_year = FinancialPeriod.year_from_date(planned_disbursement.period_start_date)
+
+          planned_disbursement.financial_quarter = financial_quarter
+          planned_disbursement.financial_year = financial_year
+          planned_disbursement.save(validate: false)
+        end
+      end
+    end
+  end
+end