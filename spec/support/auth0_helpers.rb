23ffadb421054afb7e6acc10d910ec43166dd655
meyric <meyric@dxw.com>
2020-11-04 16:59:21 +0000

    Fix Auth0 create_user call
    We recently updated the Auth0 gem and I missed a breaking change.
    
    The signature of the `create_user` method has changed; the connection
    name, which is just a string that matches the default database
    connection in Auth0, comes first and is required and the name of the
    user is passed in the options as `name`.
    
    Without this change the application cannot create new users in Auth0.
    The application handles the error gracefully as the API simply returns
    an error code that we display to the user.
    
    We've update the stubbed request to require the connection now to catch
    this.

--- spec/support/auth0_helpers.rb
+++ spec/support/auth0_helpers.rb
@@ -6,13 +6,13 @@ module Auth0Helpers
 
   def stub_auth0_create_user_request(email:, auth0_identifier: "auth0|123456789")
     stub_request(:post, "https://testdomain/api/v2/users")
-      .with(body: hash_including(email: email))
+      .with(body: hash_including(connection: "Username-Password-Authentication", email: email))
       .to_return(status: 200, body: "{\"user_id\":\"#{auth0_identifier}\"}")
   end
 
   def stub_auth0_create_user_request_failure(email:, body: "{\"message\":\"The user already exists.\"}")
     stub_request(:post, "https://testdomain/api/v2/users")
-      .with(body: hash_including(email: email))
+      .with(body: hash_including(connection: "Username-Password-Authentication", email: email))
       .to_return(status: 500, body: body)
   end
 
