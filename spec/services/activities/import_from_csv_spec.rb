0952ec04369265fdc67491078972a2ab3f33b144
Stuart Harrison <pezholio@gmail.com>
2020-11-12 13:42:59 +0000

    Start building out the Activity import machinery
    This adds a couple of classes that start to build out the code that will eventually
    backfill missing activity data. So far it accepts an array of hashes (which will
    eventually come from a CSV) to update an existing Activity (which is fetched by
    its RODA ID). If a field is blank, it is ignored. If any activities in the array of attribute
    hashes are missing, then the entire operation fails and the main classes error
    array is populated.

---
+++ spec/services/activities/import_from_csv_spec.rb
@@ -0,0 +1,88 @@
+require "rails_helper"
+
+RSpec.describe Activities::ImportFromCsv do
+  let(:organisation) { create(:organisation) }
+  let(:existing_activity) { create(:activity) }
+  let(:activity_attributes) do
+    {
+      "RODA ID" => existing_activity.roda_identifier_compound,
+      "Title" => "Here is a title",
+      "Description" => "Some description goes here...",
+      "Recipient Region" => "789",
+    }
+  end
+
+  subject { described_class.new(organisation: organisation) }
+
+  it "has an error if an Activity does not exist" do
+    activity_attributes["RODA ID"] = "FAKE RODA ID"
+
+    subject.import([activity_attributes])
+
+    expect(subject.errors.count).to eq(1)
+
+    expect(subject.errors.first.csv_row).to eq(2)
+    expect(subject.errors.first.column).to eq(:roda_id)
+    expect(subject.errors.first.value).to eq("FAKE RODA ID")
+    expect(subject.errors.first.message).to eq(I18n.t("importer.errors.activity.not_found"))
+  end
+
+  it "updates an existing activity" do
+    subject.import([activity_attributes])
+
+    expect(subject.errors.count).to eq(0)
+
+    expect(existing_activity.reload.title).to eq(activity_attributes["Title"])
+    expect(existing_activity.description).to eq(activity_attributes["Description"])
+    expect(existing_activity.recipient_region).to eq(activity_attributes["Recipient Region"])
+  end
+
+  it "ignores any blank columns" do
+    activity_attributes["Title"] = ""
+
+    expect { subject.import([activity_attributes]) }.to_not change { existing_activity.title }
+    expect(subject.errors.count).to eq(0)
+  end
+
+  it "has an error and does not update any other activities if an Activity does not exist" do
+    activities = [
+      activity_attributes,
+      {
+        "RODA ID" => "FAKE RODA ID",
+        "Title" => "Here is another title",
+        "Description" => "Another description goes here...",
+        "Recipient Region" => "789",
+      },
+    ]
+
+    expect { subject.import(activities) }.to_not change { existing_activity }
+
+    expect(subject.errors.count).to eq(1)
+    expect(subject.errors.first.csv_row).to eq(3)
+    expect(subject.errors.first.column).to eq(:roda_id)
+    expect(subject.errors.first.value).to eq("FAKE RODA ID")
+    expect(subject.errors.first.message).to eq(I18n.t("importer.errors.activity.not_found"))
+  end
+
+  it "has an error and does not update any other activities if a region does not exist" do
+    activity_2 = create(:activity)
+
+    activities = [
+      activity_attributes,
+      {
+        "RODA ID" => activity_2.roda_identifier_compound,
+        "Title" => "Here is another title",
+        "Description" => "Another description goes here...",
+        "Recipient Region" => "111111",
+      },
+    ]
+
+    expect { subject.import(activities) }.to_not change { existing_activity }
+
+    expect(subject.errors.count).to eq(1)
+    expect(subject.errors.first.csv_row).to eq(3)
+    expect(subject.errors.first.column).to eq(:recipient_region)
+    expect(subject.errors.first.value).to eq("111111")
+    expect(subject.errors.first.message).to eq(I18n.t("importer.errors.activity.invalid_region"))
+  end
+end
