f054b1e2790003f816dbcc972bdab37fe4ab9b08
Stuart Harrison <pezholio@gmail.com>
2020-11-13 16:28:27 +0000

    Allow importer to create Activities
    The importer can now create activities which:
    
    a) Do not have a RODA ID specified and:
    b) Have a RODA Fragment and RODA Parent ID
    
    If the above is true, the row is passed to the ActivityCreator class, which initializes
    a new activity, assigns the attributes, saves it, and gathers up any errors.
    
    As we can only validate against a limited subset of contexts, there's a constant
    that has the contexts we can validate against with the fields we have. Eventually
    this will go away.
    
    I've also added a new converter that fetches the parent Activity by its RODA ID,
    and returns the UUID from the database. If the activity cannot be found, an error
    is added to the errors hash.

--- app/services/activities/import_from_csv.rb
+++ app/services/activities/import_from_csv.rb
@@ -21,15 +21,24 @@ module Activities
     end
 
     def import_row(row, index)
-      if row["RODA ID"].present?
+      if row["RODA ID"].blank? && row["RODA ID Fragment"].present? && row["Parent RODA ID"].present?
+        action = create_activity(row, index)
+      elsif row["RODA ID"].present?
         action = update_activity(row, index)
       else
-        add_error(index, :roda_id, row["RODA ID"], I18n.t("importer.errors.activity.cannot_update")) && return
+        add_error(index, :roda_id, row["RODA ID"], I18n.t("importer.errors.activity.cannot_create_or_update")) && return
       end
 
       action.errors.each do |attr_name, (value, message)|
         add_error(index, attr_name, value, message)
       end
+    end
+
+    def create_activity(row, index)
+      creator = ActivityCreator.new(@organisation, row)
+      creator.create
+
+      creator
     end
 
     def update_activity(row, index)
@@ -74,6 +83,47 @@ module Activities
       end
     end
 
+    class ActivityCreator
+      IMPORT_VALIDATION_STEPS = [
+        :level_step,
+        :parent_step,
+        :identifier_step,
+        :roda_identifier_step,
+      ]
+
+      attr_reader :errors
+
+      def initialize(organisation, row)
+        @organisation = organisation
+        @activity = Activity.new
+        @errors = {}
+        @converter = Converter.new(row)
+        @errors.update(@converter.errors)
+      end
+
+      def create
+        return unless @converter.errors.blank?
+
+        @activity.organisation = @organisation
+        @activity.reporting_organisation = @organisation
+
+        @activity.assign_attributes(@converter.to_h)
+        @activity.level = calculate_level
+        @activity.cache_roda_identifier
+
+        # TODO: This will eventually need to validate against all contexts (Activity::VALIDATION_STEPS)
+        return if @activity.save(context: IMPORT_VALIDATION_STEPS)
+
+        @activity.errors.each do |attr_name, message|
+          @errors[attr_name] ||= [@converter.raw(attr_name), message]
+        end
+      end
+
+      def calculate_level
+        @activity&.parent&.child_level
+      end
+    end
+
     class Converter
       include CodelistHelper
 
@@ -84,6 +134,8 @@ module Activities
         description: "Description",
         recipient_region: "Recipient Region",
         delivery_partner_identifier: "Delivery partner identifier",
+        roda_identifier_fragment: "RODA ID Fragment",
+        parent_id: "Parent RODA ID",
       }
 
       def initialize(row)
@@ -125,6 +177,14 @@ module Activities
           :recipient_region,
           I18n.t("importer.errors.activity.invalid_region"),
         )
+      end
+
+      def convert_parent_id(roda_id)
+        parent = Activity.by_roda_identifier(roda_id)
+
+        raise I18n.t("importer.errors.activity.parent_not_found") if parent.nil?
+
+        parent.id
       end
 
       def validate_from_codelist(code, entity, message)
--- spec/services/activities/import_from_csv_spec.rb
+++ spec/services/activities/import_from_csv_spec.rb
@@ -2,36 +2,50 @@ require "rails_helper"
 
 RSpec.describe Activities::ImportFromCsv do
   let(:organisation) { create(:organisation) }
+  let(:parent_activity) { create(:activity) }
   let(:existing_activity) { create(:activity) }
-  let(:activity_attributes) do
+  let(:existing_activity_attributes) do
     {
       "RODA ID" => existing_activity.roda_identifier_compound,
+      "RODA ID Fragment" => "",
+      "Parent RODA ID" => "",
       "Title" => "Here is a title",
       "Description" => "Some description goes here...",
       "Recipient Region" => "789",
       "Delivery partner identifier" => "1234567890",
     }
   end
+  let(:new_activity_attributes) do
+    {
+      "RODA ID" => "",
+      "RODA ID Fragment" => "234566",
+      "Parent RODA ID" => parent_activity.roda_identifier_fragment,
+      "Title" => "Here is a title",
+      "Description" => "Some description goes here...",
+      "Recipient Region" => "789",
+      "Delivery partner identifier" => "98765432",
+    }
+  end
 
   subject { described_class.new(organisation: organisation) }
 
-  it "returns an error when the RODA ID is not present" do
-    activity_attributes["RODA ID"] = ""
-    expect { subject.import([activity_attributes]) }.to_not change { Activity.count }
+  it "returns an error when the ID and fragments are not present" do
+    existing_activity_attributes["RODA ID"] = ""
+
+    expect { subject.import([existing_activity_attributes]) }.to_not change { Activity.count }
 
     expect(subject.errors.count).to eq(1)
-
     expect(subject.errors.first.csv_row).to eq(2)
     expect(subject.errors.first.column).to eq(:roda_id)
     expect(subject.errors.first.value).to eq("")
-    expect(subject.errors.first.message).to eq(I18n.t("importer.errors.activity.cannot_update"))
+    expect(subject.errors.first.message).to eq(I18n.t("importer.errors.activity.cannot_create_or_update"))
   end
 
   context "when updating an existing activity" do
     it "has an error if an Activity does not exist" do
-      activity_attributes["RODA ID"] = "FAKE RODA ID"
+      existing_activity_attributes["RODA ID"] = "FAKE RODA ID"
 
-      subject.import([activity_attributes])
+      subject.import([existing_activity_attributes])
 
       expect(subject.errors.count).to eq(1)
 
@@ -42,26 +56,26 @@ RSpec.describe Activities::ImportFromCsv do
     end
 
     it "updates an existing activity" do
-      subject.import([activity_attributes])
+      subject.import([existing_activity_attributes])
 
       expect(subject.errors.count).to eq(0)
 
-      expect(existing_activity.reload.title).to eq(activity_attributes["Title"])
-      expect(existing_activity.description).to eq(activity_attributes["Description"])
-      expect(existing_activity.recipient_region).to eq(activity_attributes["Recipient Region"])
-      expect(existing_activity.delivery_partner_identifier).to eq(activity_attributes["Delivery partner identifier"])
+      expect(existing_activity.reload.title).to eq(existing_activity_attributes["Title"])
+      expect(existing_activity.description).to eq(existing_activity_attributes["Description"])
+      expect(existing_activity.recipient_region).to eq(existing_activity_attributes["Recipient Region"])
+      expect(existing_activity.delivery_partner_identifier).to eq(existing_activity_attributes["Delivery partner identifier"])
     end
 
     it "ignores any blank columns" do
-      activity_attributes["Title"] = ""
+      existing_activity_attributes["Title"] = ""
 
-      expect { subject.import([activity_attributes]) }.to_not change { existing_activity.title }
+      expect { subject.import([existing_activity_attributes]) }.to_not change { existing_activity.title }
       expect(subject.errors.count).to eq(0)
     end
 
     it "has an error and does not update any other activities if an Activity does not exist" do
       activities = [
-        activity_attributes,
+        existing_activity_attributes,
         {
           "RODA ID" => "FAKE RODA ID",
           "Title" => "Here is another title",
@@ -83,7 +97,7 @@ RSpec.describe Activities::ImportFromCsv do
       activity_2 = create(:activity)
 
       activities = [
-        activity_attributes,
+        existing_activity_attributes,
         {
           "RODA ID" => activity_2.roda_identifier_compound,
           "Title" => "Here is another title",
@@ -99,6 +113,75 @@ RSpec.describe Activities::ImportFromCsv do
       expect(subject.errors.first.column).to eq(:recipient_region)
       expect(subject.errors.first.value).to eq("111111")
       expect(subject.errors.first.message).to eq(I18n.t("importer.errors.activity.invalid_region"))
+    end
+  end
+
+  context "when creating a new activity" do
+    it "creates the activity" do
+      rows = [new_activity_attributes]
+      expect { subject.import(rows) }.to change { Activity.count }.by(1)
+
+      expect(subject.errors.count).to eq(0)
+
+      new_activity = Activity.order(:created_at).last
+
+      expected_roda_identifier_compound = [
+        parent_activity.roda_identifier_compound,
+        new_activity_attributes["RODA ID Fragment"],
+      ].join("-")
+
+      expect(new_activity.parent).to eq(parent_activity)
+      expect(new_activity.roda_identifier_compound).to eq(expected_roda_identifier_compound)
+      expect(new_activity.title).to eq(new_activity_attributes["Title"])
+      expect(new_activity.description).to eq(new_activity_attributes["Description"])
+      expect(new_activity.roda_identifier_fragment).to eq(new_activity_attributes["RODA ID Fragment"])
+      expect(new_activity.recipient_region).to eq(new_activity_attributes["Recipient Region"])
+      expect(new_activity.delivery_partner_identifier).to eq(new_activity_attributes["Delivery partner identifier"])
+    end
+
+    it "has an error if a region does not exist" do
+      new_activity_attributes["Recipient Region"] = "111111"
+
+      expect { subject.import([new_activity_attributes]) }.to_not change { Activity.count }
+
+      expect(subject.errors.count).to eq(1)
+      expect(subject.errors.first.csv_row).to eq(2)
+      expect(subject.errors.first.column).to eq(:recipient_region)
+      expect(subject.errors.first.value).to eq("111111")
+      expect(subject.errors.first.message).to eq(I18n.t("importer.errors.activity.invalid_region"))
+    end
+
+    it "has an error if the parent activity cannot be found" do
+      new_activity_attributes["Parent RODA ID"] = "111111"
+
+      expect { subject.import([new_activity_attributes]) }.to_not change { Activity.count }
+
+      expect(subject.errors.count).to eq(1)
+      expect(subject.errors.first.csv_row).to eq(2)
+      expect(subject.errors.first.column).to eq(:parent_id)
+      expect(subject.errors.first.value).to eq("111111")
+      expect(subject.errors.first.message).to eq(I18n.t("importer.errors.activity.parent_not_found"))
+    end
+
+    it "has an error if the identifier is invalid" do
+      new_activity_attributes["RODA ID Fragment"] = "%^$!234566"
+
+      expect { subject.import([new_activity_attributes]) }.to_not change { Activity.count }
+
+      expect(subject.errors.count).to eq(1)
+      expect(subject.errors.first.csv_row).to eq(2)
+      expect(subject.errors.first.column).to eq(:roda_identifier_fragment)
+      expect(subject.errors.first.value).to eq("%^$!234566")
+      expect(subject.errors.first.message).to eq(I18n.t("activerecord.errors.models.activity.attributes.roda_identifier_fragment.invalid_characters"))
+    end
+  end
+
+  context "when updating and importing" do
+    it "creates and imports activities" do
+      rows = [existing_activity_attributes, new_activity_attributes]
+      expect { subject.import(rows) }.to change { Activity.count }.by(1)
+
+      expect(subject.errors.count).to eq(0)
     end
   end
 end
