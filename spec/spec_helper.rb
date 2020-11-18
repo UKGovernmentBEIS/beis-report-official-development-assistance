6ea095cf0f43c39600ed5c824df366cd5c3d5674
Robert Lee-Cann <leeky@leeky.org.uk>
2020-11-18 14:37:14 +0000

    Ensure tests run in a random order each time
    I recently noticed that my tests were always using the same seed,
    instead of running in a different order each time, as per the RSpec
    configuration.
    
    This fix ensures the seed is always regenerated every time, unless the
    `--seed` switch is passed.
    
    Source: https://github.com/rails/spring/issues/113
    
    Additionally, I've uncommented the `Kernel.srand` line, which means that
    random number generation is consistent when the seed is the same.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -108,12 +108,12 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = :random
-  #
-  #   # Seed global randomization in this process using the `--seed` CLI option.
-  #   # Setting this allows you to use `--seed` to deterministically reproduce
-  #   # test failures related to randomization by passing the same `--seed` value
-  #   # as the one that triggered the failure.
-  #   Kernel.srand config.seed
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
 
   # Prevent --bisect from locking up
   config.bisect_runner = :shell
