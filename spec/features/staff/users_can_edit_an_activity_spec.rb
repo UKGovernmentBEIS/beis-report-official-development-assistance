72cb4502a27244366accae092ed061c4ecf02cf2
Robert Lee-Cann <leeky@leeky.org.uk>
2020-11-23 19:54:03 +0000

    Show the GCRF challenge area on the details page

--- spec/features/staff/users_can_edit_an_activity_spec.rb
+++ spec/features/staff/users_can_edit_an_activity_spec.rb
@@ -557,6 +557,15 @@ def assert_all_edit_links_go_to_the_correct_form_step(activity:)
   click_on(t("default.link.back"))
   click_on t("tabs.activity.details")
 
+  within(".gcrf_challenge_area") do
+    click_on(t("default.link.edit"))
+    expect(page).to have_current_path(
+      activity_step_path(activity, :gcrf_challenge_area)
+    )
+  end
+  click_on(t("default.link.back"))
+  click_on t("tabs.activity.details")
+
   unless activity.fund?
     within(".collaboration_type") do
       click_on(t("default.link.edit"))
