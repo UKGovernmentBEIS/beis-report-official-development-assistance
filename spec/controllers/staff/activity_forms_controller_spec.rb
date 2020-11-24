a133f3c737f86eba5c7d475f24dfbdf9b89499f0
Robert Lee-Cann <leeky@leeky.org.uk>
2020-11-24 23:17:52 +0000

    Use controller spec for testing step skipping
    We're currently using integration tests to check that the correct steps
    are shown to the user, depending on the level of the activity being
    edited.
    
    This is quite slow and gets complicated when you started dealing with
    combinations of both level and fund affecting the steps being skipped.
    
    The 'wicked' gem's `render_wizard` method redirects the user to the next
    form step when `skip_step` is called. Therefore a controller test can
    be used to test this logic.

---
+++ spec/controllers/staff/activity_forms_controller_spec.rb
@@ -0,0 +1,117 @@
+require "rails_helper"
+
+RSpec.describe Staff::ActivityFormsController do
+  let(:user) { create(:delivery_partner_user, organisation: organisation) }
+  let(:organisation) { create(:delivery_partner_organisation) }
+
+  before do
+    allow(controller).to receive(:current_user).and_return(user)
+    allow(controller).to receive(:logged_in_using_omniauth?).and_return(true)
+  end
+
+  describe "#show" do
+    context "when editing a fund" do
+      let(:activity) { create(:fund_activity, organisation: organisation) }
+
+      context "parent step" do
+        subject { get_step :parent }
+
+        it { is_expected.to skip_to_next_step }
+      end
+
+      context "gcrf_challenge_area step" do
+        subject { get_step :gcrf_challenge_area }
+
+        it { is_expected.to render_current_step }
+      end
+    end
+
+    context "when editing a programme" do
+      let(:fund) { create(:fund_activity) }
+      let(:activity) { create(:programme_activity, organisation: organisation, parent: fund) }
+
+      context "parent step" do
+        subject { get_step :parent }
+
+        it { is_expected.to render_current_step }
+      end
+
+      context "gcrf_challenge_area step" do
+        subject { get_step :gcrf_challenge_area }
+
+        it { is_expected.to render_current_step }
+      end
+    end
+
+    context "when editing a project" do
+      let(:fund) { create(:fund_activity) }
+      let(:programme) { create(:programme_activity, parent: fund) }
+      let(:activity) { create(:project_activity, organisation: organisation, parent: programme) }
+
+      context "parent step" do
+        subject { get_step :parent }
+
+        it { is_expected.to render_current_step }
+      end
+
+      context "gcrf_challenge_area step" do
+        subject { get_step :gcrf_challenge_area }
+
+        it { is_expected.to render_current_step }
+      end
+    end
+
+    context "when editing a third-party project" do
+      let(:fund) { create(:fund_activity) }
+      let(:programme) { create(:programme_activity, parent: fund) }
+      let(:project) { create(:project_activity, parent: programme) }
+      let(:activity) { create(:third_party_project_activity, organisation: organisation, parent: project) }
+
+      context "parent step" do
+        subject { get_step :parent }
+
+        it { is_expected.to render_current_step }
+      end
+
+      context "gcrf_challenge_area step" do
+        subject { get_step :gcrf_challenge_area }
+
+        it { is_expected.to render_current_step }
+      end
+    end
+  end
+
+  private
+
+  def get_step(step)
+    get :show, params: {activity_id: activity.id, id: step}
+  end
+
+  RSpec::Matchers.define :skip_to_next_step do
+    match do |actual|
+      expect(actual).to redirect_to(controller.next_wizard_path)
+    end
+
+    description do
+      "skip to the next step (#{controller.next_step})"
+    end
+
+    failure_message do |actual|
+      "expected to skip the next step (#{controller.step}), but didn't"
+    end
+  end
+
+  RSpec::Matchers.define :render_current_step do
+    match do |actual|
+      expect(actual).to render_template(controller.step)
+    end
+
+    description do
+      "render the current step"
+    end
+
+    failure_message do |actual|
+      "expected to render the current form step (#{controller.step}), but didn't"
+    end
+  end
+end
