5d027e1c1b9df5b42d8f83f2eb957e5f5e5a5fe1
James Coglan <james.coglan@dxw.com>
2020-11-29 18:40:27 +0000

    Check that imported forecasts go into the latest report
    When developers upload files to back-fill the forecast data for last
    quarter, they will specify which report to add that data to. This is
    because last quarter's reports are no longer editable and so won't be
    the one PlannedDisbursementHistory would normally pick.
    
    As a safety check, we make sure the given report is still the latest one
    for the fund and organisation in question. The versioing model we use
    for forecasts assumes that reports and the data they contain are totally
    ordered -- we do not go back and change older reports that have been
    committed to the history.
    
    It's still safe to modify the most recent report, even if it's no longer
    editable by end users, as long as a new report has not been opened for
    users to add data to. The data users enter in Report 2 will be informed
    by the data the site shows for Report 1, so if we go back and change
    Report 1 that may invalidate data in Report 2.
    
    So to make sure what we're doing is safe, and rather than implementing a
    model that supports arbitrarily editing history, we check that the given
    Report is the latest one in its sequence, and is therefore safe to
    modify as users have not yet begun filling a new report out.
    
    Doing this check in ImportPlannedDisbursements means it's only done once
    per import, whereas putting it in PlannedDisbursementHistory would mean
    it was done for every forecast value.

--- app/services/import_planned_disbursements.rb
+++ app/services/import_planned_disbursements.rb
@@ -4,13 +4,27 @@ class ImportPlannedDisbursements
   RODA_ID_KEY = "RODA identifier"
   FORECAST_COLUMN_HEADER = /FC +(\d{4})\/\d{2} +FY +Q([1-4])/
 
+  attr_reader :errors
+
   def initialize(report:)
     @report = report
+    @errors = []
   end
 
   def import(forecasts)
-    forecasts.each { |row| import_row(row) }
+    latest_report = Report
+      .where(fund: @report.fund_id, organisation: @report.organisation_id)
+      .in_historical_order
+      .first
+
+    ActiveRecord::Base.transaction do
+      log_report_not_latest_error(latest_report) unless @report == latest_report
+      forecasts.each { |row| import_row(row) }
+      raise ActiveRecord::Rollback unless @errors.empty?
+    end
   end
+
+  private
 
   def import_row(row)
     roda_identifier = row[RODA_ID_KEY]
@@ -30,5 +44,22 @@ class ImportPlannedDisbursements
   def import_forecast(activity, quarter, year, value)
     history = PlannedDisbursementHistory.new(activity, quarter, year, report: @report)
     history.set_value(value)
+  end
+
+  def log_report_not_latest_error(latest_report)
+    organisation = @report.organisation.name
+    fund = @report.fund.roda_identifier
+
+    message = [
+      "The report #{@report.id} (#{organisation}, #{quarter @report} for #{fund},",
+      "#{@report.state}) is not the latest for that organisation and fund.",
+      "The latest is #{latest_report.id}, for #{quarter latest_report} (#{latest_report.state}).",
+    ]
+
+    @errors << message.join(" ")
+  end
+
+  def quarter(report)
+    "Q#{report.financial_quarter} #{report.financial_year}"
   end
 end
