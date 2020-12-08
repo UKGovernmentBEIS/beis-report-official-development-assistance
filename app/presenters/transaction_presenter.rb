e14f753dee6a4ae52653b33a8623bea26fba8ca1
Cristina <cristina@dxw.com>
2020-12-08 19:33:37 +0000

    Transaction presenter can show quarter and year
    The display of transactions summary needs to be consistent with the display of the forecasted spend summary.
    
    We need to calculate the financial quarter and year from the transaction date, and show them in the same formatted way that the forecasts show this information.

--- app/presenters/transaction_presenter.rb
+++ app/presenters/transaction_presenter.rb
@@ -23,4 +23,10 @@ class TransactionPresenter < SimpleDelegator
     return if super.blank?
     ActionController::Base.helpers.number_to_currency(super, unit: "£")
   end
+
+  def financial_quarter_and_year
+    return nil if date.blank?
+    financial_year = FinancialPeriod.year_from_date(to_model.date).to_i
+    "Q#{FinancialPeriod.quarter_from_date(to_model.date)} #{financial_year}-#{financial_year + 1}"
+  end
 end
