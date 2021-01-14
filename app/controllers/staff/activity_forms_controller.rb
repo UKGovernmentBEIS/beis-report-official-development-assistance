14d08586d0a8175449abf49061eb1f84321e417f
Stuart Harrison <pezholio@gmail.com>
2021-01-14 15:07:59 +0000

    Refactor ActivityFormsController
    The ActivityFormsController has a lot of repetition and makes the controller quite
    large. I've factored all the update actions into a lean(er) class that accepts the
    activity and the specific step, then works out which parameters to fetch and apply,
    as well as any extra steps that need to be performed. I haven't bothered with any
    unit tests for this class, which does make me a little anxious, but I think it's fine, as
    this is all covered by feature tests.

--- app/controllers/staff/activity_forms_controller.rb
+++ app/controllers/staff/activity_forms_controller.rb
@@ -1,10 +1,6 @@
 class Staff::ActivityFormsController < Staff::BaseController
   include Wicked::Wizard
-  include DateHelper
-  include ActivityHelper
   include CodelistHelper
-
-  DEFAULT_PROGRAMME_STATUS_FOR_FUNDS = "spend_in_progress"
 
   steps(*Activity::FORM_STEPS)
 
@@ -66,109 +62,12 @@ class Staff::ActivityFormsController < Staff::BaseController
     @page_title = t("page_title.activity_form.show.#{step}", sector_category: t("activity.sector_category.#{@activity.sector_category}"), level: t("page_content.activity.level.#{@activity.level}"))
     authorize @activity
 
-    if @activity.fund?
-      @activity.assign_attributes(programme_status: DEFAULT_PROGRAMME_STATUS_FOR_FUNDS)
-    end
+    updater = Activity::Updater.new(activity: @activity, params: params)
+    updater.update(step)
 
-    case step
-    when :level
-      @activity.assign_attributes(level: level)
-      UpdateActivityAsFund.new(activity: @activity).call if @activity.fund?
-    when :parent
-      case @activity.level.to_sym
-      when :programme then UpdateActivityAsProgramme.new(activity: @activity, parent_id: parent_id).call
-      when :project then UpdateActivityAsProject.new(activity: @activity, parent_id: parent_id).call
-      when :third_party_project then UpdateActivityAsThirdPartyProject.new(activity: @activity, parent_id: parent_id).call
-      end
-    when :identifier
-      @activity.assign_attributes(delivery_partner_identifier: delivery_partner_identifier)
-    when :roda_identifier
-      @activity.assign_attributes(roda_identifier_fragment: roda_identifier_fragment)
-      @activity.cache_roda_identifier!
-    when :purpose
-      @activity.assign_attributes(title: title, description: description)
-    when :objectives
-      @activity.assign_attributes(objectives: objectives)
-    when :sector_category
-      @activity.assign_attributes(sector_category: sector_category, sector: nil)
-    when :sector
-      @activity.assign_attributes(sector: sector)
-    when :call_present
-      @activity.assign_attributes(call_present: call_present)
-    when :call_dates
-      @activity.assign_attributes(
-        call_open_date: format_date(call_open_date),
-        call_close_date: format_date(call_close_date),
-      )
-    when :total_applications_and_awards
-      @activity.assign_attributes(total_applications: total_applications, total_awards: total_awards)
-    when :programme_status
-      @activity.assign_attributes(programme_status: programme_status)
-    when :country_delivery_partners
-      @activity.assign_attributes(country_delivery_partners: country_delivery_partners)
-    when :dates
-      assign_inputs_on_dates_step
-      if @activity.errors.present?
-        render_step :dates
-        return
-      end
-    when :geography
-      @activity.assign_attributes(geography: geography, recipient_region: nil, recipient_country: nil)
-    when :region
-      @activity.assign_attributes(recipient_region: recipient_region)
-    when :country
-      if recipient_country.blank?
-        @activity.assign_attributes(recipient_country: nil)
-      else
-        inferred_region = country_to_region_mapping.find { |pair| pair["country"] == recipient_country }["region"]
-        @activity.assign_attributes(recipient_region: inferred_region, recipient_country: recipient_country)
-      end
-    when :requires_additional_benefitting_countries
-      @activity.assign_attributes(requires_additional_benefitting_countries: requires_additional_benefitting_countries)
-    when :intended_beneficiaries
-      @activity.assign_attributes(intended_beneficiaries: intended_beneficiaries.drop(1))
-    when :gdi
-      @activity.assign_attributes(gdi: gdi)
-    when :collaboration_type
-      @activity.assign_attributes(collaboration_type: collaboration_type)
-    when :flow
-      @activity.assign_attributes(flow: flow)
-    when :aid_type
-      @activity.assign_attributes(aid_type: aid_type)
-      if can_infer_fstc?(@activity.aid_type)
-        @activity.fstc_applies = fstc_from_aid_type(@activity.aid_type)
-      end
-    when :fstc_applies
-      @activity.assign_attributes(fstc_applies: fstc_applies)
-    when :policy_markers
-      @activity.assign_attributes(
-        policy_marker_gender: policy_markers_iati_codes_to_enum(policy_marker_gender),
-        policy_marker_climate_change_adaptation: policy_markers_iati_codes_to_enum(policy_marker_climate_change_adaptation),
-        policy_marker_climate_change_mitigation: policy_markers_iati_codes_to_enum(policy_marker_climate_change_mitigation),
-        policy_marker_biodiversity: policy_markers_iati_codes_to_enum(policy_marker_biodiversity),
-        policy_marker_desertification: policy_markers_iati_codes_to_enum(policy_marker_desertification),
-        policy_marker_disability: policy_markers_iati_codes_to_enum(policy_marker_disability),
-        policy_marker_disaster_risk_reduction: policy_markers_iati_codes_to_enum(policy_marker_disaster_risk_reduction),
-        policy_marker_nutrition: policy_markers_iati_codes_to_enum(policy_marker_nutrition),
-      )
-    when :covid19_related
-      @activity.assign_attributes(covid19_related: covid19_related)
-    when :gcrf_challenge_area
-      @activity.assign_attributes(gcrf_challenge_area)
-    when :sustainable_development_goals
-      @activity.assign_attributes(sustainable_development_goals)
-
-      unless @activity.sdgs_apply?
-        @activity.assign_attributes(sdg_1: nil, sdg_2: nil, sdg_3: nil)
-      end
-    when :fund_pillar
-      @activity.assign_attributes(fund_pillar: fund_pillar)
-    when :oda_eligibility
-      @activity.assign_attributes(oda_eligibility: oda_eligibility)
-    when :oda_eligibility_lead
-      @activity.assign_attributes(oda_eligibility_lead: oda_eligibility_lead)
-    when :uk_dp_named_contact
-      @activity.assign_attributes(uk_dp_named_contact: uk_dp_named_contact)
+    if step == :dates && @activity.errors.present?
+      render_step :dates
+      return
     end
 
     update_form_state
@@ -182,190 +81,6 @@ class Staff::ActivityFormsController < Staff::BaseController
 
   def activity_id
     params[:activity_id]
-  end
-
-  def level
-    params.require(:activity).permit(:level).fetch("level", nil)
-  end
-
-  def parent_id
-    params.require(:activity).permit(:parent).fetch("parent", nil)
-  end
-
-  def delivery_partner_identifier
-    params.require(:activity).permit(:delivery_partner_identifier).fetch("delivery_partner_identifier", nil)
-  end
-
-  def roda_identifier_fragment
-    params.require(:activity).permit(:roda_identifier_fragment).fetch("roda_identifier_fragment", nil)
-  end
-
-  def sector_category
-    params.require(:activity).permit(:sector_category).fetch("sector_category", nil)
-  end
-
-  def sector
-    params.require(:activity).permit(:sector).fetch("sector", nil)
-  end
-
-  def title
-    params.require(:activity).permit(:title).fetch("title", nil)
-  end
-
-  def description
-    params.require(:activity).permit(:description).fetch("description", nil)
-  end
-
-  def objectives
-    params.require(:activity).permit(:objectives).fetch("objectives", nil)
-  end
-
-  def call_present
-    params.require(:activity).permit(:call_present).fetch("call_present", nil)
-  end
-
-  def call_open_date
-    call_open_date = params.require(:activity).permit(:call_open_date)
-    {day: call_open_date["call_open_date(3i)"], month: call_open_date["call_open_date(2i)"], year: call_open_date["call_open_date(1i)"]}
-  end
-
-  def call_close_date
-    call_close_date = params.require(:activity).permit(:call_close_date)
-    {day: call_close_date["call_close_date(3i)"], month: call_close_date["call_close_date(2i)"], year: call_close_date["call_close_date(1i)"]}
-  end
-
-  def total_applications
-    params.require(:activity).permit(:total_applications).fetch("total_applications", nil)
-  end
-
-  def total_awards
-    params.require(:activity).permit(:total_awards).fetch("total_awards", nil)
-  end
-
-  def programme_status
-    params.require(:activity).permit(:programme_status).fetch("programme_status", nil)
-  end
-
-  def country_delivery_partners
-    params.require(:activity).permit(country_delivery_partners: []).fetch("country_delivery_partners", []).reject(&:blank?)
-  end
-
-  def extract_date_parts_for(date_attr)
-    date_params = params.require(:activity).permit(date_attr)
-    {day: date_params["#{date_attr}(3i)"], month: date_params["#{date_attr}(2i)"], year: date_params["#{date_attr}(1i)"]}
-  end
-
-  def assign_inputs_on_dates_step
-    %i[
-      planned_start_date
-      planned_end_date
-      actual_start_date
-      actual_end_date
-    ].each do |date_attr|
-      @activity.assign_attributes(date_attr => validated_date(extract_date_parts_for(date_attr)))
-    rescue InvalidDateError
-      @activity.errors.add(date_attr, t("activerecord.errors.models.activity.attributes.#{date_attr}.invalid"))
-    end
-  end
-
-  def geography
-    params.require(:activity).permit(:geography).fetch("geography", nil)
-  end
-
-  def recipient_region
-    params.require(:activity).permit(:recipient_region).fetch("recipient_region", nil)
-  end
-
-  def recipient_country
-    params.require(:activity).permit(:recipient_country).fetch("recipient_country", nil)
-  end
-
-  def requires_additional_benefitting_countries
-    params.require(:activity).permit(:requires_additional_benefitting_countries).fetch("requires_additional_benefitting_countries", nil)
-  end
-
-  def intended_beneficiaries
-    params.require(:activity).permit(intended_beneficiaries: []).fetch("intended_beneficiaries", [])
-  end
-
-  def gdi
-    params.require(:activity).permit(:gdi).fetch("gdi", nil)
-  end
-
-  def collaboration_type
-    params.require(:activity).permit(:collaboration_type).fetch("collaboration_type", nil)
-  end
-
-  def flow
-    params.require(:activity).permit(:flow).fetch("flow", nil)
-  end
-
-  def sustainable_development_goals
-    params.require(:activity).permit(:sdg_1, :sdg_2, :sdg_3, :sdgs_apply)
-  end
-
-  def fund_pillar
-    params.require(:activity).permit(:fund_pillar).fetch("fund_pillar", nil)
-  end
-
-  def aid_type
-    params.require(:activity).permit(:aid_type).fetch("aid_type", nil)
-  end
-
-  def fstc_applies
-    params.require(:activity).permit(:fstc_applies).fetch("fstc_applies", nil)
-  end
-
-  def policy_marker_gender
-    params.require(:activity).permit(:policy_marker_gender).fetch("policy_marker_gender", nil)
-  end
-
-  def policy_marker_climate_change_adaptation
-    params.require(:activity).permit(:policy_marker_climate_change_adaptation).fetch("policy_marker_climate_change_adaptation", nil)
-  end
-
-  def policy_marker_climate_change_mitigation
-    params.require(:activity).permit(:policy_marker_climate_change_mitigation).fetch("policy_marker_climate_change_mitigation", nil)
-  end
-
-  def policy_marker_biodiversity
-    params.require(:activity).permit(:policy_marker_biodiversity).fetch("policy_marker_biodiversity", nil)
-  end
-
-  def policy_marker_desertification
-    params.require(:activity).permit(:policy_marker_desertification).fetch("policy_marker_desertification", nil)
-  end
-
-  def policy_marker_disability
-    params.require(:activity).permit(:policy_marker_disability).fetch("policy_marker_disability", nil)
-  end
-
-  def policy_marker_disaster_risk_reduction
-    params.require(:activity).permit(:policy_marker_disaster_risk_reduction).fetch("policy_marker_disaster_risk_reduction", nil)
-  end
-
-  def policy_marker_nutrition
-    params.require(:activity).permit(:policy_marker_nutrition).fetch("policy_marker_nutrition", nil)
-  end
-
-  def covid19_related
-    params.require(:activity).permit(:covid19_related).fetch("covid19_related", 0)
-  end
-
-  def gcrf_challenge_area
-    params.require(:activity).permit(:gcrf_challenge_area)
-  end
-
-  def oda_eligibility
-    params.require(:activity).permit(:oda_eligibility).fetch("oda_eligibility", nil)
-  end
-
-  def oda_eligibility_lead
-    params.require(:activity).permit(:oda_eligibility_lead).fetch("oda_eligibility_lead", nil)
-  end
-
-  def uk_dp_named_contact
-    params.require(:activity).permit(:uk_dp_named_contact).fetch("uk_dp_named_contact", nil)
   end
 
   def finish_wizard_path
@@ -394,11 +109,6 @@ class Staff::ActivityFormsController < Staff::BaseController
   def record_auditable_activity
     action = @activity.form_steps_completed? ? "update" : "create"
     @activity.create_activity key: "activity.#{action}.#{step}", owner: current_user
-  end
-
-  def country_to_region_mapping
-    yaml = YAML.safe_load(File.read("#{Rails.root}/vendor/data/codelists/BEIS/country_to_region_mapping.yml"))
-    yaml["data"]
   end
 
   def assign_default_collaboration_type_value_if_nil
---
+++ app/services/activity/updater.rb
@@ -0,0 +1,188 @@
+class Activity
+  class Updater
+    include DateHelper
+    include CodelistHelper
+    include ActivityHelper
+
+    DEFAULT_PROGRAMME_STATUS_FOR_FUNDS = "spend_in_progress"
+
+    attr_reader :activity, :params
+
+    def initialize(activity:, params:)
+      @activity = activity
+      @params = params
+    end
+
+    def update(step)
+      set_default_programme_status_for_fund if activity.fund?
+
+      updater = "set_#{step}"
+      if respond_to?(updater, :include_private)
+        __send__(updater)
+      else
+        assign_attributes_for_step(step)
+      end
+    end
+
+    private
+
+    def set_default_programme_status_for_fund
+      activity.assign_attributes(programme_status: DEFAULT_PROGRAMME_STATUS_FOR_FUNDS)
+    end
+
+    def set_level
+      activity.assign_attributes(level: params_for("level"))
+      UpdateActivityAsFund.new(activity: activity).call if activity.fund?
+    end
+
+    def set_parent
+      parent_id = params_for("parent")
+
+      case activity.level.to_sym
+      when :programme then UpdateActivityAsProgramme.new(activity: activity, parent_id: parent_id).call
+      when :project then UpdateActivityAsProject.new(activity: activity, parent_id: parent_id).call
+      when :third_party_project then UpdateActivityAsThirdPartyProject.new(activity: activity, parent_id: parent_id).call
+      end
+    end
+
+    def set_identifier
+      assign_attributes_for_step("delivery_partner_identifier")
+    end
+
+    def set_roda_identifier
+      assign_attributes_for_step("roda_identifier_fragment")
+      activity.cache_roda_identifier!
+    end
+
+    def set_purpose
+      activity.assign_attributes(title: params_for("title"), description: params_for("description"))
+    end
+
+    def set_sector_category
+      activity.assign_attributes(sector_category: params_for("sector_category"), sector: nil)
+    end
+
+    def set_call_dates
+      activity.assign_attributes(
+        call_open_date: format_date(date_params_for("call_open_date")),
+        call_close_date: format_date(date_params_for("call_close_date")),
+      )
+    end
+
+    def set_total_applications_and_awards
+      activity.assign_attributes(total_applications: params_for("total_applications"), total_awards: params_for("total_awards"))
+    end
+
+    def set_country_delivery_partners
+      country_delivery_partners = activity_params
+        .permit(country_delivery_partners: [])
+        .fetch("country_delivery_partners", []).reject(&:blank?)
+
+      activity.assign_attributes(country_delivery_partners: country_delivery_partners)
+    end
+
+    def set_dates
+      assign_inputs_on_dates_step
+    end
+
+    def set_geography
+      activity.assign_attributes(geography: params_for("geography"), recipient_region: nil, recipient_country: nil)
+    end
+
+    def set_region
+      activity.assign_attributes(recipient_region: params_for("recipient_region"))
+    end
+
+    def set_country
+      recipient_country = params_for("recipient_country")
+
+      if recipient_country.blank?
+        activity.assign_attributes(recipient_country: nil)
+      else
+        inferred_region = country_to_region_mapping.find { |pair| pair["country"] == recipient_country }["region"]
+        activity.assign_attributes(recipient_region: inferred_region, recipient_country: recipient_country)
+      end
+    end
+
+    def set_intended_beneficiaries
+      intended_beneficiaries = activity_params
+        .permit(intended_beneficiaries: [])
+        .fetch("intended_beneficiaries", []).drop(1)
+      activity.assign_attributes(intended_beneficiaries: intended_beneficiaries)
+    end
+
+    def set_aid_type
+      activity.assign_attributes(aid_type: params_for("aid_type"))
+      if can_infer_fstc?(activity.aid_type)
+        activity.fstc_applies = fstc_from_aid_type(activity.aid_type)
+      end
+    end
+
+    def set_policy_markers
+      activity.assign_attributes(
+        policy_marker_gender: policy_markers_from_params("policy_marker_gender"),
+        policy_marker_climate_change_adaptation: policy_markers_from_params("policy_marker_climate_change_adaptation"),
+        policy_marker_climate_change_mitigation: policy_markers_from_params("policy_marker_climate_change_mitigation"),
+        policy_marker_biodiversity: policy_markers_from_params("policy_marker_biodiversity"),
+        policy_marker_desertification: policy_markers_from_params("policy_marker_desertification"),
+        policy_marker_disability: policy_markers_from_params("policy_marker_disability"),
+        policy_marker_disaster_risk_reduction: policy_markers_from_params("policy_marker_disaster_risk_reduction"),
+        policy_marker_nutrition: policy_markers_from_params("policy_marker_nutrition"),
+      )
+    end
+
+    def set_covid19_related
+      activity.assign_attributes(covid19_related: params_for("covid19_related", 0))
+    end
+
+    def set_sustainable_development_goals
+      activity.assign_attributes(activity_params.permit(:sdg_1, :sdg_2, :sdg_3, :sdgs_apply))
+      unless activity.sdgs_apply?
+        activity.assign_attributes(sdg_1: nil, sdg_2: nil, sdg_3: nil)
+      end
+    end
+
+    def assign_attributes_for_step(step)
+      activity.assign_attributes({step => params_for(step)})
+    end
+
+    def policy_markers_from_params(param)
+      policy_markers_iati_codes_to_enum(params_for(param))
+    end
+
+    def activity_params
+      params.require(:activity)
+    end
+
+    def params_for(attribute, default = nil)
+      activity_params.permit(attribute).fetch(attribute, default)
+    end
+
+    def date_params_for(attribute)
+      date = activity_params.permit(attribute)
+      {
+        day: date["#{attribute}(3i)"],
+        month: date["#{attribute}(2i)"],
+        year: date["#{attribute}(1i)"],
+      }
+    end
+
+    def assign_inputs_on_dates_step
+      %i[
+        planned_start_date
+        planned_end_date
+        actual_start_date
+        actual_end_date
+      ].each do |date_attr|
+        activity.assign_attributes(date_attr => validated_date(date_params_for(date_attr)))
+      rescue InvalidDateError
+        activity.errors.add(date_attr, I18n.t("activerecord.errors.models.activity.attributes.#{date_attr}.invalid"))
+      end
+    end
+
+    def country_to_region_mapping
+      yaml = YAML.safe_load(File.read("#{Rails.root}/vendor/data/codelists/BEIS/country_to_region_mapping.yml"))
+      yaml["data"]
+    end
+  end
+end
