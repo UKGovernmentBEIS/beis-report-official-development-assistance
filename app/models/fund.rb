564082707435c2d6046e53bd50ba1ad2ca9d2519
Stuart Harrison <pezholio@gmail.com>
2021-02-05 10:48:45 +0000

    Add a Fund class
    This allows us to easily get a fund's name and ID from a given fund Activity

---
+++ app/models/fund.rb
@@ -0,0 +1,49 @@
+class Fund
+  attr_reader :code
+
+  class InvalidActivity < StandardError; end
+  class InvalidFund < StandardError; end
+
+  MAPPINGS = {
+    "NF" => 1,
+    "GCRF" => 2,
+  }
+
+  def initialize(id)
+    @code = self.class.codelist.find { |c| c["code"] == id }
+
+    raise InvalidFund if @code.nil?
+  end
+
+  def id
+    code["code"]
+  end
+
+  def name
+    code["name"]
+  end
+
+  class << self
+    def from_activity(activity)
+      raise InvalidActivity unless activity.fund?
+
+      id = MAPPINGS.fetch(activity.roda_identifier_fragment) { raise InvalidFund }
+
+      new(id)
+    end
+
+    def all
+      valid_codes.map { |c| new(code) }
+    end
+
+    def codelist
+      Codelist.new(type: "fund_types", source: "beis")
+    end
+
+    private
+
+    def valid_codes
+      codelist.values_for("code")
+    end
+  end
+end
