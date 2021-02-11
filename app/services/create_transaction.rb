120685b167b10fd949d7d966c0e20d0220558c45
Stuart Harrison <pezholio@gmail.com>
2021-02-11 16:24:10 +0000

    Set a default description if one is not present
    The default description is set using the financial quarter and year, and the activity title.

--- app/services/create_transaction.rb
+++ app/services/create_transaction.rb
@@ -1,18 +1,19 @@
 class CreateTransaction
-  attr_accessor :activity, :report
+  attr_accessor :activity, :report, :transaction
 
   def initialize(activity:, report: nil)
     self.activity = activity
     self.report = report || Report.editable_for_activity(activity)
+    self.transaction = Transaction.new
   end
 
   def call(attributes: {})
-    transaction = Transaction.new
-
     transaction.parent_activity = activity
     transaction.assign_attributes(attributes)
 
     convert_and_assign_value(transaction, attributes[:value])
+
+    transaction.description = default_description if transaction.description.nil?
 
     unless activity.organisation.service_owner?
       transaction.report = report
@@ -33,5 +34,11 @@ class CreateTransaction
     transaction.value = ConvertFinancialValue.new.convert(value.to_s)
   rescue ConvertFinancialValue::Error
     transaction.errors.add(:value, I18n.t("activerecord.errors.models.transaction.attributes.value.not_a_number"))
+  end
+
+  def default_description
+    return nil unless transaction.date.present?
+
+    "#{transaction.financial_quarter_and_year} spend on #{activity.title}"
   end
 end
