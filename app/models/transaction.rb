7e76674e18e7a7cbdb27a5a0f625963f9c2b00da
James Coglan <james.coglan@dxw.com>
2021-02-16 12:29:53 +0000

    Set a transaction's financial quarter from its date
    We're going to migrate transactions from having an exact date, to having
    a financial quarter. To do this we need to set the financial quarter for
    existing transactions, based on their date. Once the system is assigning
    financial quarters for all created/updated transactions, we can
    back-fill this data for all the others.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -17,9 +17,22 @@ class Transaction < ApplicationRecord
   validates :value, numericality: {other_than: 0, less_than_or_equal_to: 99_999_999_999.00}
   validates :date, date_not_in_future: true, date_within_boundaries: true
 
+  before_save :set_financial_quarter_from_date
+
   def financial_quarter_and_year
     return nil if date.blank?
 
     FinancialQuarter.for_date(date).to_s
+  end
+
+  private
+
+  def set_financial_quarter_from_date
+    return if date.blank?
+    return if financial_quarter.present? && financial_year.present?
+
+    financial_quarter = FinancialQuarter.for_date(date)
+    self.financial_quarter = financial_quarter.quarter
+    self.financial_year = financial_quarter.financial_year.start_year
   end
 end
