c80425f9ece2763f5f8b4a4a8061d0c0c05c90c0
Stuart Harrison <pezholio@gmail.com>
2021-03-15 13:43:20 +0000

    Refactor UserMailer tests to reflect other tests
    There was a lot of stubbing out in the original UserMailer-related tests, which we
    can refactor out with a combination of testing the queue and using a new matcher
    to check the personalisations that would have been sent to Notify in production.

--- spec/services/send_welcome_email_spec.rb
+++ spec/services/send_welcome_email_spec.rb
@@ -1,15 +1,13 @@
 require "rails_helper"
 
 RSpec.describe SendWelcomeEmail do
-  let(:user) { build_stubbed(:administrator) }
-  before(:each) do
-    stub_welcome_email_delivery
-  end
+  let(:user) { create(:administrator) }
 
   describe "#call" do
     it "enqueues the welcome email to be sent" do
-      expect_welcome_email_delivery(user: user)
-      described_class.new(user: user).call
+      expect {
+        described_class.new(user: user).call
+      }.to have_enqueued_mail(UserMailer, :welcome).with(args: [user])
     end
   end
 end
--- spec/support/email_helpers.rb
+++ spec/support/email_helpers.rb
@@ -1,24 +1,4 @@
 module EmailHelpers
-  def stub_welcome_email_delivery
-    welcome_email_delivery = instance_double(ActionMailer::MessageDelivery)
-    allow(UserMailer).to receive(:welcome)
-      .and_return(welcome_email_delivery)
-    allow(welcome_email_delivery).to receive(:deliver_later)
-  end
-
-  def expect_welcome_email_delivery(user: nil)
-    welcome_email_delivery = instance_double(ActionMailer::MessageDelivery)
-    if user
-      expect(UserMailer).to receive(:welcome)
-        .with(user)
-        .and_return(welcome_email_delivery)
-    else
-      expect(UserMailer).to receive(:welcome)
-        .and_return(welcome_email_delivery)
-    end
-    expect(welcome_email_delivery).to receive(:deliver_later)
-  end
-
   RSpec::Matchers.define :be_sent_email do
     match do |actual|
       email = emails.find { |email| email.to == [actual.email] }
@@ -33,6 +13,24 @@ module EmailHelpers
   RSpec::Matchers.define :with_subject do |expected|
     match do |actual|
       email.subject == expected
+    end
+
+    def emails
+      ActionMailer::Base.deliveries
+    end
+
+    def email
+      emails.find { |email| email.to == [actual.email] }
+    end
+  end
+
+  RSpec::Matchers.define :with_personalisations do |expected|
+    match do |actual|
+      personalisation["unparsed_value"] == expected
+    end
+
+    def personalisation
+      JSON.parse(email.to_json)["header"].find { |header| header["name"] == "personalisation" }
     end
 
     def emails
