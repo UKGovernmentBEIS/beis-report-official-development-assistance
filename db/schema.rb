41de56564bddc0677d5942a0dbd46e2b60565498
Robert Lee-Cann <leeky@leeky.org.uk>
2021-03-16 10:41:01 +0000

    Copy extending_organisation from organisation
    In order to infer `accountable_organisation` from
    `extending_organisation` we first need to make sure this attribute is
    populated correctly.
    
    I spotted a few occasions (legacy data) where `extending_organisation`
    was empty. In these cases, this value can be copied over from
    `organisation`.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_082933) do
+ActiveRecord::Schema.define(version: 2021_03_16_102910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
