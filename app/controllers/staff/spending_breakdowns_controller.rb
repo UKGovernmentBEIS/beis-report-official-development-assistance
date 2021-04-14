dc46edb96a1f6f7b5a588692c55162597e5e0fd9
James Coglan <james.coglan@dxw.com>
2021-04-14 17:42:57 +0100

    Download a spending breakdown CSV
    This adds a link next to 'Download report as CSV' that provides a second
    download with a different breakdown of the financial information. For
    each activity, it includes:
    
    - RODA, BEIS and DP identifiers
    - title and description
    - programme status and ODA eligibility
    - for the most recent 7 quarters, the total spend, total refund and net
    - for 13 quarters before those, the total net spend
    - for 5 quarters after the report date, the forecast spend

---
+++ app/controllers/staff/spending_breakdowns_controller.rb
@@ -0,0 +1,30 @@
+class Staff::SpendingBreakdownsController < Staff::BaseController
+  include Secured
+  include StreamCsvDownload
+
+  def show
+    @report = Report.find(params[:report_id])
+    authorize @report
+
+    @report_presenter = ReportPresenter.new(@report)
+    @report_activities = Activity.projects_and_third_party_projects_for_report(@report)
+
+    respond_to do |format|
+      format.csv { send_csv }
+    end
+  end
+
+  private
+
+  def send_csv
+    filename = @report_presenter.filename_for_report_download
+    headers = ActivitySpendingBreakdown.new(report: @report).headers
+
+    stream_csv_download(filename: filename, headers: headers) do |csv|
+      @report_activities.each do |activity|
+        breakdown = ActivitySpendingBreakdown.new(report: @report, activity: activity)
+        csv << breakdown.values
+      end
+    end
+  end
+end
