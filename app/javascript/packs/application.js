d62d855be31e5dc61c45de5e06575e01b983fbce
James Coglan <james.coglan@dxw.com>
2021-04-22 11:57:23 +0100

    Move our own JavaScript into Webpacker
    Here we move the custom JS code we have in the application from
    `app/assets/javascripts` to `app/javascript/packs`, so that it gets
    compiled by Webpack rather than by the legacy Rails asset pipeline.
    
    JavaScript source code lives in `app/javascript/src`. Files in
    `app/javascript/packs` define bundles of JS that are actually loaded
    into our pages; these `packs` files should import files from `src`.
    
    Files in `packs` can be loaded into pages using the
    `javascript_pack_tag` view helper.
    
    Third-party JS is still loaded using `//= require` comments in the
    original application file and are processed by the asset pipeline.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,5 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import cookieConsent from "../src/cookie-consent"
+import toggleProvidingOrgFields from "../src/toggle-providing-org-fields"
