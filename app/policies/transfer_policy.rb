1325aed12b24cb691f228a7003400cfb7074dfbc
Cristina <cristina@dxw.com>
2021-05-18 20:31:43 +0100

    The first iteration of the transfer policy
    Previously, only BEIS users were allowed to modify transfers,
    and their permissions were extensive across all activity levels.
    
    The main changes are:
    - Opening up transfer permissions to delivery partner users at levels
    C and D (projects and third-party projects respectively)
    - BEIS users can no longer create or edit transfers at levels C and D.
    
    Co-authored-by: jcoglan <james.coglan@dxw.com>

--- app/policies/transfer_policy.rb
+++ app/policies/transfer_policy.rb
@@ -1,13 +1,37 @@
 class TransferPolicy < ApplicationPolicy
   def show?
-    beis_user?
+    return true if beis_user?
+
+    if record.source.project? || record.source.third_party_project?
+      user.organisation == record.source.organisation
+    end
   end
 
   def create?
-    beis_user?
+    return beis_user? if record.source.fund? || record.source.programme?
+
+    if delivery_partner_user?
+      user.organisation == record.source.organisation && editable_report_for_organisation_and_fund.present?
+    end
   end
 
   def update?
-    beis_user?
+    return beis_user? if record.source.fund? || record.source.programme?
+
+    if delivery_partner_user?
+      user.organisation == record.source.organisation && record.report&.editable?
+    end
+  end
+
+  def destroy?
+    return beis_user? if record.source.programme?
+
+    if delivery_partner_user?
+      user.organisation == record.source.organisation && record.report&.editable?
+    end
+  end
+
+  private def editable_report_for_organisation_and_fund
+    Report.editable_for_activity(record.source)
   end
 end
