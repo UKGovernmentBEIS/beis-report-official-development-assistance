f03940043bc3a2492c630de7c7f11117d2af6ae9
James Coglan <james.coglan@dxw.com>
2021-05-20 15:59:22 +0100

    Use BEIS as providing org for rolled up forecasts
    Rolled up forecasts are an aggregate of individual forecasts, and as
    such they don't necessarily have the same providing organisation.
    
    For IATI reporting purposes, it has been decided to hardcode certain values,
    such as the type (as `original`) and the providing organisation (as BEIS).
    
    Co-authored-by: CristinaRO <cristina@dxw.com>

--- app/presenters/planned_disbursement_aggregate.rb
+++ app/presenters/planned_disbursement_aggregate.rb
@@ -1,6 +1,5 @@
 class PlannedDisbursementAggregate
   delegate :start_date, :end_date, to: :@financial_quarter, prefix: :period
-  delegate :providing_organisation_type, :providing_organisation_name, :providing_organisation_reference, to: "@planned_disbursements.first"
 
   def initialize(financial_quarter, planned_disbursements)
     @financial_quarter = financial_quarter
@@ -17,5 +16,21 @@ class PlannedDisbursementAggregate
 
   def value
     @planned_disbursements.sum(&:value)
+  end
+
+  def providing_organisation_name
+    providing_organisation.name
+  end
+
+  def providing_organisation_type
+    providing_organisation.organisation_type
+  end
+
+  def providing_organisation_reference
+    providing_organisation.iati_reference
+  end
+
+  private def providing_organisation
+    @_providing_organisation ||= Organisation.service_owner
   end
 end
