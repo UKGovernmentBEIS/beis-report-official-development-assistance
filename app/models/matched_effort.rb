3690eae55b2f651109a81619f1d0d888108a5367
Stuart Harrison <pezholio@gmail.com>
2021-05-25 09:52:13 +0100

    Add some basic validations to MatchedEffort

--- app/models/matched_effort.rb
+++ app/models/matched_effort.rb
@@ -4,4 +4,6 @@ class MatchedEffort < ApplicationRecord
 
   enum funding_type: Codelist.new(type: "matched_effort_funding_type", source: "beis").hash_of_coded_names
   enum category: Codelist.new(type: "matched_effort_category", source: "beis").hash_of_coded_names
+
+  validates_presence_of :organisation_id, :funding_type, :category, :committed_amount, :currency, :exchange_rate, :date_of_exchange_rate
 end
--- spec/models/matched_effort_spec.rb
+++ spec/models/matched_effort_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe MatchedEffort, type: :model do
     it { should belong_to(:activity) }
     it { should belong_to(:organisation) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:organisation_id) }
+    it { should validate_presence_of(:funding_type) }
+    it { should validate_presence_of(:category) }
+    it { should validate_presence_of(:committed_amount) }
+    it { should validate_presence_of(:currency) }
+    it { should validate_presence_of(:exchange_rate) }
+    it { should validate_presence_of(:date_of_exchange_rate) }
+  end
 end
