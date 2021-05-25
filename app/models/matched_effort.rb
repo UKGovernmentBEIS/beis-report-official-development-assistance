495ce2a6800b8a741d17a02a76a893197a9197e6
Stuart Harrison <pezholio@gmail.com>
2021-05-25 09:29:39 +0100

    Introduce structs for matched effort categories
    This makes the validation logic simpler, and will also help
    us build out the form options in the next commit

--- app/models/matched_effort.rb
+++ app/models/matched_effort.rb
@@ -13,4 +13,44 @@ class MatchedEffort < ApplicationRecord
 
   validates_with MatchedEffortOrganisationValidator, if: -> { organisation_id.present? }
   validates_with MatchedEffortCategoryValidator
+
+  FundingType = Struct.new(:code, :name) {
+    class << self
+      def all
+        FUNDING_TYPE_CODES.map { |type|
+          new(type["code"], type["name"])
+        }
+      end
+
+      def by_coded_name(coded_name)
+        all.find { |type| type.coded_name == coded_name }
+      end
+    end
+
+    def coded_name
+      MatchedEffort.funding_types.invert[code]
+    end
+
+    def categories
+      Category.all.select { |category| category.funding_type == code }
+    end
+  }
+
+  Category = Struct.new(:code, :name, :funding_type) {
+    class << self
+      def all
+        CATEGORY_CODES.map { |category|
+          new(category["code"], category["name"], category["funding_type"])
+        }
+      end
+
+      def by_coded_name(coded_name)
+        all.find { |type| type.coded_name == coded_name }
+      end
+    end
+
+    def coded_name
+      MatchedEffort.categories.invert[code]
+    end
+  }
 end
--- app/validators/matched_effort_category_validator.rb
+++ app/validators/matched_effort_category_validator.rb
@@ -2,19 +2,18 @@ class MatchedEffortCategoryValidator < ActiveModel::Validator
   def validate(matched_effort)
     return if matched_effort.funding_type.blank? || matched_effort.category.blank?
 
-    type_code = MatchedEffort.funding_types[matched_effort.funding_type]
-    category_code = MatchedEffort.categories[matched_effort.category]
+    funding_type = MatchedEffort::FundingType.by_coded_name(matched_effort.funding_type)
+    category = MatchedEffort::Category.by_coded_name(matched_effort.category)
 
-    category_data = MatchedEffort::CATEGORY_CODES.list.find { |c| c["code"] == category_code }
-    funding_type_data = MatchedEffort::FUNDING_TYPE_CODES.list.find { |c| c["code"] == type_code }
+    category_type_codes = funding_type.categories.map(&:coded_name)
 
-    unless category_data["funding_type"] == type_code
+    unless category_type_codes.include?(matched_effort.category)
       matched_effort.category = nil
       matched_effort.errors.add(:category,
         I18n.t(
           "activerecord.errors.models.matched_effort.attributes.category.invalid",
-          category: category_data["name"],
-          funding_type: funding_type_data["name"]
+          category: category.name,
+          funding_type: funding_type.name
         ))
     end
   end
