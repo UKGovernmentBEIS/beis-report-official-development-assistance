136ca5cc8ecb0862559a5858ace0648126d46637
Stuart Harrison <pezholio@gmail.com>
2021-06-09 11:22:27 +0100

    Use a single policy for Transfers
    Because creating incoming and outgoing transfers requires the same
    permissions, we're creating a concern that we can share between the two
    policies.

---
+++ app/policies/concerns/transfer_policy.rb
@@ -0,0 +1,39 @@
+module TransferPolicy
+  extend ActiveSupport::Concern
+
+  def show?
+    return true if beis_user?
+
+    if target_activity.project? || target_activity.third_party_project?
+      user.organisation == target_activity.organisation
+    end
+  end
+
+  def create?
+    return beis_user? if target_activity.fund? || target_activity.programme?
+
+    if delivery_partner_user?
+      user.organisation == target_activity.organisation && editable_report_for_organisation_and_fund.present?
+    end
+  end
+
+  def update?
+    return beis_user? if target_activity.fund? || target_activity.programme?
+
+    if delivery_partner_user?
+      user.organisation == target_activity.organisation && record.report&.editable?
+    end
+  end
+
+  def destroy?
+    return beis_user? if target_activity.programme?
+
+    if delivery_partner_user?
+      user.organisation == target_activity.organisation && record.report&.editable?
+    end
+  end
+
+  private def editable_report_for_organisation_and_fund
+    Report.editable_for_activity(target_activity)
+  end
+end
--- app/policies/outgoing_transfer_policy.rb
+++ app/policies/outgoing_transfer_policy.rb
@@ -1,37 +1,7 @@
 class OutgoingTransferPolicy < ApplicationPolicy
-  def show?
-    return true if beis_user?
+  include TransferPolicy
 
-    if record.source.project? || record.source.third_party_project?
-      user.organisation == record.source.organisation
-    end
-  end
-
-  def create?
-    return beis_user? if record.source.fund? || record.source.programme?
-
-    if delivery_partner_user?
-      user.organisation == record.source.organisation && editable_report_for_organisation_and_fund.present?
-    end
-  end
-
-  def update?
-    return beis_user? if record.source.fund? || record.source.programme?
-
-    if delivery_partner_user?
-      user.organisation == record.source.organisation && record.report&.editable?
-    end
-  end
-
-  def destroy?
-    return beis_user? if record.source.programme?
-
-    if delivery_partner_user?
-      user.organisation == record.source.organisation && record.report&.editable?
-    end
-  end
-
-  private def editable_report_for_organisation_and_fund
-    Report.editable_for_activity(record.source)
+  def target_activity
+    record.source
   end
 end
