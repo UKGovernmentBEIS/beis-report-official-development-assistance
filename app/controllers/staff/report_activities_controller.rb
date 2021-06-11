45de893f5f45ee215e9a050d7ba2cf006dc00784
Cristina <cristina@dxw.com>
2021-06-11 19:13:43 +0100

    Populate Activities tab
    Show projects and third-party projects for the report’s fund and organisation.
    
    These are the same criteria for the activities used to populate the Variance tab, and the activities that are included in the report as CSV download.

--- app/controllers/staff/report_activities_controller.rb
+++ app/controllers/staff/report_activities_controller.rb
@@ -9,6 +9,8 @@ class Staff::ReportActivitiesController < Staff::BaseController
 
     @report_presenter = ReportPresenter.new(@report)
 
+    @activities = Activity.projects_and_third_party_projects_for_report(@report).order(:title, :roda_identifier_fragment)
+
     render "staff/reports/activities"
   end
 end
