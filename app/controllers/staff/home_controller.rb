06c2949ebc66b314db2684a8496f9b0ffa988703
meyric <meyric@dxw.com>
2021-07-01 12:10:44 +0100

    Show the hierarchical list of activities
    On the delivery partner home page we show the hierarchical list of
    activities so that a user can quickly browse to an activity record.
    
    We've reused the existing partial for this which means duplicating the
    `fetch_grouped_programmes_for` method in the home controller. It feels
    like this method might be better served as a scope on the Activity
    model, but we decided to keep the duplication for now as the method also
    results in some unexpected child activities are shown (those in
    incorrect statuses) so we will look to fix those issues together as
    future work.

--- app/controllers/staff/home_controller.rb
+++ app/controllers/staff/home_controller.rb
@@ -7,7 +7,22 @@ class Staff::HomeController < Staff::BaseController
       authorize @delivery_partner_organisations
       render :service_owner
     else
+      @grouped_programmes = fetch_grouped_programmes_for(current_user.organisation, :current)
       render :delivery_partner
     end
+  end
+
+  private def fetch_grouped_programmes_for(organisation, scope)
+    activities = policy_scope(
+      Activity.includes(
+        :organisation,
+        parent: [:parent, :organisation],
+        child_activities: [:child_activities, :organisation, :parent]
+      ).programme
+       .send(scope)
+    )
+    activities = activities.where(extending_organisation: organisation)
+    activities.order(:roda_identifier_compound)
+      .group_by(&:parent)
   end
 end
