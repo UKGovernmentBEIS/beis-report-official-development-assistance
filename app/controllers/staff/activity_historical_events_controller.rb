2275464a4be94c68ef8c294f6c84252f22bf16a2
Ed Davey <ed@dxw.com>
2021-07-05 22:13:43 +0100

    Group historical events according to reference

--- app/controllers/staff/activity_historical_events_controller.rb
+++ app/controllers/staff/activity_historical_events_controller.rb
@@ -7,6 +7,13 @@ class Staff::ActivityHistoricalEventsController < Staff::BaseController
     activity = Activity.find(params[:activity_id])
     authorize activity
     @activity = ActivityPresenter.new(activity)
-    @historical_events = @activity.historical_events.includes([:user, :report])
+    @historical_events = @activity
+      .historical_events
+      .includes([:user, :report])
+      .group_by { |event|
+        {reference: event.reference,
+         user: event.user&.email,
+         timestamp: event.created_at.strftime("%d %b %Y at %R"),}
+      }
   end
 end
