4a487a3d2871b0b91bbb07cf1493cc409caef519
pezholio <pezholio@gmail.com>
2021-07-08 14:01:24 +0100

    Rationalise and tweak the tests
    Now we’re using a fetcher object to fetch the activities,
    we can rationalise the tests to remove some unnecessary
    ones (where we’re already testing the main object), and
    move a bunch of tests to the controller tests, where we
    can stub and mock, rather than create objects in the
    database.

---
+++ spec/controllers/staff/activities_controller_spec.rb
@@ -0,0 +1,116 @@
+require "rails_helper"
+
+RSpec.describe Staff::ActivitiesController do
+  shared_examples "fetches activities" do |params|
+    let(:scope) { params[:scope] }
+    let(:route) { params[:route] }
+
+    let(:fund) { build_stubbed(:fund_activity) }
+    let(:programme) { build_stubbed(:programme_activity) }
+    let(:project) { build_stubbed(:project_activity) }
+    let(:third_party_projects) { build_stubbed_list(:third_party_project_activity, 2) }
+    let(:activities) do
+      {
+        fund => {
+          programme => {
+            project => third_party_projects,
+          },
+        },
+      }
+    end
+
+    let(:fetcher) { instance_double(Activity::GroupedActivitiesFetcher, call: activities) }
+
+    before do
+      allow(controller).to receive(:current_user).and_return(user)
+      allow(controller).to receive(:logged_in_using_omniauth?).and_return(true)
+      allow(Activity::GroupedActivitiesFetcher).to receive(:new).and_return(fetcher)
+    end
+
+    context "when signed in as a BEIS user" do
+      let(:user) { create(:beis_user) }
+
+      it "defaults to fetching the BEIS organisation's activities" do
+        get route
+
+        expect(Activity::GroupedActivitiesFetcher).to have_received(:new).with(
+          user: user,
+          organisation: user.organisation,
+          scope: scope
+        )
+      end
+
+      it "allows fetching of another organisation's activites" do
+        organisation = create(:delivery_partner_organisation)
+
+        get route, params: {organisation_id: organisation.id}
+
+        expect(Activity::GroupedActivitiesFetcher).to have_received(:new).with(
+          user: user,
+          organisation: organisation,
+          scope: scope
+        )
+      end
+
+      it "assigns the activities correctly" do
+        get route
+
+        expect(assigns(:grouped_activities)).to eq(activities)
+      end
+
+      it "defaults to the BEIS organisation when the organisation parameter is blank" do
+        get route, params: {organisation_id: ""}
+
+        expect(Activity::GroupedActivitiesFetcher).to have_received(:new).with(
+          user: user,
+          organisation: user.organisation,
+          scope: scope
+        )
+      end
+
+      it "defaults to the BEIS organisation when the organisation parameter is not a known organisation" do
+        get route, params: {organisation_id: "this-is-not-a-know-organisation"}
+
+        expect(Activity::GroupedActivitiesFetcher).to have_received(:new).with(
+          user: user,
+          organisation: user.organisation,
+          scope: scope
+        )
+      end
+    end
+
+    context "when signed in as a delivery partner" do
+      let(:user) { create(:delivery_partner_user) }
+
+      it "fetches the activities for the user's organisation" do
+        get route
+
+        expect(Activity::GroupedActivitiesFetcher).to have_received(:new).with(
+          user: user,
+          organisation: user.organisation,
+          scope: scope
+        )
+      end
+
+      it "fetches the activities for the user's organisation when the organisation ID is that of another organisation" do
+        organisation = create(:delivery_partner_organisation)
+
+        get route, params: {organisation_id: organisation.id}
+
+        expect(Activity::GroupedActivitiesFetcher).to have_received(:new).with(
+          user: user,
+          organisation: user.organisation,
+          scope: scope
+        )
+      end
+    end
+  end
+
+  describe "#index" do
+    include_examples "fetches activities", {route: :index, scope: :current}
+  end
+
+  describe "#historic" do
+    include_examples "fetches activities", {route: :historic, scope: :historic}
+  end
+end
