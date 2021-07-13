89e6fe851850b5a73a782b2451aae7d1df48b094
meyric <meyric@dxw.com>
2021-07-13 15:24:43 +0100

    Split the user can view activities spec
    The spec covers both viewing multiple activities and view an individual
    activity which makes for a big spec file and can be overwhelming to
    read.
    
    Here we take out the specs that deal with an activity details and create
    it's own file.

--- spec/features/staff/users_can_view_activities_spec.rb
+++ spec/features/staff/users_can_view_activities_spec.rb
@@ -132,18 +132,6 @@ RSpec.feature "Users can view activities" do
         expect(page).to have_content budget.value
       end
 
-      scenario "the activity details can be viewed" do
-        activity = create(:project_activity, organisation: user.organisation)
-
-        visit organisation_activity_details_path(activity.organisation, activity)
-
-        within ".govuk-tabs__list-item--selected" do
-          expect(page).to have_content "Details"
-        end
-        expect(page).to have_content activity.title
-        expect(page).to have_link t("page_content.activity.implementing_organisation.button.new")
-      end
-
       scenario "an activity can be viewed" do
         programme = create(:programme_activity, extending_organisation: user.organisation)
         activity = create(:project_activity, parent: programme, organisation: user.organisation, sdgs_apply: true, sdg_1: 5)
@@ -167,35 +155,6 @@ RSpec.feature "Users can view activities" do
 
         within ".sustainable_development_goals" do
           expect(page).to have_content "Gender Equality"
-        end
-      end
-
-      scenario "activities have human readable date format" do
-        travel_to Time.zone.local(2020, 1, 29) do
-          activity = create(:project_activity, planned_start_date: Date.new(2020, 2, 3),
-                                               planned_end_date: Date.new(2024, 6, 22),
-                                               actual_start_date: Date.new(2020, 1, 2),
-                                               actual_end_date: Date.new(2020, 1, 29),
-                                               organisation: user.organisation)
-
-          visit organisation_activity_path(user.organisation, activity)
-          click_on t("tabs.activity.details")
-
-          within(".planned_start_date") do
-            expect(page).to have_content("3 Feb 2020")
-          end
-
-          within(".planned_end_date") do
-            expect(page).to have_content("22 Jun 2024")
-          end
-
-          within(".actual_start_date") do
-            expect(page).to have_content("2 Jan 2020")
-          end
-
-          within(".actual_end_date") do
-            expect(page).to have_content("29 Jan 2020")
-          end
         end
       end
     end
