976036256884e104ef33475910e425ebd22b6ade
pezholio <pezholio@gmail.com>
2021-07-23 09:45:16 +0100

    Add a fetcher to fetch reports for an organisation

---
+++ app/services/report/organisation_reports_fetcher.rb
@@ -0,0 +1,30 @@
+class Report
+  class OrganisationReportsFetcher
+    def initialize(organisation:)
+      @organisation = organisation
+    end
+
+    def current
+      @current ||= fetch(reports.not_approved)
+    end
+
+    def historic
+      @historic ||= fetch(reports.approved)
+    end
+
+    private
+
+    def reports
+      Report.where(organisation: organisation)
+    end
+
+    private def fetch(relation)
+      relation
+        .includes([:fund])
+        .order("financial_year, financial_quarter DESC")
+        .map { |report| ReportPresenter.new(report) }
+    end
+
+    attr_reader :organisation
+  end
+end
