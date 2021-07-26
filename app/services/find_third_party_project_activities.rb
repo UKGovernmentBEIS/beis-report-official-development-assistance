676be2d07f743e89a9a6d26bd9bd5f4079d52d93
pezholio <pezholio@gmail.com>
2021-07-26 13:43:30 +0100

    Filter third party projects by fund optionally

--- app/services/find_third_party_project_activities.rb
+++ app/services/find_third_party_project_activities.rb
@@ -1,15 +1,16 @@
 class FindThirdPartyProjectActivities
   include Pundit
 
-  attr_accessor :organisation, :user
+  attr_accessor :organisation, :user, :fund_code
 
-  def initialize(organisation:, user:)
+  def initialize(organisation:, user:, fund_code: nil)
     @organisation = organisation
     @user = user
+    @fund_code = fund_code
   end
 
   def call
-    third_party_projects = ThirdPartyProjectPolicy::Scope.new(user, Activity.third_party_project)
+    third_party_projects = ThirdPartyProjectPolicy::Scope.new(user, projects_scope)
       .resolve
       .includes(:organisation, :extending_organisation, :implementing_organisations, :budgets, :parent)
       .order("created_at ASC")
@@ -19,5 +20,11 @@ class FindThirdPartyProjectActivities
     else
       third_party_projects.where(organisation_id: organisation.id)
     end
+  end
+
+  private
+
+  def projects_scope
+    fund_code.nil? ? Activity.third_party_project : Activity.third_party_project.where(source_fund_code: fund_code)
   end
 end
