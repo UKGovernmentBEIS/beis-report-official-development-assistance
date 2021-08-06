7b8d7fd8a3455e65590a6eb423902891407c4a10
pezholio <pezholio@gmail.com>
2021-08-06 16:51:20 +0100

    Dig down from other subregions
    If a subregion is a higher specificity, we need to use
    the region to subregion mapping to get all the
    countries within those subregions. There are still
    some activities that don't get saved, because their
    region is set to `Developing countries, unspecified`.
    We need to make a decision around what to do with
    this.

--- db/data/20210804154421_backfill_benefitting_countries.rb
+++ db/data/20210804154421_backfill_benefitting_countries.rb
@@ -1,6 +1,7 @@
 # Run me with `rails runner db/data/20210804154421_backfill_benefitting_countries.rb`
 
 benefitting_countries_hash = Codelist.new(type: "intended_beneficiaries").list
+region_to_subregion_hash = Codelist.new(type: "region_to_subregion_mapping", source: "beis").list
 activities_to_update = Activity.where.not(geography: nil).where(benefitting_countries: nil)
 total_activities_to_update = activities_to_update.count
 
@@ -18,7 +19,25 @@ activities_to_update.each_with_index do |activity, counter|
   elsif activity.geography == "recipient_region"
     recipient_region = activity.recipient_region
 
-    countries = benefitting_countries_hash.fetch(recipient_region, [])
+    next if recipient_region.nil? || recipient_region == "998"
+
+    recipient_regions = if !benefitting_countries_hash.key?(recipient_region)
+      regions = [recipient_region]
+      loop do
+        subregions = regions.map { |r| region_to_subregion_hash.fetch(r) }.flatten
+
+        break subregions if benefitting_countries_hash.keys.any? { |r| subregions.include?(r) }
+
+        regions = subregions
+      end
+    else
+      [recipient_region]
+    end
+
+    countries = recipient_regions.map { |region|
+      benefitting_countries_hash.fetch(region, [])
+    }.flatten
+
     activity.benefitting_countries = countries.map { |c| c["code"] }
     activity.save(validate: false)
   end
