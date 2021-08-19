83412974f5d72e0f3afd23ab5c63b46f3623263b
pezholio <pezholio@gmail.com>
2021-08-19 14:55:14 +0100

    Add breadcrumbs for comments
    This adds breadcrumbs for editing comments in
    the context of the Activities page. Adding
    comments happens from the reports page, so I've
    added a `prepare_default_report_variance_trail`
    which adds a breadcrumb for the variance tab
    (where the add comments page is linked from)

--- spec/controllers/concerns/reports/breadcrumbed_spec.rb
+++ spec/controllers/concerns/reports/breadcrumbed_spec.rb
@@ -16,45 +16,68 @@ RSpec.describe StubController, type: :controller do
   before do
     allow(subject).to receive(:reports_path).and_return("reports_path")
     allow(subject).to receive(:report_path).and_return("report_path")
+    allow(subject).to receive(:report_variance_path).and_return("report_path")
   end
 
   context "when the report is historic" do
     let(:report) { build(:report, state: :approved) }
 
-    it "adds the historic reports path to the breadcrumbs" do
-      expect(subject).to receive(:add_breadcrumb).with("Historic Reports", reports_path(anchor: "historic"))
-      expect(subject).to receive(:add_breadcrumb).with(t("page_title.report.show", report_fund: report.fund.source_fund.name, report_financial_quarter: report.financial_quarter_and_year), report_path(report))
+    describe "#prepare_default_report_trail" do
+      it "adds the historic reports path to the breadcrumbs" do
+        expect(subject).to receive(:add_breadcrumb).with("Historic Reports", reports_path(anchor: "historic"))
+        expect(subject).to receive(:add_breadcrumb).with(t("page_title.report.show", report_fund: report.fund.source_fund.name, report_financial_quarter: report.financial_quarter_and_year), report_path(report))
 
-      subject.prepare_default_report_trail(report)
+        subject.prepare_default_report_trail(report)
+      end
+
+      it "sets the breadcrumb context in the session" do
+        subject.prepare_default_report_trail(report)
+
+        expect(session[:breadcrumb_context]).to eq({
+          type: :report,
+          model: report,
+        })
+      end
     end
 
-    it "sets the breadcrumb context in the session" do
-      subject.prepare_default_report_trail(report)
+    describe "#prepare_default_report_variance_trail" do
+      it "adds the historic reports path and the variance to the breadcrumbs" do
+        expect(subject).to receive(:add_breadcrumb).with("Historic Reports", reports_path(anchor: "historic"))
+        expect(subject).to receive(:add_breadcrumb).with(t("page_title.report.show", report_fund: report.fund.source_fund.name, report_financial_quarter: report.financial_quarter_and_year), report_variance_path(report))
 
-      expect(session[:breadcrumb_context]).to eq({
-        type: :report,
-        model: report,
-      })
+        subject.prepare_default_report_trail(report)
+      end
     end
   end
 
   context "when the report is current" do
     let(:report) { build(:report, state: :active) }
 
-    it "adds the current reports path to the breadcrumbs" do
-      expect(subject).to receive(:add_breadcrumb).with("Current Reports", reports_path)
-      expect(subject).to receive(:add_breadcrumb).with(t("page_title.report.show", report_fund: report.fund.source_fund.name, report_financial_quarter: report.financial_quarter_and_year), report_path(report))
+    describe "#prepare_default_report_trail" do
+      it "adds the current reports path to the breadcrumbs" do
+        expect(subject).to receive(:add_breadcrumb).with("Current Reports", reports_path)
+        expect(subject).to receive(:add_breadcrumb).with(t("page_title.report.show", report_fund: report.fund.source_fund.name, report_financial_quarter: report.financial_quarter_and_year), report_path(report))
 
-      subject.prepare_default_report_trail(report)
+        subject.prepare_default_report_trail(report)
+      end
+
+      it "sets the breadcrumb context in the session" do
+        subject.prepare_default_report_trail(report)
+
+        expect(session[:breadcrumb_context]).to eq({
+          type: :report,
+          model: report,
+        })
+      end
     end
 
-    it "sets the breadcrumb context in the session" do
-      subject.prepare_default_report_trail(report)
+    describe "#prepare_default_report_variance_trail" do
+      it "adds the historic reports path and the variance to the breadcrumbs" do
+        expect(subject).to receive(:add_breadcrumb).with("Current Reports", reports_path)
+        expect(subject).to receive(:add_breadcrumb).with(t("page_title.report.show", report_fund: report.fund.source_fund.name, report_financial_quarter: report.financial_quarter_and_year), report_variance_path(report))
 
-      expect(session[:breadcrumb_context]).to eq({
-        type: :report,
-        model: report,
-      })
+        subject.prepare_default_report_trail(report)
+      end
     end
   end
 end
