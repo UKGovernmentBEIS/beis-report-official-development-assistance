681ccdc4330504e03f41788222a3a4df1273e478
pezholio <pezholio@gmail.com>
2021-09-02 15:04:14 +0100

    Add some more granular policies for exports
    This will be used to allow delivery partners to
    view a list of exports for their own organisation, as
    well as download all the external income for their
    organisation too.

--- app/policies/export/organisation_policy.rb
+++ app/policies/export/organisation_policy.rb
@@ -6,6 +6,19 @@ module Export
 
     def show?
       return true if user.service_owner?
+      return true if user.delivery_partner? && user.organisation == record
+    end
+
+    def show_external_income?
+      show?
+    end
+
+    def show_transactions?
+      return true if user.service_owner?
+    end
+
+    def show_xml?
+      return true if user.service_owner?
     end
   end
 end
--- spec/policies/export/organisation_policy_spec.rb
+++ spec/policies/export/organisation_policy_spec.rb
@@ -1,13 +1,18 @@
 require "rails_helper"
 
 RSpec.describe Export::OrganisationPolicy do
-  subject { described_class.new(user, :export) }
+  let(:organisation) { create(:delivery_partner_organisation) }
+
+  subject { described_class.new(user, organisation) }
 
   context "for a BEIS user" do
     let(:user) { create(:beis_user) }
 
     it { is_expected.to permit_action(:index) }
     it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_action(:show_external_income) }
+    it { is_expected.to permit_action(:show_transactions) }
+    it { is_expected.to permit_action(:show_xml) }
   end
 
   context "for a delivery partner" do
@@ -15,5 +20,19 @@ RSpec.describe Export::OrganisationPolicy do
 
     it { is_expected.to forbid_action(:index) }
     it { is_expected.to forbid_action(:show) }
+    it { is_expected.to forbid_action(:show_external_income) }
+    it { is_expected.to forbid_action(:show_transactions) }
+    it { is_expected.to forbid_action(:show_xml) }
+
+    context "when the user's organisation matches the organisation" do
+      let(:organisation) { user.organisation }
+
+      it { is_expected.to permit_action(:show) }
+      it { is_expected.to permit_action(:show_external_income) }
+
+      it { is_expected.to forbid_action(:index) }
+      it { is_expected.to forbid_action(:show_transactions) }
+      it { is_expected.to forbid_action(:show_xml) }
+    end
   end
 end
