cbf444e825ebaa3bf8fb56436d418f8d5df6f224
pezholio <pezholio@gmail.com>
2021-09-06 10:55:33 +0100

    Show delivery partner users a link to exports
    This shows delivery partners a link to see a list of
    all their exports. I've added a view spec, rather
    than adding the extra load of another feature spec.

---
+++ spec/views/staff/shared/navigation_spec.rb
@@ -0,0 +1,39 @@
+RSpec.describe "shared/_navigation" do
+  let(:policy_stub) { double("policy") }
+
+  before do
+    without_partial_double_verification do
+      allow(view).to receive(:policy) do |record|
+        Pundit.policy(user, record)
+      end
+      allow(view).to receive(:current_user).and_return(user)
+      allow(user).to receive(:organisation_id).and_return(SecureRandom.uuid)
+    end
+
+    render
+  end
+
+  context "when the current user is a BEIS user" do
+    let(:user) { build(:beis_user) }
+
+    it "shows the link to the exports index" do
+      expect(rendered).to have_link(t("page_title.export.index"), href: exports_path)
+    end
+
+    it "does not show the link to an organisation export page" do
+      expect(rendered).to_not have_link(t("page_title.export.index"), href: exports_organisation_path(id: user.organisation_id))
+    end
+  end
+
+  context "when the current user is a delivery partner" do
+    let(:user) { build(:delivery_partner_user) }
+
+    it "does not show the link to the exports index" do
+      expect(rendered).to_not have_link(t("page_title.export.index"), href: exports_path)
+    end
+
+    it "shows the link to the delivery partner's organisation export page" do
+      expect(rendered).to have_link(t("page_title.export.index"), href: exports_organisation_path(id: user.organisation_id))
+    end
+  end
+end
