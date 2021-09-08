8c26f2486f828754337efa07baba393071492f31
pezholio <pezholio@gmail.com>
2021-09-08 15:27:14 +0100

    Add seperate code lists for regions and levels
    This way, we can reduce duplication in the code
    lists, and also allows us to refer directly to the
    Developing countries, unspecified country in the
    codelist, as opposed to it being in the code. I've
    also added a codelist for the region levels, so we
    can be super-clear about which level is which.
    
    This also allows us to not have to rely on the
    ordering of the regions to know which one is the
    most specific.

--- app/models/benefitting_country.rb
+++ app/models/benefitting_country.rb
@@ -14,7 +14,8 @@ class BenefittingCountry
     def region_from_country_codes(codes)
       codes.map { |c| find_by_code(c) }
         .map { |c| c.regions }
-        .inject(:&).last || Region::DEVELOPING_COUNTRIES_REGION
+        .inject(:&)
+        .max_by { |r| r.level.code } || Region.find_by_code(Region::DEVELOPING_COUNTRIES_CODE)
     end
 
     private
@@ -25,11 +26,7 @@ class BenefittingCountry
         code: country["code"],
         recipient_code: country["recipient_code"],
         regions: country["regions"].map { |region|
-          Region.new(
-            name: region["name"],
-            code: region["code"],
-            level: region["level"],
-          )
+          Region.find_by_code(region)
         }
       )
     end
@@ -39,11 +36,27 @@ class BenefittingCountry
     include ActiveModel::Model
     attr_accessor :name, :code, :level
 
-    DEVELOPING_COUNTRIES_REGION = new(
-      name: "Developing countries, unspecified",
-      code: "998",
-      level: 1,
-    )
+    DEVELOPING_COUNTRIES_CODE = "998"
+
+    class << self
+      def all
+        @all ||= Codelist.new(type: "benefitting_regions", source: "beis").map { |region| new_from_hash(region) }
+      end
+
+      def find_by_code(code)
+        all.find { |region| region.code == code }
+      end
+
+      private
+
+      def new_from_hash(region)
+        new(
+          name: region["name"],
+          code: region["code"],
+          level: Level.find_by_code(region["level"]),
+        )
+      end
+    end
 
     def hash
       code.hash
@@ -51,6 +64,27 @@ class BenefittingCountry
 
     def eql?(other)
       code == other.code
+    end
+
+    class Level
+      include ActiveModel::Model
+      attr_accessor :name, :code
+
+      class << self
+        def all
+          @all ||= Codelist.new(type: "benefitting_region_levels", source: "beis").map { |level| new_from_hash(level) }
+        end
+
+        def find_by_code(code)
+          all.find { |level| level.code == code }
+        end
+
+        private
+
+        def new_from_hash(level)
+          new(name: level["name"], code: level["code"])
+        end
+      end
     end
   end
 end
--- spec/models/benefitting_country_spec.rb
+++ spec/models/benefitting_country_spec.rb
@@ -1,12 +1,78 @@
 require "spec_helper"
 
 RSpec.describe BenefittingCountry do
-  describe ".all" do
-    subject { described_class.all }
+  let(:region) { BenefittingCountry::Region::Level.new(code: 1, name: "Region") }
+  let(:subregion1) { BenefittingCountry::Region::Level.new(code: 2, name: "Sub-region 1") }
+  let(:subregion2) { BenefittingCountry::Region::Level.new(code: 3, name: "Sub-region 2") }
 
-    it "returns all the countries" do
-      expect(subject.count).to eq(177)
-    end
+  let(:africa) { BenefittingCountry::Region.new(name: "Africa, regional", code: "298", level: region) }
+  let(:south_of_sahara) { BenefittingCountry::Region.new(name: "South of Sahara, regional", code: "289", level: subregion1) }
+  let(:middle_africa) { BenefittingCountry::Region.new(name: "Middle Africa, regional", code: "1028", level: subregion2) }
+  let(:eastern_africa) { BenefittingCountry::Region.new(name: "Eastern Africa, regional", code: "1027", level: subregion2) }
+  let(:southern_africa) { BenefittingCountry::Region.new(name: "Southern Africa, regional", code: "102", level: subregion2) }
+  let(:north_of_sahara) { BenefittingCountry::Region.new(name: "North of Sahara, regional", code: "189", level: subregion2) }
+  let(:asia) { BenefittingCountry::Region.new(name: "Asia, regional", code: "798", level: region) }
+  let(:middle_east) { BenefittingCountry::Region.new(name: "Middle East, regional", code: "589", level: subregion1) }
+
+  let(:countries) do
+    [
+      BenefittingCountry.new(
+        code: "AO",
+        name: "Angola",
+        regions: [
+          africa,
+          south_of_sahara,
+          middle_africa,
+        ]
+      ),
+      BenefittingCountry.new(
+        code: "GA",
+        name: "Gabon",
+        regions: [
+          africa,
+          south_of_sahara,
+          middle_africa,
+        ]
+      ),
+      BenefittingCountry.new(
+        code: "ZW",
+        name: "Zimbabwe",
+        regions: [
+          africa,
+          south_of_sahara,
+          eastern_africa,
+        ]
+      ),
+      BenefittingCountry.new(
+        code: "ZA",
+        name: "South Africa",
+        regions: [
+          africa,
+          south_of_sahara,
+          eastern_africa,
+        ]
+      ),
+      BenefittingCountry.new(
+        code: "DZ",
+        name: "Algeria",
+        regions: [
+          africa,
+          north_of_sahara,
+        ]
+      ),
+      BenefittingCountry.new(
+        code: "LB",
+        name: "Lebanon",
+        regions: [
+          asia,
+          middle_east,
+        ]
+      ),
+    ]
+  end
+
+  before do
+    allow(described_class).to receive(:all).and_return(countries)
   end
 
   describe ".region_from_country_codes" do
@@ -45,10 +111,10 @@ RSpec.describe BenefittingCountry do
     end
 
     context "when there are more than two countries" do
-      let(:codes) { ["KG", "TJ", "TM", "AF"] }
+      let(:codes) { ["AO", "GA", "AO", "ZW", "DZ"] }
 
       it "gets the most specific region" do
-        expect(subject.code).to eq("689")
+        expect(subject.code).to eq("298")
       end
     end
   end
