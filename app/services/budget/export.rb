7febf518abb128ce5c06439c2b369a4e1115f19f
pezholio <pezholio@gmail.com>
2021-09-20 11:37:29 +0100

    Add a class to export budgets for a fund
    This adds a service class to export all budgets for
    a given fund. I've follwed the same pattern as the
    other export classes, with three public methods,
    one for the filename, one for the headers, and
    another for the body.
    
    The headers method returns the headers
    defined in  the `HEADERS` constant, and then a
    range of  financial years taken from the maximum
    and minimum financial years in the result set.
    
    The body method goes through all that fund's
    activities, returns the activities data and then
    cycles through all that activity's budgets using the
    range of possible financial years. If there is no
    data for a given year, we return zero.
    
    To make cycling through the financial years
    possible, I've added `#pred` and `#succ` methods
    to the FinancialYear class, to make the classes
    iterable, in a similar way to what we’ve already
    done with the FinancialQuarter class

---
+++ app/services/budget/export.rb
@@ -0,0 +1,71 @@
+class Budget
+  class Export
+    HEADERS = [
+      "RODA identifier",
+      "Delivery partner identifier",
+      "Level",
+      "Title",
+    ]
+
+    def initialize(source_fund:)
+      @source_fund = source_fund
+    end
+
+    def headers
+      return HEADERS if budgets.empty?
+
+      HEADERS + financial_year_range.map(&:to_s)
+    end
+
+    def rows
+      return [] if budgets.empty?
+
+      activities.map do |activity|
+        activity_data(activity) + budget_data(activity.budgets)
+      end
+    end
+
+    def filename
+      [
+        source_fund.short_name,
+        "budgets.csv",
+      ].join("_")
+    end
+
+    private
+
+    attr_reader :source_fund
+
+    def activity_data(activity)
+      [
+        activity.roda_identifier,
+        activity.delivery_partner_identifier,
+        I18n.t("table.body.activity.level.#{activity.level}"),
+        activity.title,
+      ]
+    end
+
+    def budget_data(budgets)
+      financial_year_range.map do |financial_year|
+        value = budgets.find { |budget| budget.financial_year == financial_year }&.value || 0
+        "%.2f" % value
+      end
+    end
+
+    def activities
+      @_activities ||= Activity.includes(:budgets).not_fund.where(source_fund_code: source_fund.id)
+    end
+
+    def budgets
+      @_budgets ||= activities.map(&:budgets).flatten.uniq
+    end
+
+    def financial_years
+      budgets.map(&:financial_year).uniq
+    end
+
+    def financial_year_range
+      @_financial_year_range ||= Range.new(*financial_years.minmax)
+    end
+  end
+end
