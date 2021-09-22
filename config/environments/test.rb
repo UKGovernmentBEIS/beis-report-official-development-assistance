ca36d112799862bd6b0e2143955de3e161bd7324
meyric <meyric@dxw.com>
2021-09-22 09:40:00 +0100

    Upgrade Ruby and JS dependencies
    We also configure the test environment to not annotate the layout and
    partials with the file name, we can only assume this was never working
    as we have a test that now fails because of this.
    
    It feels right that this is disabled in the test environment as it is
    also disabled in production and we want parity between the two.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
-  config.action_view.annotate_rendered_view_with_filenames = true
+  config.action_view.annotate_rendered_view_with_filenames = false
 
   # Set a css_compressor so sassc-rails does not overwrite the compressor
   # See https://github.com/DFE-Digital/dfe-teachers-payment-service/commit/74ec587cfbe9aa6d0df01a72e99d70ffe9024748
