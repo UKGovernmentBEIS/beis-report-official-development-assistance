df186892a0f504d3af17ce088dfdcbe897129a2e
Ed Davey <ed@dxw.com>
2021-10-14 11:45:55 +0100

    Refactor policy spec for speed
    from 30 seconds to 8 seconds on my development machine.

--- spec/policies/refund_policy_spec.rb
+++ spec/policies/refund_policy_spec.rb
@@ -11,56 +11,76 @@ RSpec.describe RefundPolicy do
     context "when the activity is a fund" do
       let(:activity) { create(:fund_activity, organisation: user.organisation) }
 
-      it { is_expected.to permit_action(:show) }
-      it { is_expected.to forbid_action(:create) }
-      it { is_expected.to forbid_action(:edit) }
-      it { is_expected.to forbid_action(:update) }
+      it "controls actions as expected" do
+        aggregate_failures do
+          is_expected.to permit_action(:show)
+          is_expected.to forbid_action(:create)
+          is_expected.to forbid_action(:edit)
+          is_expected.to forbid_action(:update)
 
-      it { is_expected.to forbid_action(:destroy) }
+          is_expected.to forbid_action(:destroy)
+        end
+      end
     end
 
     context "when the activity is a programme" do
       context "and the activity has an associated report" do
         let(:activity) { create(:programme_activity, :with_report, organisation: user.organisation) }
 
-        it { is_expected.to permit_action(:show) }
-        it { is_expected.to permit_action(:create) }
-        it { is_expected.to permit_action(:edit) }
-        it { is_expected.to permit_action(:update) }
-        it { is_expected.to permit_action(:destroy) }
+        it "controls actions as expected" do
+          aggregate_failures do
+            is_expected.to permit_action(:show)
+            is_expected.to permit_action(:create)
+            is_expected.to permit_action(:edit)
+            is_expected.to permit_action(:update)
+            is_expected.to permit_action(:destroy)
+          end
+        end
       end
 
       context "and the activity does not have an associated report" do
         let(:activity) { create(:programme_activity, organisation: user.organisation) }
 
-        it { is_expected.to permit_action(:show) }
-        it { is_expected.to forbid_action(:create) }
-        it { is_expected.to permit_action(:edit) }
-        it { is_expected.to permit_action(:update) }
-        it { is_expected.to permit_action(:destroy) }
+        it "controls actions as expected" do
+          aggregate_failures do
+            is_expected.to permit_action(:show)
+            is_expected.to forbid_action(:create)
+            is_expected.to permit_action(:edit)
+            is_expected.to permit_action(:update)
+            is_expected.to permit_action(:destroy)
+          end
+        end
       end
     end
 
     context "when the activity is a project" do
       let(:activity) { create(:project_activity, organisation: create(:delivery_partner_organisation)) }
 
-      it { is_expected.to permit_action(:show) }
+      it "controls actions as expected" do
+        aggregate_failures do
+          is_expected.to permit_action(:show)
 
-      it { is_expected.to forbid_action(:create) }
-      it { is_expected.to forbid_action(:edit) }
-      it { is_expected.to forbid_action(:update) }
-      it { is_expected.to forbid_action(:destroy) }
+          is_expected.to forbid_action(:create)
+          is_expected.to forbid_action(:edit)
+          is_expected.to forbid_action(:update)
+          is_expected.to forbid_action(:destroy)
+        end
+      end
     end
 
     context "when the activity is a third party project" do
       let(:activity) { create(:third_party_project_activity, organisation: create(:delivery_partner_organisation)) }
 
-      it { is_expected.to permit_action(:show) }
+      it "controls actions as expected" do
+        aggregate_failures do
+          is_expected.to permit_action(:show)
 
-      it { is_expected.to forbid_action(:create) }
-      it { is_expected.to forbid_action(:edit) }
-      it { is_expected.to forbid_action(:update) }
-      it { is_expected.to forbid_action(:destroy) }
+          is_expected.to forbid_action(:create)
+          is_expected.to forbid_action(:edit)
+          is_expected.to forbid_action(:update)
+          is_expected.to forbid_action(:destroy)
+        end
+      end
     end
   end
 
@@ -70,32 +90,44 @@ RSpec.describe RefundPolicy do
     context "when the activity is a fund" do
       let(:activity) { create(:fund_activity) }
 
-      it { is_expected.to forbid_action(:show) }
-      it { is_expected.to forbid_action(:create) }
-      it { is_expected.to forbid_action(:edit) }
-      it { is_expected.to forbid_action(:update) }
-      it { is_expected.to forbid_action(:destroy) }
+      it "controls actions as expected" do
+        aggregate_failures do
+          is_expected.to forbid_action(:show)
+          is_expected.to forbid_action(:create)
+          is_expected.to forbid_action(:edit)
+          is_expected.to forbid_action(:update)
+          is_expected.to forbid_action(:destroy)
+        end
+      end
     end
 
     context "when the activity is a programme" do
       let(:activity) { create(:programme_activity) }
 
-      it { is_expected.to forbid_action(:show) }
-      it { is_expected.to forbid_action(:create) }
-      it { is_expected.to forbid_action(:edit) }
-      it { is_expected.to forbid_action(:update) }
-      it { is_expected.to forbid_action(:destroy) }
+      it "controls actions as expected" do
+        aggregate_failures do
+          is_expected.to forbid_action(:show)
+          is_expected.to forbid_action(:create)
+          is_expected.to forbid_action(:edit)
+          is_expected.to forbid_action(:update)
+          is_expected.to forbid_action(:destroy)
+        end
+      end
     end
 
     context "when the activity is a project" do
       let(:activity) { create(:project_activity) }
 
       context "and the activity does not belong to the users organisation" do
-        it { is_expected.to forbid_action(:show) }
-        it { is_expected.to forbid_action(:create) }
-        it { is_expected.to forbid_action(:edit) }
-        it { is_expected.to forbid_action(:update) }
-        it { is_expected.to forbid_action(:destroy) }
+        it "controls actions as expected" do
+          aggregate_failures do
+            is_expected.to forbid_action(:show)
+            is_expected.to forbid_action(:create)
+            is_expected.to forbid_action(:edit)
+            is_expected.to forbid_action(:update)
+            is_expected.to forbid_action(:destroy)
+          end
+        end
       end
 
       context "and the activity does belong to the users organisation" do
@@ -106,24 +138,32 @@ RSpec.describe RefundPolicy do
         context "when there is no editable report" do
           let(:report) { create(:report, state: :inactive) }
 
-          it { is_expected.to permit_action(:show) }
+          it "controls actions as expected" do
+            aggregate_failures do
+              is_expected.to permit_action(:show)
 
-          it { is_expected.to forbid_action(:create) }
-          it { is_expected.to forbid_action(:edit) }
-          it { is_expected.to forbid_action(:update) }
-          it { is_expected.to forbid_action(:destroy) }
+              is_expected.to forbid_action(:create)
+              is_expected.to forbid_action(:edit)
+              is_expected.to forbid_action(:update)
+              is_expected.to forbid_action(:destroy)
+            end
+          end
         end
 
         context "when there is an editable report" do
           let(:report) { create(:report, state: :active) }
 
           context "and the report is not for the organisation or fund of the activity" do
-            it { is_expected.to permit_action(:show) }
+            it "controls actions as expected" do
+              aggregate_failures do
+                is_expected.to permit_action(:show)
 
-            it { is_expected.to forbid_action(:create) }
-            it { is_expected.to forbid_action(:edit) }
-            it { is_expected.to forbid_action(:update) }
-            it { is_expected.to forbid_action(:destroy) }
+                is_expected.to forbid_action(:create)
+                is_expected.to forbid_action(:edit)
+                is_expected.to forbid_action(:update)
+                is_expected.to forbid_action(:destroy)
+              end
+            end
           end
 
           context "and the report is for the organisation but not the fund of the activity" do
@@ -131,12 +171,16 @@ RSpec.describe RefundPolicy do
               report.update(organisation: activity.organisation)
             end
 
-            it { is_expected.to permit_action(:show) }
+            it "controls actions as expected" do
+              aggregate_failures do
+                is_expected.to permit_action(:show)
 
-            it { is_expected.to forbid_action(:create) }
-            it { is_expected.to forbid_action(:edit) }
-            it { is_expected.to forbid_action(:update) }
-            it { is_expected.to forbid_action(:destroy) }
+                is_expected.to forbid_action(:create)
+                is_expected.to forbid_action(:edit)
+                is_expected.to forbid_action(:update)
+                is_expected.to forbid_action(:destroy)
+              end
+            end
           end
 
           context "and the report is for the organisation and fund of the activity" do
@@ -145,12 +189,16 @@ RSpec.describe RefundPolicy do
             end
 
             context "when the report is not the one in which the actual was created" do
-              it { is_expected.to permit_action(:show) }
-              it { is_expected.to permit_action(:create) }
+              it "controls actions as expected" do
+                aggregate_failures do
+                  is_expected.to permit_action(:show)
+                  is_expected.to permit_action(:create)
 
-              it { is_expected.to forbid_action(:edit) }
-              it { is_expected.to forbid_action(:update) }
-              it { is_expected.to forbid_action(:destroy) }
+                  is_expected.to forbid_action(:edit)
+                  is_expected.to forbid_action(:update)
+                  is_expected.to forbid_action(:destroy)
+                end
+              end
             end
 
             context "when the report is the one in which the actual was created" do
@@ -158,11 +206,15 @@ RSpec.describe RefundPolicy do
                 refund.update(report: report)
               end
 
-              it { is_expected.to permit_action(:show) }
-              it { is_expected.to permit_action(:create) }
-              it { is_expected.to permit_action(:edit) }
-              it { is_expected.to permit_action(:update) }
-              it { is_expected.to permit_action(:destroy) }
+              it "controls actions as expected" do
+                aggregate_failures do
+                  is_expected.to permit_action(:show)
+                  is_expected.to permit_action(:create)
+                  is_expected.to permit_action(:edit)
+                  is_expected.to permit_action(:update)
+                  is_expected.to permit_action(:destroy)
+                end
+              end
             end
           end
         end
