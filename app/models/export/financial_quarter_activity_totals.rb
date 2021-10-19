89fc9035fc88fe30d02d2e9616da3710965c4c87
meyric <meyric@dxw.com>
2021-10-19 11:50:27 +0100

    Introduce Export::FinancialQuarterActivityTotals
    This class is extracted from SpendingBreakdown and will be used to
    produce totals for a given type, activity and financial quarter.
    
    I've split it out to keep the new class clean and to better document
    it's purpose.

---
+++ app/models/export/financial_quarter_activity_totals.rb
@@ -0,0 +1,37 @@
+class Export::FinancialQuarterActivityTotals
+  TYPES = {
+    actual: "Actual",
+    refund: "Refund",
+  }
+
+  def initialize(type:, activity:, totals:, financial_quarter:)
+    @transaction_type = TYPES.fetch(type)
+    @activity = activity
+    @totals = totals
+    @financial_quarter = financial_quarter
+  end
+
+  def net_total
+    total + adjustments_total
+  end
+
+  def total
+    @totals.fetch([
+      @activity.id,
+      @financial_quarter.quarter,
+      @financial_quarter.financial_year.start_year,
+      @transaction_type,
+      nil,
+    ], 0)
+  end
+
+  def adjustments_total
+    @totals.fetch([
+      @activity.id,
+      @financial_quarter.quarter,
+      @financial_quarter.financial_year.start_year,
+      "Adjustment",
+      @transaction_type,
+    ], 0)
+  end
+end
