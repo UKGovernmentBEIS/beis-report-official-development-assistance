e20dc44a07e83eeb6abe30e53128a11dcf351e08
meyric <meyric@dxw.com>
2021-10-21 15:36:10 +0100

    Introduce ActivityImplementingOrganisationColumn
    Allows Implementing organisations to be exported.
    
    Returns the name only as the only use case we have for this
    right now is the report csv which only requires the name.
    
    Multiple implementing organisations are delimited with a pipe (|).
    
    When the activity is at Level B (a programme) there is no guarantee
    there will be any implementing organisations, but the
    extending organisation will have the correct organisation details, so we
    use that.
    
    We pass in an ActiveRecord::Relation of activities so we can leverage
    Rails lazy loading of the organisations, failing to pass in the correct
    type would cause an error, failing to pass in the correct type would
    cause an error, so we check this.

---
+++ app/models/export/activity_implementing_organisation_column.rb
@@ -0,0 +1,37 @@
+class Export::ActivityImplementingOrganisationColumn
+  def initialize(activities_relation:)
+    @activities = valid_activities(activities_relation)
+  end
+
+  def headers
+    ["Implementing organisations"]
+  end
+
+  def rows
+    return [] if @activities.empty?
+
+    @activities.includes(:extending_organisation, :implementing_organisations).map { |activity|
+      implementing_organisation_names = if activity.programme?
+        extending_organisation_name_for_activity(activity)
+      else
+        implementing_organisation_names_for_actvity(activity)
+      end
+      [activity.id, [implementing_organisation_names.join("|")]]
+    }.to_h
+  end
+
+  private
+
+  def extending_organisation_name_for_activity(activity)
+    [activity.extending_organisation.name]
+  end
+
+  def implementing_organisation_names_for_actvity(activity)
+    activity.implementing_organisations.map { |organisation| organisation.name }
+  end
+
+  def valid_activities(activities)
+    raise ArgumentError.new("activities must be an ActiveRecord:Relation") unless activities.is_a?(ActiveRecord::Relation)
+    activities
+  end
+end
