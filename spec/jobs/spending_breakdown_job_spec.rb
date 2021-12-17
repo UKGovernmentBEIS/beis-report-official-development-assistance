1275da09988a29145a7914ec054500ec66807aa1
Ed Davey <ed@dxw.com>
2021-12-17 06:35:51 +0000

    Ask SpendingBreakdown to build the breakdown for the given fund

--- spec/jobs/spending_breakdown_job_spec.rb
+++ spec/jobs/spending_breakdown_job_spec.rb
@@ -3,11 +3,13 @@ require "rails_helper"
 RSpec.describe SpendingBreakdownJob, type: :job do
   let(:requester) { double(:user) }
   let(:fund) { double(:fund) }
+  let(:breakdown) { instance_double(Export::SpendingBreakdown, filename: double("filename")) }
 
   describe "#perform" do
     before do
       allow(User).to receive(:find)
-      allow(Fund).to receive(:new)
+      allow(Fund).to receive(:new).and_return(fund)
+      allow(Export::SpendingBreakdown).to receive(:new).and_return(breakdown)
     end
 
     it "asks the user object for the user with a given id" do
@@ -20,6 +22,12 @@ RSpec.describe SpendingBreakdownJob, type: :job do
       SpendingBreakdownJob.perform_now(requester_id: double, fund_id: "fund123")
 
       expect(Fund).to have_received(:new).with("fund123")
+    end
+
+    it "uses Export::SpendingBreakdown to build the breakdown for the given fund" do
+      SpendingBreakdownJob.perform_now(requester_id: double, fund_id: double)
+
+      expect(Export::SpendingBreakdown).to have_received(:new).with(source_fund: fund)
     end
   end
 end
