4a6066187e45e1c2cefdeeeebd4cebbd447f0d38
Ed Davey <ed@dxw.com>
2021-12-17 06:35:51 +0000

    Ask SpendingBreakdown to build the breakdown for the given fund

--- app/jobs/spending_breakdown_job.rb
+++ app/jobs/spending_breakdown_job.rb
@@ -2,5 +2,7 @@ class SpendingBreakdownJob < ApplicationJob
   def perform(requester_id:, fund_id:)
     requester = User.find(requester_id)
     fund = Fund.new(fund_id)
+
+    export = Export::SpendingBreakdown.new(source_fund: fund)
   end
 end
