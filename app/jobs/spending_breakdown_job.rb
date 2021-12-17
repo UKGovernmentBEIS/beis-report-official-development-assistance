dfe9fca56395e9ffe6fcecc34f9d65fb97b8bc15
Ed Davey <ed@dxw.com>
2021-12-17 06:57:02 +0000

    Write export to a tempfile

--- app/jobs/spending_breakdown_job.rb
+++ app/jobs/spending_breakdown_job.rb
@@ -1,8 +1,22 @@
 class SpendingBreakdownJob < ApplicationJob
+  require "csv"
+
   def perform(requester_id:, fund_id:)
     requester = User.find(requester_id)
     fund = Fund.new(fund_id)
 
     export = Export::SpendingBreakdown.new(source_fund: fund)
+    tempfile = save_tempfile(export)
+  end
+
+  def save_tempfile(export)
+    tmpfile = Tempfile.new
+    CSV.open(tmpfile, "wb", {headers: true}) do |csv|
+      csv << export.headers
+      export.rows.each do |row|
+        csv << row
+      end
+    end
+    tmpfile
   end
 end
--- spec/jobs/spending_breakdown_job_spec.rb
+++ spec/jobs/spending_breakdown_job_spec.rb
@@ -3,13 +3,27 @@ require "rails_helper"
 RSpec.describe SpendingBreakdownJob, type: :job do
   let(:requester) { double(:user) }
   let(:fund) { double(:fund) }
-  let(:breakdown) { instance_double(Export::SpendingBreakdown, filename: double("filename")) }
+  let(:row1) { double("row1") }
+  let(:row2) { double("row1") }
+
+  let(:breakdown) do
+    instance_double(
+      Export::SpendingBreakdown,
+      filename: double("filename"),
+      headers: %w[col1 col2],
+      rows: [row1, row2]
+    )
+  end
+  let(:tempfile) { double("tempfile") }
+  let(:csv) { double("csv", "<<" => true) }
 
   describe "#perform" do
     before do
       allow(User).to receive(:find)
       allow(Fund).to receive(:new).and_return(fund)
       allow(Export::SpendingBreakdown).to receive(:new).and_return(breakdown)
+      allow(Tempfile).to receive(:new).and_return(tempfile)
+      allow(CSV).to receive(:open).and_yield(csv)
     end
 
     it "asks the user object for the user with a given id" do
@@ -28,6 +42,15 @@ RSpec.describe SpendingBreakdownJob, type: :job do
       SpendingBreakdownJob.perform_now(requester_id: double, fund_id: double)
 
       expect(Export::SpendingBreakdown).to have_received(:new).with(source_fund: fund)
+    end
+
+    it "writes the breakdown to a 'tempfile'" do
+      SpendingBreakdownJob.perform_now(requester_id: double, fund_id: double)
+
+      expect(CSV).to have_received(:open).with(tempfile, "wb", {headers: true})
+      expect(csv).to have_received(:<<).with(%w[col1 col2])
+      expect(csv).to have_received(:<<).with(row1)
+      expect(csv).to have_received(:<<).with(row2)
     end
   end
 end
