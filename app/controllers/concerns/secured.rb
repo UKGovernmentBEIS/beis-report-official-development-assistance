ba6c5ada9b4efa1825dc94ea255bccf846a6418f
Cristina <cristina@dxw.com>
2022-02-01 15:19:59 +0000

    RED: but Successful sign in via header link
    Auth#current_user deleted so devise can take over
    Secured#logged_in_using_omniauth renamed to 'redirect_unauthenticated' and logging deleted
    Password added to User factory

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -2,23 +2,11 @@ module Secured
   extend ActiveSupport::Concern
 
   included do
-    before_action :logged_in_using_omniauth?
+    before_action :redirect_unauthenticated
   end
 
-  def logged_in_using_omniauth?
-    # DEBUG: Session info can be missing
-    if Rails.env.production? && session[:userinfo].blank?
-      # Is there a session object?
-      Rails.logger.info(session)
-
-      # Are there any keys?
-      Rails.logger.info(session.keys)
-
-      # Is Redis available as a session_store?
-      Rails.logger.info(Redis.new(url: ENV["REDIS_URL"]).ping)
-    end
-
-    if session[:userinfo].blank?
+  def redirect_unauthenticated
+    if !authenticated?
       session[:redirect_path] = request.env["PATH_INFO"]
       redirect_to "/"
     end
