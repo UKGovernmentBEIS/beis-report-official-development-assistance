6a7915ee920e3c406c43bf3ecf6bc5fb88711f24
Russell Garner <rgarner@zephyros-systems.co.uk>
2022-02-03 17:18:26 +0000

    RED: Make specs run
    Delete only the bits of Auth0 required to make the specs run without
    failing with load errors due to removal of the gem. That means
    commenting odd bits out for context; we'll need to replace with Devise
    analogues but that will come later.
    
    Co-authored-by: Cristina Rotaru <cristina@dxw.com>

--- app/services/create_user.rb
+++ app/services/create_user.rb
@@ -12,14 +12,14 @@ class CreateUser
     User.transaction do
       user.organisation = organisation
       user.save
-      begin
-        CreateUserInAuth0.new(user: user).call
-      rescue Auth0::Exception => e
-        result.success = false
-        result.error_message = extract_auth0_error_message(e)
-        Rails.logger.error("Error adding user #{user.email} to Auth0 during CreateUser with: #{result.error_message}")
-        raise ActiveRecord::Rollback
-      end
+      # begin
+      #   CreateUserInAuth0.new(user: user).call
+      # rescue Auth0::Exception => e
+      #   result.success = false
+      #   result.error_message = extract_auth0_error_message(e)
+      #   Rails.logger.error("Error adding user #{user.email} to Auth0 during CreateUser with: #{result.error_message}")
+      #   raise ActiveRecord::Rollback
+      # end
     end
 
     SendWelcomeEmail.new(user: user).call if user.persisted?
