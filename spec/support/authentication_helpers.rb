217b4b4bf3408df3145c5c6b55aa1aa3bd9fe825
Russell Garner <rgarner@zephyros-systems.co.uk>
2022-02-03 17:45:25 +0000

    RED: Replace stub_authenticated_session
    This was only in force for Auth0; replace with
    Warden::Test::Helpers#login_as.
    
    This sits neatly in AuthenticationHelpers#authenticate! and buys us a
    few hundred more passing tests.
    
    Co-authored-by: Cristina Rotaru <cristina@dxw.com>

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,4 +1,6 @@
 module AuthenticationHelpers
+  include Warden::Test::Helpers
+
   def mock_successful_authentication(uid: "12345", name: "Alex", email: "alex@example.com")
     # OmniAuth.config.mock_auth[:auth0] = OmniAuth::AuthHash.new(
     #   provider: "auth0",
@@ -15,16 +17,6 @@ module AuthenticationHelpers
       .with(identifier: user.identifier)
       .and_return(user)
 
-    stub_authenticated_session(
-      uid: user.identifier,
-      name: user.name,
-      email: user.email
-    )
-  end
-
-  def stub_authenticated_session(uid: "123456789", name: "Alex", email: "alex@example.com")
-    mock_successful_authentication(uid: uid, name: name, email: email)
-    visit root_path
-    click_on t("header.link.sign_in")
+    login_as(user, scope: :user)
   end
 end
