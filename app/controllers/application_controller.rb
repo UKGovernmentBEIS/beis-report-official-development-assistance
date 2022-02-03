1390ce32298a221d27c7e144e275c2427778f189
Russell Garner <rgarner@zephyros-systems.co.uk>
2022-02-03 17:00:35 +0000

    RED: Remove auth0 routes/controller/view
    They aren't accessible or useful right now, even as context. We
    acknowledge that there are many associated environmental variables and
    terraform provisionings and so on. We'll open a separate card for these,
    otherwise the PR will become too large to reason about.
    
    Also remove an auth0-dependent view.
    While we wanted to remove it later in a sweep-up operation, we have a
    missing-keys spec that fails while it is here.
    
    Co-authored-by: Cristina Rotaru <cristina@dxw.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,32 +2,7 @@ class ApplicationController < ActionController::Base
   include Auth
   include Ip
 
-  def sign_out
-    reset_session
-    redirect_to logout_url.to_s
-  end
-
   private
-
-  def logout_url
-    domain = ENV["AUTH0_DOMAIN"]
-    client_id = ENV["AUTH0_CLIENT_ID"]
-    request_params = {
-      returnTo: root_url,
-      client_id: client_id
-    }
-
-    URI::HTTPS.build(host: domain, path: "/v2/logout", query: to_query(request_params))
-  end
-
-  def to_query(hash)
-    hash.map { |k, v| "#{k}=#{CGI.escape(v)}" unless v.nil? }.reject(&:nil?).join("&")
-  end
-
-  def append_info_to_payload(payload)
-    super
-    payload[:remote_ip] = request_ip
-  end
 
   def add_breadcrumb(name, path, options = {})
     super name, path, options.merge(title: name)
