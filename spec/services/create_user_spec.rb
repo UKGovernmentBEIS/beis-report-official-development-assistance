e01d64c087b0761b55dc09d92ca56ee86e7a4260
Russell Garner <rgarner@zephyros-systems.co.uk>
2022-02-14 12:56:06 +0000

    BEIS can invite users via Devise
    We make CreateUser responsible for validating the @user as
    passed, which means it needs to be able to accept a nil organisation,
    so make UsersController#organisation able to return nil so as not to
    fall over when one is not present.
    
    We set a UUID as the user's password such that the model still
    validates, accepting that it'll never be read or used.
    
    We do use a Devise protected method, set_reset_password_token, in
    UserMailer#welcome; this sets the password reset token on the User
    model. We think we're ok with this; the Devise way is to
    send_password_reset_instructions and we need to alter that slightly.
    
    Co-authored-by: CristinaRO <cristina@dxw.com>

--- spec/services/create_user_spec.rb
+++ spec/services/create_user_spec.rb
@@ -1,80 +1,36 @@
 require "rails_helper"
 
-RSpec.describe CreateUser, pending: "To be finished as part of the user invite card" do
-  let(:user) { build(:administrator) }
-  before(:each) do
-    stub_auth0_token_request
-  end
+RSpec.describe CreateUser do
+  let(:user) { build(:administrator, :new_user) }
 
   describe "#call" do
     it "returns a successful result" do
-      stub_auth0_create_user_request(email: user.email)
-
-      result = described_class.new(user: user, organisation: build_stubbed(:delivery_partner_organisation)).call
+      result = CreateUser.new(user: user, organisation: build_stubbed(:delivery_partner_organisation)).call
 
       expect(result.success?).to eq(true)
-      expect(result.failure?).to eq(false)
     end
 
     it "sends a welcome email to the user" do
-      stub_auth0_create_user_request(email: user.email)
-
       expect {
-        described_class.new(user: user, organisation: build_stubbed(:delivery_partner_organisation)).call
+        CreateUser.new(user: user, organisation: build_stubbed(:delivery_partner_organisation)).call
       }.to have_enqueued_mail(UserMailer, :welcome).with(user)
+    end
+
+    it "creates a default password, known to no-one" do
+      CreateUser.new(user: user, organisation: build_stubbed(:delivery_partner_organisation)).call
+      expect(User.last.encrypted_password).not_to be_blank
     end
 
     context "when an organisation is provided" do
       it "associates a user to an organisation" do
-        stub_auth0_create_user_request(email: user.email)
-
         organisation = create(:delivery_partner_organisation)
 
-        described_class.new(
+        CreateUser.new(
           user: user,
           organisation: organisation
         ).call
 
         expect(user.reload.organisation).to eql(organisation)
-      end
-    end
-
-    context "when Auth0 errors" do
-      before(:each) do
-        stub_auth0_create_user_request_failure(email: user.email)
-      end
-
-      it "returns a failed result" do
-        result = described_class.new(user: user, organisation: build_stubbed(:delivery_partner_organisation)).call
-        expect(result.failure?).to eq(true)
-      end
-
-      it "does not save the user" do
-        described_class.new(user: user, organisation: build_stubbed(:delivery_partner_organisation)).call
-        expect(User.find_by(email: user.email)).to be_nil
-      end
-
-      it "logs a failure message" do
-        expect(Rails.logger).to receive(:error)
-          .with("Error adding user #{user.email} to Auth0 during CreateUser with: The user already exists.")
-        described_class.new(user: user, organisation: build_stubbed(:delivery_partner_organisation)).call
-      end
-
-      it "does not email the user" do
-        expect(SendWelcomeEmail).not_to receive(:new)
-        described_class.new(user: user, organisation: build_stubbed(:delivery_partner_organisation)).call
-      end
-
-      context "when Auth0 returns an unparseable error message" do
-        before do
-          stub_auth0_create_user_request_failure(email: user.email, body: "something unparseable")
-        end
-
-        it "logs a generic failure message" do
-          expect(Rails.logger).to receive(:error)
-            .with("Error adding user #{user.email} to Auth0 during CreateUser with: Unknown error")
-          described_class.new(user: user, organisation: build_stubbed(:delivery_partner_organisation)).call
-        end
       end
     end
   end
