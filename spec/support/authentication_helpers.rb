237258b433dfc7d0c14540dddcad5eeadbef8408
Cristina <cristina@dxw.com>
2022-02-15 12:24:35 +0000

    RED: Remove auth0 and omniauth
    Make the app not crash on start-up, after removing all the auth0 and omniauth gems, helpers, and initialisers.
    
    We know the test suite is failing, and we agreed to work this way for this long-running integration branch.
    
    Co-authored-by: dragon-dxw <david.mckee@dxw.com>
    Co-authored-by: rgarner <rgarner@zephyros-systems.co.uk>

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,13 +1,13 @@
 module AuthenticationHelpers
   def mock_successful_authentication(uid: "12345", name: "Alex", email: "alex@example.com")
-    OmniAuth.config.mock_auth[:auth0] = OmniAuth::AuthHash.new(
-      provider: "auth0",
-      uid: uid,
-      info: {
-        name: name,
-        email: email
-      }
-    )
+    # OmniAuth.config.mock_auth[:auth0] = OmniAuth::AuthHash.new(
+    #   provider: "auth0",
+    #   uid: uid,
+    #   info: {
+    #     name: name,
+    #     email: email
+    #   }
+    # )
   end
 
   def authenticate!(user: build_stubbed(:administrator))
