b10e3d378751baae0257dade458b75a34d21f8c1
Russell Garner <rgarner@zephyros-systems.co.uk>
2022-02-15 07:47:42 +0000

    Drop a spec and outer context
    We had a commented-out spec, but it wasn't testing anything that isn't
    better tested in user_can_sign_in_spec, so drop it. Also drop the outer
    context for what follows as it's not useful when we drop the first spec.

--- spec/features/staff/beis_users_can_invite_new_users_spec.rb
+++ spec/features/staff/beis_users_can_invite_new_users_spec.rb
@@ -1,70 +1,57 @@
 RSpec.feature "BEIS users can invite new users to the service" do
-  # context "when the user is not logged in" do
-  #   it "redirects the user to the root path" do
-  #     # Because it fails intermittently we need to comment out the whole block instead of using `pending`;
-  #     # it only flips the failure state!
-  #     pending "fails intermittently; look at as part of the user invite card."
-  #
-  #     visit new_user_path
-  #     expect(current_path).to eq(root_path)
-  #   end
-  # end
+  let(:user) { create(:administrator) }
 
-  context "when the user is authenticated" do
-    let(:user) { create(:administrator) }
+  before do
+    authenticate!(user: user)
+  end
 
-    before do
-      authenticate!(user: user)
+  context "when the user belongs to BEIS" do
+    let(:user) { create(:beis_user) }
+
+    scenario "a new user can be created" do
+      organisation = create(:delivery_partner_organisation)
+      second_organisation = create(:delivery_partner_organisation)
+      new_user_name = "Foo Bar"
+      new_user_email = "email@example.com"
+
+      perform_enqueued_jobs do
+        create_user(organisation, new_user_name, new_user_email)
+      end
+
+      expect(page).to have_content(organisation.name)
+      expect(page).not_to have_content(second_organisation.name)
+
+      new_user = User.where(email: new_user_email).first
+      reset_password_link_regex = %r{http://test.local/users/password/edit\?reset_password_token=.*}
+      expect(new_user).to have_received_email.with_personalisations(
+        link: match(reset_password_link_regex),
+        name: new_user_name,
+        service_url: "test.local"
+      )
     end
 
-    context "when the user belongs to BEIS" do
-      let(:user) { create(:beis_user) }
+    context "when the name and email are not provided" do
+      it "shows the user validation errors instead" do
+        visit new_user_path
 
-      scenario "a new user can be created" do
-        organisation = create(:delivery_partner_organisation)
-        second_organisation = create(:delivery_partner_organisation)
-        new_user_name = "Foo Bar"
-        new_user_email = "email@example.com"
+        expect(page).to have_content(t("page_title.users.new"))
+        fill_in "user[name]", with: "" # deliberately omit a value
+        fill_in "user[email]", with: "" # deliberately omit a value
 
-        perform_enqueued_jobs do
-          create_user(organisation, new_user_name, new_user_email)
-        end
+        click_button t("default.button.submit")
 
-        expect(page).to have_content(organisation.name)
-        expect(page).not_to have_content(second_organisation.name)
-
-        new_user = User.where(email: new_user_email).first
-        reset_password_link_regex = %r{http://test.local/users/password/edit\?reset_password_token=.*}
-        expect(new_user).to have_received_email.with_personalisations(
-          link: match(reset_password_link_regex),
-          name: new_user_name,
-          service_url: "test.local"
-        )
-      end
-
-      context "when the name and email are not provided" do
-        it "shows the user validation errors instead" do
-          visit new_user_path
-
-          expect(page).to have_content(t("page_title.users.new"))
-          fill_in "user[name]", with: "" # deliberately omit a value
-          fill_in "user[email]", with: "" # deliberately omit a value
-
-          click_button t("default.button.submit")
-
-          expect(page).to have_content(t("activerecord.errors.models.user.attributes.name.blank"))
-          expect(page).to have_content(t("activerecord.errors.models.user.attributes.email.blank"))
-        end
+        expect(page).to have_content(t("activerecord.errors.models.user.attributes.name.blank"))
+        expect(page).to have_content(t("activerecord.errors.models.user.attributes.email.blank"))
       end
     end
+  end
 
-    context "when the user does not belong to BEIS" do
-      let(:user) { create(:delivery_partner_user) }
+  context "when the user does not belong to BEIS" do
+    let(:user) { create(:delivery_partner_user) }
 
-      it "does not show them the manage user button" do
-        visit organisation_path(user.organisation)
-        expect(page).not_to have_content(t("page_title.users.index"))
-      end
+    it "does not show them the manage user button" do
+      visit organisation_path(user.organisation)
+      expect(page).not_to have_content(t("page_title.users.index"))
     end
   end
 
