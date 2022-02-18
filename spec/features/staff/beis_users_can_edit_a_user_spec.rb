8cb37d2a7c2e9ee93137626a0657b3d0da762ccf
Cristina <cristina@dxw.com>
2022-02-18 12:22:32 +0000

    Admin can deactivate a user
    A deactivated user does not get signed in, so we have removed the old
    text and only slightly modified the default devise message.
    
    We are not deleting unused translations at this stage. That has been
    planned as a larger “mop-up” operation for later.
    
    We also handle the user editing specs, because this seemed like the best
    place to reinstate them.
    
    Co-authored-by: rgarner <rgarner@zephyros-systems.co.uk>

--- spec/features/staff/beis_users_can_edit_a_user_spec.rb
+++ spec/features/staff/beis_users_can_edit_a_user_spec.rb
@@ -1,11 +1,7 @@
 require "rails_helper"
 
-RSpec.feature "BEIS users can edit other users", pending: "To be finished as part of the user edit card" do
+RSpec.feature "BEIS users can edit other users" do
   let!(:user) { create(:delivery_partner_user, organisation: create(:delivery_partner_organisation)) }
-
-  before do
-    stub_auth0_token_request
-  end
 
   scenario "the email address is disabled" do
     user = create(:beis_user)
@@ -22,26 +18,15 @@ RSpec.feature "BEIS users can edit other users", pending: "To be finished as par
     expect(email_field).to be_disabled
   end
 
-  scenario "the details of the user can be updated" do
+  scenario "the user's name can be updated" do
     user = create(:beis_user)
     authenticate!(user: user)
 
     target_user = create(:delivery_partner_user, name: "Old Name", email: "old@example.com")
 
-    updated_name = "New Name"
-
-    stub_auth0_update_user_request(
-      auth0_identifier: target_user.identifier,
-      name: updated_name,
-      email: target_user.email
-    )
-
-    # Navigate from the landing page
-    visit organisation_path(user.organisation)
-
-    click_on(t("page_title.users.index"))
-
     # Navigate to the users page
+    visit users_path
+
     expect(page).to have_content(t("page_title.users.index"))
 
     # Find the target user and click on edit button
@@ -49,24 +34,25 @@ RSpec.feature "BEIS users can edit other users", pending: "To be finished as par
     find("tr", text: target_user.name).click_link("Edit")
 
     # Fill out the form
-    fill_in "user[name]", with: updated_name
+    fill_in "user[name]", with: "New Name"
 
     # Submit the form
     click_button t("form.button.user.submit")
 
     # Verify the user was updated
-    expect(page).to have_content(updated_name)
+    expect(page).to have_content("New Name")
   end
 
   scenario "an active user can be deactivated" do
     administrator_user = create(:beis_user)
     authenticate!(user: administrator_user)
 
-    visit organisation_path(administrator_user.organisation)
-    click_on t("page_title.users.index")
+    # Navigate to the users page
+    visit users_path
+
     find("tr", text: user.name).click_link("Edit")
 
-    choose t("form.user.active.inactive")
+    choose "Deactivate"
     click_on t("default.button.submit")
 
     expect(user.reload.active).to be false
@@ -77,11 +63,12 @@ RSpec.feature "BEIS users can edit other users", pending: "To be finished as par
     user = create(:inactive_user, organisation: create(:delivery_partner_organisation))
     authenticate!(user: administrator_user)
 
-    visit organisation_path(administrator_user.organisation)
-    click_on t("page_title.users.index")
+    # Navigate to the users page
+    visit users_path
+
     find("tr", text: user.name).click_link("Edit")
 
-    choose t("form.user.active.active")
+    choose "Activate"
     click_on t("default.button.submit")
 
     expect(user.reload.active).to be true
