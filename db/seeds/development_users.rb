5f4c9f8079fdc27e1f2b4ede5f5cb6375e467ab6
James Darling <james@abscond.org>
2022-02-18 11:25:12 +0000

    Fix seeds for devise users
    Since we switched to using local passwords for users, the seeds were broken.
    
    This adds passwords to the seed users

--- db/seeds/development_users.rb
+++ db/seeds/development_users.rb
@@ -1,17 +1,21 @@
-administrator = User.find_or_create_by(
+administrator = User.find_or_initialize_by(
   name: "Administrator",
   email: "roda@dxw.com",
   identifier: "auth0|5dc53e4b85758e0e95b062f0"
 )
-delivery_partner = User.find_or_create_by(
+delivery_partner = User.find_or_initialize_by(
   name: "Delivery Partner",
   email: "roda+dp@dxw.com",
   identifier: "auth0|5e5e1ee731555a0cb0ab5a75"
 )
+
+administrator.password = "letmein"
+delivery_partner.password = "letmein"
+
 beis = Organisation.service_owner
 administrator.organisation = beis
-administrator.save
+administrator.save!
 
 other_organisation = Organisation.find_by(role: :delivery_partner)
 delivery_partner.organisation = other_organisation
-delivery_partner.save
+delivery_partner.save!
