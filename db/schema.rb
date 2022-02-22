3188e2b76c60f6f48ccbe9955df47999763162d1
Cristina <cristina@dxw.com>
2022-02-22 15:39:05 +0000

    Run rails g devise_two_factor User SECRET_KEY_BASE
    Deciding to use the Rails secret key for this spike, we ran the two
    factor gem's generator, followed by rails db:migrate. We had to resolve
    the changes it made to User, but no biggie.
    
    Co-authored-by: Cristina Rotaru <cristina@dxw.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_145958) do
+ActiveRecord::Schema.define(version: 2022_02_18_161451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -325,6 +325,12 @@ ActiveRecord::Schema.define(version: 2022_02_01_145958) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "encrypted_otp_secret"
+    t.string "encrypted_otp_secret_iv"
+    t.string "encrypted_otp_secret_salt"
+    t.integer "consumed_timestep"
+    t.boolean "otp_required_for_login"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["identifier"], name: "index_users_on_identifier"
     t.index ["organisation_id"], name: "index_users_on_organisation_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
