47bb2c4459439173226f66c371802a60548f0227
Cristina <cristina@dxw.com>
2022-02-22 15:06:49 +0000

    Set a generous 5 minutes OTP expiry
    This allows for variation in SMS delivery times.
    
    The only configurable parameter via the devise-two-factor gem is
    `otp_allowed_drift`. We don’t have access to the ROTP gem’s `interval`,
    but we can set the `drift`. Technically this means the code is also
    valid 5 minutes before we send it!
    
    Co-authored-by: rgarner <rgarner@zephyros-systems.co.uk>

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,6 +8,9 @@
 #
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
+
+Devise.otp_allowed_drift = 5.minutes.in_seconds # value in seconds
+
 Devise.setup do |config|
   config.warden do |manager|
     manager.default_strategies(scope: :user).unshift :two_factor_authenticatable
