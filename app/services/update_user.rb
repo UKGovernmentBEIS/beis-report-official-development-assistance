d06fb240236d956b335b3454e42ef4b12ba6a6e6
Cristina <cristina@dxw.com>
2022-03-03 12:36:23 +0000

    BEIS users can reset a user’s mobile number

--- app/services/update_user.rb
+++ app/services/update_user.rb
@@ -1,9 +1,10 @@
 class UpdateUser
-  attr_accessor :user, :organisation
+  attr_accessor :user, :organisation, :reset_mfa
 
-  def initialize(user:, organisation:)
+  def initialize(user:, organisation:, reset_mfa: false)
     self.user = user
     self.organisation = organisation
+    self.reset_mfa = reset_mfa
   end
 
   def call
@@ -11,6 +12,11 @@ class UpdateUser
 
     User.transaction do
       user.organisation = organisation
+
+      if reset_mfa
+        user.mobile_number = nil
+        user.mobile_number_confirmed_at = nil
+      end
 
       user.save
     end
