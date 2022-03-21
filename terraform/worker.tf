e4b7dcc9f3a427f3f3db07349bbdeb760496eac4
Russell Garner <rgarner@zephyros-systems.co.uk>
2022-03-21 07:41:26 +0000

    Remove Auth0 env vars
    From:
    
    - Terraform
    - GitHub workflows
    - local dev .envs
    - Docker
    - usage in Rails helper

--- terraform/worker.tf
+++ terraform/worker.tf
@@ -22,9 +22,6 @@ resource "cloudfoundry_app" "beis-roda-worker" {
     "RAILS_ENV"                        = "production"
     "SECRET_KEY_BASE"                  = var.secret_key_base
     "DOMAIN"                           = "https://${var.custom_hostname}.${var.custom_domain}"
-    "AUTH0_CLIENT_ID"                  = var.auth0_client_id
-    "AUTH0_CLIENT_SECRET"              = var.auth0_client_secret
-    "AUTH0_DOMAIN"                     = var.auth0_domain
     "NOTIFY_KEY"                       = var.notify_key
     "NOTIFY_WELCOME_EMAIL_TEMPLATE"    = var.notify_welcome_email_template
     "NOTIFY_VIEW_TEMPLATE"             = var.notify_view_template
