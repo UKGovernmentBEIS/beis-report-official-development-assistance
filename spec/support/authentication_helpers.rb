b5fddfe1416cedfca98c77d9b3fab6fe6b751b58
Russell Garner <rgarner@zephyros-systems.co.uk>
2022-03-21 07:35:25 +0000

    Remove redundant auth0 spec helper

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,17 +1,6 @@
 module AuthenticationHelpers
   include Warden::Test::Helpers
 
-  def mock_successful_authentication(uid: "12345", name: "Alex", email: "alex@example.com")
-    # OmniAuth.config.mock_auth[:auth0] = OmniAuth::AuthHash.new(
-    #   provider: "auth0",
-    #   uid: uid,
-    #   info: {
-    #     name: name,
-    #     email: email
-    #   }
-    # )
-  end
-
   def authenticate!(user: build_stubbed(:administrator))
     allow(User).to receive(:find_by)
       .with(identifier: user.identifier)
