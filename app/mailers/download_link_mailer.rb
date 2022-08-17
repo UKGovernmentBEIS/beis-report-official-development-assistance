15c356d58feda00cf287fa1e7bc8fdda2fc47f0d
Cristina <579522+CristinaRO@users.noreply.github.com>
2022-08-17 15:34:28 +0100

    Download link mailer includes env name in subject

--- app/mailers/download_link_mailer.rb
+++ app/mailers/download_link_mailer.rb
@@ -9,7 +9,8 @@ class DownloadLinkMailer < ApplicationMailer
       subject: t(
         "mailer.download_link.success.subject",
         application_name: t("app.title"),
-        file_name: file_name
+        file_name: file_name,
+        environment_name: environment_mailer_prefix
       )
     )
   end
@@ -20,7 +21,8 @@ class DownloadLinkMailer < ApplicationMailer
       to: recipient.email,
       subject: t(
         "mailer.download_link.failure.subject",
-        application_name: t("app.title")
+        application_name: t("app.title"),
+        environment_name: environment_mailer_prefix
       )
     )
   end
