0c9be5402f9d2551e4d31d55c636c330e34a08dd
Cristina <579522+CristinaRO@users.noreply.github.com>
2022-08-17 17:41:00 +0100

    Show environment name in customised devise emails
    For non-production environments, add the environment name to the email
    subject of the welcome and password reset emails.
    
    NOTE: The welcome email uses a GOV.UK Notify template, which
    will have to be updated to include the `environment_mailer_prefix`
    personalisation.

--- spec/features/staff/beis_users_can_invite_new_users_spec.rb
+++ spec/features/staff/beis_users_can_invite_new_users_spec.rb
@@ -26,7 +26,8 @@ RSpec.feature "BEIS users can invite new users to the service" do
       expect(new_user).to have_received_email.with_personalisations(
         link: match(reset_password_link_regex),
         name: new_user_name,
-        service_url: "test.local"
+        service_url: "test.local",
+        environment_mailer_prefix: nil
       )
     end
 
