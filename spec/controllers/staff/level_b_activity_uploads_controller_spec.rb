3a27a7cb6806e21e18c5b8d7d36323584b2b85ff
Ynda Jas <yndajas@gmail.com>
2022-09-07 20:20:29 +0100

    Add Level B template CSV download action
    This adds a controller action to enable users to download the template
    CSV for Level B activity data bulk uploads, linked from the view/form
    created in the previous commit
    
    Since both the `#new` and `#show` actions require retrieving the
    organisation based on the organisation ID param, this is refactored into
    a private method used in both actions
    
    The CSV headers are a reduced version of the existing activity data bulk
    upload template, but may still be changed subject to client input

--- spec/controllers/staff/level_b_activity_uploads_controller_spec.rb
+++ spec/controllers/staff/level_b_activity_uploads_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Staff::LevelBActivityUploadsController do
-  let(:organisation) { create(:partner_organisation) }
+  let(:organisation) { create(:partner_organisation, beis_organisation_reference: "porg") }
   let(:user) { create(:beis_user) }
   before { authenticate!(user: user) }
 
@@ -16,6 +16,17 @@ RSpec.describe Staff::LevelBActivityUploadsController do
       get :new, params: {organisation_id: organisation.id}
 
       expect(response.body).to include(t("action.activity.bulk_download.button"))
+    end
+  end
+
+  describe "#show" do
+    it "downloads the CSV template with the correct filename" do
+      get :show, params: {organisation_id: organisation.id}
+
+      expect(response.headers.to_h).to include({
+        "Content-Type" => "text/csv",
+        "Content-Disposition" => "attachment; filename=PORG-Level_B_activities_upload.csv"
+      })
     end
   end
 end
