7944546ef33a057b442de1e35f97bdb707db6389
Ynda Jas <yndajas@gmail.com>
2022-09-07 20:20:29 +0100

    Add Level B template CSV download action
    This adds a controller action to enable users to download the template
    CSV for Level B activity data bulk uploads, linked from the view/form
    created in the previous commit
    
    Since both the `#new` and `#show` actions require retrieving the
    organisation based on the organisation ID param, this is refactored into
    a private method used in both actions
    
    The CSV headers are a reduced version of the existing activity data bulk
    upload template, but may still be changed subject to client input

--- spec/features/staff/beis_users_can_upload_level_b_activities_spec.rb
+++ spec/features/staff/beis_users_can_upload_level_b_activities_spec.rb
@@ -10,4 +10,38 @@ RSpec.feature "BEIS users can upload Level B activities" do
   scenario "viewing the page for downloading or uploading a CSV template" do
     expect(page).to have_content(t("page_title.activity.upload_level_b", organisation_name: organisation.name))
   end
+
+  scenario "downloading the CSV template" do
+    click_link t("action.activity.bulk_download.button")
+
+    csv_data = page.body.delete_prefix("\uFEFF")
+    rows = CSV.parse(csv_data, headers: false).first
+
+    expect(rows).to match_array([
+      "RODA ID",
+      "Parent RODA ID",
+      "Transparency identifier",
+      "Title",
+      "Description",
+      "Benefitting Countries",
+      "Partner organisation identifier",
+      "GDI",
+      "GCRF Strategic Area",
+      "GCRF Challenge Area",
+      "SDG 1", "SDG 2", "SDG 3",
+      "Newton Fund Pillar",
+      "Covid-19 related research",
+      "ODA Eligibility",
+      "Activity Status",
+      "Planned start date", "Planned end date",
+      "Actual start date", "Actual end date",
+      "Sector",
+      "Channel of delivery code",
+      "Collaboration type (Bi/Multi Marker)",
+      "Aid type",
+      "Free Standing Technical Cooperation",
+      "Aims/Objectives (PO Definition)",
+      "NF Partner Country PO"
+    ])
+  end
 end
