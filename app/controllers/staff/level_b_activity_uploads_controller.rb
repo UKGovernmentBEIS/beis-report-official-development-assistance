38450179e1dce34468df9f8ccec48045771987a4
Ynda Jas <yndajas@gmail.com>
2022-09-07 20:20:29 +0100

    Add Level B template CSV download action
    This adds a controller action to enable users to download the template
    CSV for Level B activity data bulk uploads, linked from the view/form
    created in the previous commit
    
    Since both the `#new` and `#show` actions require retrieving the
    organisation based on the organisation ID param, this is refactored into
    a private method used in both actions
    
    The CSV headers are a reduced version of the existing activity data bulk
    upload template, but may still be changed subject to client input

--- app/controllers/staff/level_b_activity_uploads_controller.rb
+++ app/controllers/staff/level_b_activity_uploads_controller.rb
@@ -2,9 +2,30 @@
 
 class Staff::LevelBActivityUploadsController < Staff::BaseController
   include Secured
+  include StreamCsvDownload
 
   def new
+    authorize organisation, :bulk_upload?
+
+    @organisation_presenter = OrganisationPresenter.new(organisation)
+  end
+
+  def show
+    authorize organisation, :bulk_upload?
+
+    @organisation_presenter = OrganisationPresenter.new(organisation)
+    filename = @organisation_presenter.filename_for_activities_template
+
+    stream_csv_download(filename: filename, headers: csv_headers)
+  end
+
+  private
+
+  def csv_headers
+    ["RODA ID"] + Activities::ImportFromCsv.level_b_column_headings
+  end
+
+  def organisation
     @organisation ||= Organisation.find(params[:organisation_id])
-    authorize @organisation, :bulk_upload?
   end
 end
