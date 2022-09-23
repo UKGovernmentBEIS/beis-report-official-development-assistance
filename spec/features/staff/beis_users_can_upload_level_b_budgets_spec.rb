aafac579ef727e37f27b82c6ff44cdc0ea3cb56d
Ynda Jas <yndajas@gmail.com>
2022-09-23 17:46:18 +0100

    Add Level B budget bulk upload template CSV download action
    This adds a controller action to enable users to download the template
    CSV for Level B budget data bulk uploads, linked from the view/form
    created in the previous commit
    
    The headers are as per the design collaboration with the client, but may still
    be subject to change. The naming of some columns differs slightly to that used
    in the activity templates
    
    Co-authored-by: Cristina <579522+CristinaRO@users.noreply.github.com>

--- spec/features/staff/beis_users_can_upload_level_b_budgets_spec.rb
+++ spec/features/staff/beis_users_can_upload_level_b_budgets_spec.rb
@@ -9,4 +9,23 @@ RSpec.feature "BEIS users can upload Level B budgets" do
   scenario "viewing the page for downloading or uploading a CSV template" do
     expect(page).to have_content(t("page_title.budget.upload_level_b"))
   end
+
+  scenario "downloading the CSV template" do
+    click_link t("action.budget.bulk_download.button")
+
+    csv_data = page.body.delete_prefix("\uFEFF")
+    rows = CSV.parse(csv_data, headers: false).first
+
+    expect(rows).to match_array([
+      "Type",
+      "Financial year",
+      "Budget amount",
+      "Providing organisation",
+      "Providing organisation type",
+      "IATI reference",
+      "Activity RODA ID",
+      "Fund RODA ID",
+      "Partner organisation name"
+    ])
+  end
 end
