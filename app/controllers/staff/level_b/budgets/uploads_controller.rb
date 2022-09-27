425fa7dbdd48cb40b5d9ded6339fddfd5af50d7e
Ynda Jas <yndajas@gmail.com>
2022-09-27 14:57:04 +0100

    Get headers from budget importer in uploads controller
    This refactors earlier work on the budget uploads controller's show action to
    use the values of the new budget importer's FIELDS constant, rather than duplicating
    the list

--- app/controllers/staff/level_b/budgets/uploads_controller.rb
+++ app/controllers/staff/level_b/budgets/uploads_controller.rb
@@ -12,13 +12,7 @@ class Staff::LevelB::Budgets::UploadsController < Staff::BaseController
     authorize :level_b, :budget_upload?
 
     headers = [
-      "Type",
-      "Financial year",
-      "Budget amount",
-      "Providing organisation",
-      "Providing organisation type",
-      "IATI reference",
-      "Activity RODA ID",
+      *::Budget::Import::Converter::FIELDS.values,
       "Fund RODA ID",
       "Partner organisation name"
     ]
