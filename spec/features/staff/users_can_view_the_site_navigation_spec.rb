d8dc40958a5d29098aeba1050c29b92ec41a5159
Ynda Jas <yndajas@gmail.com>
2022-10-03 12:36:35 +0100

    Update site navigation feature spec
    This updates the site navigation feature spec to bring it in line with the current
    expected navigation for BEIS and partner organisation users. The spec now tests for
    the presence of the budgets link, as well as a number of other links it previously
    ignored, and the absence of these for partner organisations where relevant
    (ensuring partner organisations aren't linked to pages they're not authorised to
    access)

--- spec/features/staff/users_can_view_the_site_navigation_spec.rb
+++ spec/features/staff/users_can_view_the_site_navigation_spec.rb
@@ -18,13 +18,17 @@ RSpec.feature "Users can view the site navigation" do
     let(:user) { create(:partner_organisation_user) }
     before { authenticate!(user: user) }
 
-    it "shows the appropriate naviation" do
+    it "shows the appropriate navigation" do
       visit organisation_path(user.organisation)
 
       expect(page).to have_css ".govuk-header__navigation"
       expect(page).to have_link t("page_title.home"), href: home_path
       expect(page).to have_link t("page_title.report.index"), href: reports_path
-      expect(page).not_to have_link t("page_title.organisation.index"), href: organisations_path, class: "govuk-header__link"
+      expect(page).to have_link t("page_title.activity.index"), href: organisation_activities_path(user.organisation)
+      expect(page).to have_link t("page_title.export.index"), href: exports_organisation_path(user.organisation)
+      expect(page).to have_link t("header.link.sign_out"), href: destroy_user_session_path
+      expect(page).not_to have_link t("page_title.organisation.index"), href: organisations_path
+      expect(page).not_to have_link t("page_title.budget.index"), href: new_level_b_budgets_upload_path
       expect(page).not_to have_link t("page_title.users.index"), href: users_path
     end
   end
@@ -33,14 +37,18 @@ RSpec.feature "Users can view the site navigation" do
     let(:user) { create(:beis_user) }
     before { authenticate!(user: user) }
 
-    it "shows the appropriate naviation" do
+    it "shows the appropriate navigation" do
       visit organisation_path(user.organisation)
 
       expect(page).to have_css ".govuk-header__navigation"
       expect(page).to have_link t("page_title.home"), href: home_path
       expect(page).to have_link t("page_title.report.index"), href: reports_path
+      expect(page).to have_link t("page_title.activity.index"), href: organisation_activities_path(user.organisation)
+      expect(page).to have_link t("page_title.budget.index"), href: new_level_b_budgets_upload_path
+      expect(page).to have_link t("page_title.export.index"), href: exports_path
       expect(page).to have_link t("page_title.organisation.index"), href: organisations_path
       expect(page).to have_link t("page_title.users.index"), href: users_path
+      expect(page).to have_link t("header.link.sign_out"), href: destroy_user_session_path
     end
   end
 end
