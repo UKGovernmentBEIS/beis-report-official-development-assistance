7bf2fd9e4f88ac7725bc5e1e6c45c5ccf4416a0f
Ynda Jas <yndajas@gmail.com>
2022-11-07 17:56:35 +0000

    Set `otp_required_for_login` to false for local dev users
    This automatically sets `otp_required_for_login` to false on seeding local
    development users. We always end up doing this in local development, so this
    removes a manual Rails console step from the process for syncing the local
    database with production data
    
    This seeds file is run in the `db-restore` script

--- db/seeds/development_users.rb
+++ db/seeds/development_users.rb
@@ -7,13 +7,13 @@ partner_org_user = User.find_or_initialize_by(
   email: "roda+dp@dxw.com"
 )
 
-administrator.password = "LlEeTtMmEeIiNn!1"
-partner_org_user.password = "LlEeTtMmEeIiNn!1"
-
 beis = Organisation.service_owner
-administrator.organisation = beis
-administrator.save!
-
 other_organisation = Organisation.find_by(role: :partner_organisation)
-partner_org_user.organisation = other_organisation
-partner_org_user.save!
+
+[
+  {user: administrator, organisation: beis},
+  {user: partner_org_user, organisation: other_organisation}
+].each do |hash|
+  hash[:user].update(password: "LlEeTtMmEeIiNn!1", otp_required_for_login: false, organisation: hash[:organisation])
+  hash[:user].save!
+end
