7586b20a6965bbe8b19496953580cfe51dd945f5
Cristina <579522+CristinaRO@users.noreply.github.com>
2022-11-28 15:10:07 +0000

    Specific policy for updating a linked activity
    Restrict editing the linked programme when there are already child
    activities being linked.
    
    We discussed with BEIS, and the scenario that an erroneous link at level
    B would have made it as far as projects being linked is very low -
    someone, most likely the partner org user, would have caught it before
    linking at level C.
    
    As such, we are comfortable preventing editing at programme level once a
    lower level link exists, to avoid accidentally breaking those lower
    level links.

--- spec/policies/activity_policy_spec.rb
+++ spec/policies/activity_policy_spec.rb
@@ -24,6 +24,8 @@ RSpec.describe ActivityPolicy do
 
         is_expected.to permit_action(:create_child)
         is_expected.to permit_action(:create_transfer)
+
+        is_expected.to forbid_action(:update_linked_activity)
       end
     end
 
@@ -43,6 +45,8 @@ RSpec.describe ActivityPolicy do
 
         is_expected.to forbid_action(:create_child)
         is_expected.to permit_action(:create_transfer)
+
+        is_expected.to permit_action(:update_linked_activity)
       end
 
       context "and there is an active report" do
@@ -50,6 +54,14 @@ RSpec.describe ActivityPolicy do
 
         it { is_expected.to permit_action(:create_refund) }
         it { is_expected.to forbid_action(:create_adjustment) }
+      end
+
+      context "when the activity has child activities that are linked to other activities" do
+        let(:activity) { create(:programme_activity, :ispf_funded) }
+
+        before { allow(activity).to receive(:linked_child_activities).and_return([double(:child_activity)]) }
+
+        it { is_expected.to forbid_action(:update_linked_activity) }
       end
     end
 
@@ -59,6 +71,7 @@ RSpec.describe ActivityPolicy do
       it "only permits show and redact_from_iati" do
         is_expected.to permit_action(:show)
         is_expected.to permit_action(:redact_from_iati)
+
         is_expected.to forbid_action(:create_refund)
         is_expected.to forbid_action(:create_adjustment)
 
@@ -69,6 +82,15 @@ RSpec.describe ActivityPolicy do
 
         is_expected.to forbid_action(:create_child)
         is_expected.to forbid_action(:create_transfer)
+        is_expected.to forbid_action(:update_linked_activity)
+      end
+
+      context "and there is an active report for the project's organisation" do
+        let(:activity) { create(:project_activity, :with_report) }
+
+        it "permits update_linked_activity" do
+          is_expected.to permit_action(:update_linked_activity)
+        end
       end
     end
 
@@ -88,6 +110,7 @@ RSpec.describe ActivityPolicy do
         is_expected.to forbid_action(:create_transfer)
         is_expected.to forbid_action(:create_refund)
         is_expected.to forbid_action(:create_adjustment)
+        is_expected.to forbid_action(:update_linked_activity)
       end
     end
   end
@@ -110,13 +133,14 @@ RSpec.describe ActivityPolicy do
         is_expected.to forbid_action(:create_transfer)
         is_expected.to forbid_action(:create_refund)
         is_expected.to forbid_action(:create_adjustment)
+        is_expected.to forbid_action(:update_linked_activity)
       end
     end
 
     context "when the activity is a programme" do
       let(:activity) { create(:programme_activity) }
 
-      context "and the users organisation is not the extending organisation" do
+      context "and the user's organisation is not the extending organisation" do
         it "forbids all actions" do
           is_expected.to forbid_action(:show)
           is_expected.to forbid_action(:create)
@@ -129,10 +153,11 @@ RSpec.describe ActivityPolicy do
           is_expected.to forbid_action(:create_transfer)
           is_expected.to forbid_action(:create_refund)
           is_expected.to forbid_action(:create_adjustment)
+          is_expected.to forbid_action(:update_linked_activity)
         end
       end
 
-      context "and the users organisation is the extending organisation" do
+      context "and the user's organisation is the extending organisation" do
         before do
           activity.update(extending_organisation: user.organisation)
         end
@@ -150,9 +175,10 @@ RSpec.describe ActivityPolicy do
           is_expected.to forbid_action(:create_transfer)
           is_expected.to forbid_action(:create_refund)
           is_expected.to forbid_action(:create_adjustment)
+          is_expected.to forbid_action(:update_linked_activity)
         end
 
-        context "and there is an editable report for the users organisation" do
+        context "and there is an editable report for the user's organisation" do
           before do
             report.update(state: :active)
           end
@@ -162,6 +188,7 @@ RSpec.describe ActivityPolicy do
             is_expected.to forbid_action(:create_transfer)
             is_expected.to forbid_action(:create_refund)
             is_expected.to forbid_action(:create_adjustment)
+            is_expected.to forbid_action(:update_linked_activity)
           end
         end
       end
@@ -170,7 +197,7 @@ RSpec.describe ActivityPolicy do
     context "when the activity is a project" do
       let(:activity) { create(:project_activity) }
 
-      context "and the users organisation is not the extending organisation" do
+      context "and the user's organisation is not the extending organisation" do
         it "forbids all actions" do
           is_expected.to forbid_action(:show)
           is_expected.to forbid_action(:create)
@@ -183,15 +210,16 @@ RSpec.describe ActivityPolicy do
           is_expected.to forbid_action(:create_transfer)
           is_expected.to forbid_action(:create_refund)
           is_expected.to forbid_action(:create_adjustment)
+          is_expected.to forbid_action(:update_linked_activity)
         end
       end
 
-      context "and the users organisation is the extending organisation" do
+      context "and the user's organisation is the extending organisation" do
         before do
           activity.update(organisation: user.organisation, extending_organisation: user.organisation)
         end
 
-        context "and there is no editable report for the users organisation" do
+        context "and there is no editable report for the user's organisation" do
           before do
             report.update(state: :approved)
           end
@@ -209,10 +237,11 @@ RSpec.describe ActivityPolicy do
             is_expected.to forbid_action(:create_transfer)
             is_expected.to forbid_action(:create_refund)
             is_expected.to forbid_action(:create_adjustment)
+            is_expected.to forbid_action(:update_linked_activity)
           end
         end
 
-        context "and there is an editable report for the users organisation" do
+        context "and there is an editable report for the user's organisation" do
           before do
             report.update(state: :active)
           end
@@ -230,6 +259,7 @@ RSpec.describe ActivityPolicy do
             is_expected.to permit_action(:create_transfer)
             is_expected.to permit_action(:create_refund)
             is_expected.to permit_action(:create_adjustment)
+            is_expected.to permit_action(:update_linked_activity)
           end
         end
       end
@@ -238,7 +268,7 @@ RSpec.describe ActivityPolicy do
     context "when the activity is a third-party project" do
       let(:activity) { create(:third_party_project_activity) }
 
-      context "and the users organisation is not the extending organisation" do
+      context "and the user's organisation is not the extending organisation" do
         it "forbids all actions" do
           is_expected.to forbid_action(:show)
           is_expected.to forbid_action(:create)
@@ -251,15 +281,16 @@ RSpec.describe ActivityPolicy do
           is_expected.to forbid_action(:create_transfer)
           is_expected.to forbid_action(:create_refund)
           is_expected.to forbid_action(:create_adjustment)
+          is_expected.to forbid_action(:update_linked_activity)
         end
       end
 
-      context "and the users organisation is the extending organisation" do
+      context "and the user's organisation is the extending organisation" do
         before do
           activity.update(organisation: user.organisation, extending_organisation: user.organisation)
         end
 
-        context "and there is no editable report for the users organisation" do
+        context "and there is no editable report for the user's organisation" do
           it "only permits show" do
             is_expected.to permit_action(:show)
 
@@ -272,15 +303,16 @@ RSpec.describe ActivityPolicy do
             is_expected.to forbid_action(:create_child)
             is_expected.to forbid_action(:create_transfer)
             is_expected.to forbid_action(:create_refund)
+            is_expected.to forbid_action(:update_linked_activity)
           end
         end
 
-        context "and there is an editable report for the users organisation" do
+        context "and there is an editable report for the user's organisation" do
           before do
             report.update(state: :active)
           end
 
-          it "only forbids destroy, redact_from_iati, and create_child" do
+          it "only forbids destroy, redact_from_iati, update_linked_activity, and create_child" do
             is_expected.to permit_action(:show)
             is_expected.to permit_action(:create)
             is_expected.to permit_action(:edit)
@@ -288,6 +320,7 @@ RSpec.describe ActivityPolicy do
 
             is_expected.to forbid_action(:destroy)
             is_expected.to forbid_action(:redact_from_iati)
+            is_expected.to forbid_action(:update_linked_activity)
 
             is_expected.to forbid_action(:create_child)
             is_expected.to permit_action(:create_transfer)
