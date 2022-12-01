5e9e60c2b4bc1f2463b7bfa9e08870963ec38a39
Ynda Jas <yndajas@gmail.com>
2022-12-01 13:34:04 +0000

    Exclude non-ODA programmes from IATI XML exports
    While `FindProgrammeActivities` doesn't appear to be used outside the
    `programme_activities` action in the `OrganisationsController`, I've opted to keep
    it somewhat flexible to different use cases, since it was already written that way
    
    By default, the `FindProgrammeActivities` service will now exclude ISPF non-ODA
    activities, but it has an optional parameter for including them
    
    Interestingly, the extending organisation and fund filters are only applied when
    the extending organisation is not BEIS. Given this class is only used in one
    method, I wonder how necessary this distinction is. For now I've opted to include
    the `include_ispf_non_oda_activities` filter further up, since we always want it
    regardless of organisation, as I understand it

--- app/services/find_programme_activities.rb
+++ app/services/find_programme_activities.rb
@@ -1,16 +1,20 @@
 class FindProgrammeActivities
   include Pundit::Authorization
 
-  attr_accessor :organisation, :user, :fund_code
+  attr_accessor :organisation, :user, :fund_code, :include_ispf_non_oda_activities
 
-  def initialize(organisation:, user:, fund_code: nil)
+  def initialize(organisation:, user:, fund_code: nil, include_ispf_non_oda_activities: false)
     @organisation = organisation
     @user = user
     @fund_code = fund_code
+    @include_ispf_non_oda_activities = include_ispf_non_oda_activities
   end
 
   def call
-    programmes = ProgrammePolicy::Scope.new(user, Activity.programme)
+    is_oda = [nil, true]
+    is_oda << false if include_ispf_non_oda_activities
+
+    programmes = ProgrammePolicy::Scope.new(user, Activity.programme.where(is_oda: is_oda))
       .resolve
       .includes(
         :organisation,
