f35daee743daa760c3ad32b5d0c0180ff0bc7951
Ynda Jas <yndajas@gmail.com>
2022-12-07 17:14:23 +0000

    Reject ODA fields in non-ODA activity upload
    If a user uploads a completed ODA bulk upload template using the non-ODA form, we
    don't want to create a non-ODA activity with the additional fields, but instead
    reject the upload since it doesn't match out expectations for ODA typing. This
    safeguards against uploading activities as the wrong type by mistake. This only
    seems to be possible in one direction, since the ODA templates have all the
    non-ODA fields and more, while the non-ODA templates don't include various required
    fields for ODA
    
    This compares the ODA and non-ODA attributes and adds an error if any unexpected
    attributes appear in the non-ODA upload

--- spec/services/activity/import_spec.rb
+++ spec/services/activity/import_spec.rb
@@ -313,6 +313,111 @@ RSpec.describe Activity::Import do
     end
   end
 
+  describe "::invalid_non_oda_attribute_errors" do
+    let(:converted_level_b_oda_attributes) {
+      {
+        roda_identifier: "",
+        linked_activity_id: "",
+        transparency_identifier: "1234",
+        title: "A title",
+        description: "A description",
+        benefitting_countries: "AO",
+        partner_organisation_identifier: "",
+        gdi: "4",
+        sdg_1: "",
+        sdg_2: "",
+        sdg_3: "",
+        oda_eligibility: "1",
+        programme_status: "7",
+        planned_start_date: "10/10/2020",
+        planned_end_date: "10/10/2021",
+        actual_start_date: "",
+        actual_end_date: "",
+        sector: "12182",
+        aid_type: "D01",
+        objectives: "Freetext objectives",
+        ispf_themes: "1",
+        ispf_partner_countries: "BR|EG",
+        comments: "This is a comment"
+      }
+    }
+
+    context "Level B" do
+      let(:activity) { create(:programme_activity, :ispf_funded, is_oda: true) }
+      let(:error_translation) { t("importer.errors.activity.oda_attribute_in_non_oda_activity") }
+
+      it "returns a hash with errors where ODA fields have been included in a non-ODA upload" do
+        expect(Activity::Import.invalid_non_oda_attribute_errors(
+          activity: activity, converted_attributes: converted_level_b_oda_attributes
+        )).to eq(
+          {
+            aid_type: ["D01", error_translation],
+            benefitting_countries: ["AO", error_translation],
+            gdi: ["4", error_translation],
+            objectives: ["Freetext objectives", error_translation],
+            oda_eligibility: ["1", error_translation]
+          }
+        )
+      end
+    end
+
+    context "Level C/D" do
+      let(:activity) { create(:project_activity, :ispf_funded, is_oda: true) }
+      let(:error_translation) { t("importer.errors.activity.oda_attribute_in_non_oda_activity") }
+
+      let(:converted_level_c_d_oda_attributes) {
+        converted_level_b_oda_attributes.merge(
+          {
+            covid19_related: "1",
+            oda_eligibility_lead: "ODA lead 1",
+            call_open_date: "11/01/2019",
+            call_close_date: "11/05/2019",
+            total_applications: "14",
+            total_awards: "5",
+            channel_of_delivery_code: "11000",
+            collaboration_type: "1",
+            policy_marker_gender: "gender",
+            policy_marker_climate_change_adaptation: "adaptation",
+            policy_marker_climate_change_mitigation: "mitigation",
+            policy_marker_biodiversity: "biodiversity",
+            policy_marker_desertification: "desertification",
+            policy_marker_disability: "disability",
+            policy_marker_disaster_risk_reduction: "reduction",
+            policy_marker_nutrition: "nutrition",
+            fstc_applies: "0",
+            uk_po_named_contact: "Someone Somebody"
+          }
+        )
+      }
+
+      it "returns a hash with errors where ODA fields have been included in a non-ODA upload" do
+        expect(Activity::Import.invalid_non_oda_attribute_errors(
+          activity: activity, converted_attributes: converted_level_c_d_oda_attributes
+        )).to eq(
+          {
+            aid_type: ["D01", error_translation],
+            benefitting_countries: ["AO", error_translation],
+            channel_of_delivery_code: ["11000", error_translation],
+            collaboration_type: ["1", error_translation],
+            covid19_related: ["1", error_translation],
+            fstc_applies: ["0", error_translation],
+            gdi: ["4", error_translation],
+            objectives: ["Freetext objectives", error_translation],
+            oda_eligibility_lead: ["ODA lead 1", error_translation],
+            policy_marker_biodiversity: ["biodiversity", error_translation],
+            policy_marker_climate_change_adaptation: ["adaptation", error_translation],
+            policy_marker_climate_change_mitigation: ["mitigation", error_translation],
+            policy_marker_desertification: ["desertification", error_translation],
+            policy_marker_disability: ["disability", error_translation],
+            policy_marker_disaster_risk_reduction: ["reduction", error_translation],
+            policy_marker_gender: ["gender", error_translation],
+            policy_marker_nutrition: ["nutrition", error_translation]
+          }
+        )
+      end
+    end
+  end
+
   describe "::is_oda_by_type" do
     context "when passed `:ispf_oda` as the type" do
       it "returns true" do
