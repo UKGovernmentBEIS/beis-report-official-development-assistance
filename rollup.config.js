02322cb02daff988029117c66cdb60b7a76a0061
George Eaton <george@dxw.com>
2022-12-19 15:59:52 +0000

    Output JS as iife
    This allows us to `import` separate JS components and have them all run
    when the JS is invoked (i.e. on pageload)

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import resolve from "@rollup/plugin-node-resolve"
+import { babel } from "@rollup/plugin-babel";
 
 export default {
   input: "app/javascript/application.js",
   output: {
     file: "app/assets/builds/application.js",
-    format: "es",
+    format: "iife",
     inlineDynamicImports: true,
     sourcemap: true,
   },
