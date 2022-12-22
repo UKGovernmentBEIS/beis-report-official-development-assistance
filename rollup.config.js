085303bd8b558bdaa2895bc89476e590965b9c09
George Eaton <george@dxw.com>
2022-12-22 16:03:30 +0000

    Add CommonJS rollup plugin
    This allows us to convert CommonJS modules (like a lot of the JS modules
    we currently have) into ES6 for use with Rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 
 export default {
@@ -10,6 +11,7 @@ export default {
     sourcemap: true,
   },
   plugins: [
+    commonjs(),
     babel({
       babelHelpers: "runtime",
       exclude: "node_modules/**",
