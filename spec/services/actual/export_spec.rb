726a7c5b2bdab6e078e84fe529c0fd4621cc26c1
Joseph Kempster <jkempster34@gmail.com>
2023-02-16 10:37:41 +0000

    Include the BEIS identifier in the Actual Export spec
    The RODA identifier and BEIS identifier make up the first two columns of actual
    exports.
    
    The BEIS identifier is missing from this spec, so this adds it in.

--- spec/services/actual/export_spec.rb
+++ spec/services/actual/export_spec.rb
@@ -2,54 +2,42 @@ RSpec.describe Actual::Export do
   let(:partner_organisation) { create(:partner_organisation) }
   let(:activities) { Activity.where(organisation: partner_organisation) }
   let(:export) { Actual::Export.new(activities) }
-
-  let :quarter_headers do
-    export.headers.drop(2)
-  end
-
-  let :actual_data do
-    export.rows.map { |row| row.take(1) + row.drop(2) }
-  end
+  let(:quarter_headers) { export.headers.drop(2) }
+  let(:actual_data) { export.rows }
+  let!(:project) { create(:project_activity, organisation: partner_organisation, beis_identifier: SecureRandom.uuid) }
 
   it "exports an empty data set" do
-    project = create(:project_activity, organisation: partner_organisation)
-
     expect(quarter_headers).to eq []
 
     expect(actual_data).to eq([
-      [project.roda_identifier]
+      [project.roda_identifier, project.beis_identifier]
     ])
   end
 
   it "exports one quarter of spend for a single project" do
-    project = create(:project_activity, organisation: partner_organisation)
-
     create(:actual, parent_activity: project, financial_year: 2014, financial_quarter: 1, value: 10)
     create(:actual, parent_activity: project, financial_year: 2014, financial_quarter: 1, value: 20)
 
     expect(quarter_headers).to eq ["FQ1 2014-2015"]
 
     expect(actual_data).to eq([
-      [project.roda_identifier, "30.00"]
+      [project.roda_identifier, project.beis_identifier, "30.00"]
     ])
   end
 
   it "exports two quarters of spend for a single project with zeros for intervening quarters" do
-    project = create(:project_activity, organisation: partner_organisation)
-
     create(:actual, parent_activity: project, financial_year: 2014, financial_quarter: 1, value: 10)
     create(:actual, parent_activity: project, financial_year: 2014, financial_quarter: 4, value: 20)
 
     expect(quarter_headers).to eq ["FQ1 2014-2015", "FQ2 2014-2015", "FQ3 2014-2015", "FQ4 2014-2015"]
 
     expect(actual_data).to eq([
-      [project.roda_identifier, "10.00", "0.00", "0.00", "20.00"]
+      [project.roda_identifier, project.beis_identifier, "10.00", "0.00", "0.00", "20.00"]
     ])
   end
 
   it "exports actual spend for two activities across different quarters" do
-    project = create(:project_activity, organisation: partner_organisation)
-    third_party_project = create(:third_party_project_activity, organisation: partner_organisation)
+    third_party_project = create(:third_party_project_activity, organisation: partner_organisation, beis_identifier: SecureRandom.uuid)
 
     create(:actual, parent_activity: project, financial_year: 2014, financial_quarter: 1, value: 10)
     create(:actual, parent_activity: third_party_project, financial_year: 2015, financial_quarter: 2, value: 20)
@@ -57,22 +45,21 @@ RSpec.describe Actual::Export do
     expect(quarter_headers).to eq ["FQ1 2014-2015", "FQ2 2014-2015", "FQ3 2014-2015", "FQ4 2014-2015", "FQ1 2015-2016", "FQ2 2015-2016"]
 
     expect(actual_data).to match_array([
-      [project.roda_identifier, "10.00", "0.00", "0.00", "0.00", "0.00", "0.00"],
-      [third_party_project.roda_identifier, "0.00", "0.00", "0.00", "0.00", "0.00", "20.00"]
+      [project.roda_identifier, project.beis_identifier, "10.00", "0.00", "0.00", "0.00", "0.00", "0.00"],
+      [third_party_project.roda_identifier, third_party_project.beis_identifier, "0.00", "0.00", "0.00", "0.00", "0.00", "20.00"]
     ])
   end
 
   it "includes activities that do not have any actuals recorded" do
-    project = create(:project_activity, organisation: partner_organisation)
-    third_party_project = create(:third_party_project_activity, organisation: partner_organisation)
+    third_party_project = create(:third_party_project_activity, organisation: partner_organisation, beis_identifier: SecureRandom.uuid)
 
     create(:actual, parent_activity: project, financial_year: 2014, financial_quarter: 1, value: 10)
 
     expect(quarter_headers).to eq ["FQ1 2014-2015"]
 
     expect(actual_data).to match_array([
-      [project.roda_identifier, "10.00"],
-      [third_party_project.roda_identifier, "0.00"]
+      [project.roda_identifier, project.beis_identifier, "10.00"],
+      [third_party_project.roda_identifier, third_party_project.beis_identifier, "0.00"]
     ])
   end
 end
