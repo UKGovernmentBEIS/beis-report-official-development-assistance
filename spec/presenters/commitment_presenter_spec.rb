801d11e6e3377e40e9fc0b44a48153722c5f0088
George Eaton <george@dxw.com>
2023-02-20 15:29:40 +0000

    Update Commitment table to use `transaction_date`
    over financial year & quarter.

--- spec/presenters/commitment_presenter_spec.rb
+++ spec/presenters/commitment_presenter_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe CommitmentPresenter do
       expect(described_class.new(commitment).value).to eq("£100,000.00")
     end
   end
+
+  describe "#transaction_date" do
+    it "returns a human readable date" do
+      commitment = build(:commitment, transaction_date: Date.parse("2023-02-20"))
+      result = described_class.new(commitment).transaction_date
+      expect(result).to eq("20 Feb 2023")
+    end
+  end
 end
