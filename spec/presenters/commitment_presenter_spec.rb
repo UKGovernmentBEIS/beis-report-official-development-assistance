bc830571431054fa7e3296fd5e1b057a0b066fba
George Eaton <george@dxw.com>
2023-02-20 15:36:38 +0000

    Update test to use Commitment factory
    as we use it elsewhere in this spec file.

--- spec/presenters/commitment_presenter_spec.rb
+++ spec/presenters/commitment_presenter_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe CommitmentPresenter do
   describe "#value" do
     it "returns the value to two decimal places with a currency symbol" do
-      commitment = Commitment.new(value: 100_000)
+      commitment = build(:commitment, value: 100_000)
       expect(described_class.new(commitment).value).to eq("£100,000.00")
     end
   end
