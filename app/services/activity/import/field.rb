23ae9a0cf1790453e08c19815c05b42e9c53e3f7
Ynda Jas <yndajas@gmail.com>
2023-03-17 16:27:22 +0000

    Standardise activity import subclass namespacing
    Namespacing in the format `class A::B::C` can create sibling class
    lookup issues. This isn't really a problem here, since `Field` doesn't
    use any of its sibling classes, but it has been an issue in some of its
    siblings, which have therefore been namespaced in the format below. In
    order to keep all the `Activity::Import` subclasses syntactically
    consistent, this brings `Field` in line
    
    ```
    class A
      class B
        class C
    ```

--- app/services/activity/import/field.rb
+++ app/services/activity/import/field.rb
@@ -1,76 +1,80 @@
-class Activity::Import::Field
-  class << self
-    def all
-      from_source(source)
-    end
+class Activity
+  class Import
+    class Field
+      class << self
+        def all
+          from_source(source)
+        end
 
-    def find_by_attribute_name(attribute_name:)
-      field = source[attribute_name]
-      return nil if field.nil?
+        def find_by_attribute_name(attribute_name:)
+          field = source[attribute_name]
+          return nil if field.nil?
 
-      from_source({attribute_name => field}).first
-    end
+          from_source({attribute_name => field}).first
+        end
 
-    def where_headings(headings:)
-      fields = source.select { |_key, value| headings.include?(value[:heading]) }
+        def where_headings(headings:)
+          fields = source.select { |_key, value| headings.include?(value[:heading]) }
 
-      from_source(fields)
-    end
+          from_source(fields)
+        end
 
-    def where_level_and_type(level:, type:)
-      fields = source.select { |_key, value| value.dig(:inclusion, level, type) }
+        def where_level_and_type(level:, type:)
+          fields = source.select { |_key, value| value.dig(:inclusion, level, type) }
 
-      from_source(fields)
-    end
+          from_source(fields)
+        end
 
-    def invalid_for_level_b_ispf_non_oda
-      level_b_non_oda_attributes = []
-      level_b_oda_attributes = []
+        def invalid_for_level_b_ispf_non_oda
+          level_b_non_oda_attributes = []
+          level_b_oda_attributes = []
 
-      source.each do |attribute, column|
-        level_b_non_oda_attributes << attribute if column.dig(:inclusion, :level_b, :ispf_non_oda)
-        level_b_oda_attributes << attribute if column.dig(:inclusion, :level_b, :ispf_oda)
+          source.each do |attribute, column|
+            level_b_non_oda_attributes << attribute if column.dig(:inclusion, :level_b, :ispf_non_oda)
+            level_b_oda_attributes << attribute if column.dig(:inclusion, :level_b, :ispf_oda)
+          end
+
+          level_b_oda_attributes - level_b_non_oda_attributes
+        end
+
+        def invalid_for_level_c_d_ispf_non_oda
+          level_c_d_non_oda_attributes = []
+          level_c_d_oda_attributes = []
+
+          source.each do |attribute, column|
+            level_c_d_non_oda_attributes << attribute if column.dig(:inclusion, :level_c_d, :ispf_non_oda)
+            level_c_d_oda_attributes << attribute if column.dig(:inclusion, :level_c_d, :ispf_oda)
+          end
+
+          level_c_d_oda_attributes - level_c_d_non_oda_attributes
+        end
+
+        private
+
+        def from_source(fields)
+          fields.map do |key, value|
+            new(
+              attribute_name: key.to_sym,
+              heading: value[:heading],
+              exclude_from_converter: value[:exclude_from_converter]
+            )
+          end
+        end
+
+        def source
+          ACTIVITY_CSV_COLUMNS.reject do |key, _|
+            key == :linked_activity_id && !ROLLOUT.active?(:activity_linking)
+          end
+        end
       end
 
-      level_b_oda_attributes - level_b_non_oda_attributes
-    end
+      attr_reader :attribute_name, :heading, :exclude_from_converter
 
-    def invalid_for_level_c_d_ispf_non_oda
-      level_c_d_non_oda_attributes = []
-      level_c_d_oda_attributes = []
-
-      source.each do |attribute, column|
-        level_c_d_non_oda_attributes << attribute if column.dig(:inclusion, :level_c_d, :ispf_non_oda)
-        level_c_d_oda_attributes << attribute if column.dig(:inclusion, :level_c_d, :ispf_oda)
-      end
-
-      level_c_d_oda_attributes - level_c_d_non_oda_attributes
-    end
-
-    private
-
-    def from_source(fields)
-      fields.map do |key, value|
-        new(
-          attribute_name: key.to_sym,
-          heading: value[:heading],
-          exclude_from_converter: value[:exclude_from_converter]
-        )
+      def initialize(attribute_name:, heading:, exclude_from_converter:)
+        @attribute_name = attribute_name
+        @heading = heading
+        @exclude_from_converter = exclude_from_converter
       end
     end
-
-    def source
-      ACTIVITY_CSV_COLUMNS.reject do |key, _|
-        key == :linked_activity_id && !ROLLOUT.active?(:activity_linking)
-      end
-    end
-  end
-
-  attr_reader :attribute_name, :heading, :exclude_from_converter
-
-  def initialize(attribute_name:, heading:, exclude_from_converter:)
-    @attribute_name = attribute_name
-    @heading = heading
-    @exclude_from_converter = exclude_from_converter
   end
 end
