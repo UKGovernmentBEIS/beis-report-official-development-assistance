41a32706ebd9a0dbcd55a834d2c89c8259f65c78
Cristina <579522+CristinaRO@users.noreply.github.com>
2023-11-07 15:25:56 +0000

    Pre-existing later report validation considers ODA

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -78,6 +78,7 @@ RSpec.describe Report, type: :model do
       describe "Ensuring that a new report does not attempt to rewrite history" do
         let(:organisation) { create(:partner_organisation) }
         let(:fund) { create(:fund_activity) }
+
         context "where a report already exists for a period later than that of the new report" do
           it "is not valid" do
             create(:report, :approved, organisation: organisation, fund: fund, financial_quarter: 4, financial_year: 2018)
@@ -86,7 +87,18 @@ RSpec.describe Report, type: :model do
               expect(new_report.valid?(:new)).to be(false)
             end
           end
+
+          context "and the existing report has a different ODA type than the new report" do
+            it "is valid" do
+              create(:report, :for_ispf, :approved, is_oda: false, organisation: organisation, financial_quarter: 4, financial_year: 2018)
+              travel_to(Date.parse("01-04-2020")) do
+                new_report = build(:report, :for_ispf, is_oda: true, organisation: organisation, financial_quarter: 3, financial_year: 2018)
+                expect(new_report.valid?(:new)).to be(true)
+              end
+            end
+          end
         end
+
         context "where a report does not exist for a period later than that of the new report" do
           it "is valid" do
             create(:report, :approved, organisation: organisation, fund: fund, financial_quarter: 4, financial_year: 2018)
