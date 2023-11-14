79f92ce46884b44e228ce92677f597942441cf4f
Cristina <579522+CristinaRO@users.noreply.github.com>
2023-11-14 17:15:39 +0000

    Rewrite uploader and downloader

--- app/services/export/s3_downloader.rb
+++ app/services/export/s3_downloader.rb
@@ -3,28 +3,17 @@ module Export
 
   class S3Downloader
     def initialize(filename:)
-      @config = S3UploaderConfig.new(use_public_bucket: false)
-      @bucket_name_from_env_var = ENV.fetch("EXPORT_DOWNLOAD_S3_BUCKET", false)
-      @client = if @bucket_name_from_env_var
-        Aws::S3::Client.new(
-          region: "eu-west-2",
-          credentials: Aws::ECSCredentials.new(retries: 3)
-        )
-      else
-        Aws::S3::Client.new(
-          region: config.region,
-          credentials: Aws::Credentials.new(
-            config.key_id,
-            config.secret_key
-          )
-        )
-      end
+      @config = S3Config.new
+      @client = Aws::S3::Client.new(
+        region: config.region,
+        credentials: Aws::ECSCredentials.new(retries: 3)
+      )
       @filename = filename
     end
 
     def download
       client.get_object(
-        bucket: @bucket_name_from_env_var || config.bucket,
+        bucket: config.bucket,
         key: filename,
         response_content_type: "text/csv"
       ).body.read
