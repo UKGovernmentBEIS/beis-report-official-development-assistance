4534edbd950e7d25bf34d5c560a67868d841a440
meyric <meyric@dxw.com>
2024-01-03 16:46:33 +0000

    Fix matched effort policy spec for factory bot rails
    The change introduced in factory bot rails 6.4.3 impacts this spec.
    
    https://github.com/thoughtbot/factory_bot_rails/commit/0040292890541736d0065c30ab2f798e8608787d#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5R168
    
    One we switch to Factory bot Rails > 6.4.3 we began to see Bullet
    errors when running this spec.
    
    The issue here appears to be caused by `build_stubbed` objects no longer
    behaving like database objects (which they should not have been in the
    first place), with the most straightforward fix being switching to
    `create` which utilises the database.
    
    Whilst there is a performance hit to using `create` this policy relies
    on the `ActivityPolicy` which in turns loads objects from the
    database, making stubbing pretty complicated.
    
    For the moment, we will take the perfromance hit for working specs.

--- spec/policies/matched_effort_policy_spec.rb
+++ spec/policies/matched_effort_policy_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe MatchedEffortPolicy do
   subject { described_class.new(user, matched_effort) }
 
   let!(:report) { create(:report, :approved, organisation: user.organisation, fund: activity.associated_fund) }
-  let(:matched_effort) { build_stubbed(:matched_effort, activity: activity) }
+  let(:matched_effort) { create(:matched_effort, activity: activity) }
 
   context "as a user that belongs to BEIS" do
-    let(:user) { build_stubbed(:beis_user) }
+    let(:user) { create(:beis_user) }
 
     context "when the activity is a programme activity owned by the organisation" do
-      let(:activity) { build_stubbed(:programme_activity, organisation: user.organisation) }
+      let(:activity) { create(:programme_activity, organisation: user.organisation) }
 
       it "permits all actions" do
         is_expected.to permit_action(:create)
@@ -20,7 +20,7 @@ RSpec.describe MatchedEffortPolicy do
     end
 
     context "when the activity is a project activity" do
-      let(:activity) { build_stubbed(:project_activity) }
+      let(:activity) { create(:project_activity) }
 
       it "forbids all actions" do
         is_expected.to forbid_action(:create)
@@ -31,10 +31,10 @@ RSpec.describe MatchedEffortPolicy do
   end
 
   context "as a partner organisation user" do
-    let(:user) { build_stubbed(:partner_organisation_user) }
+    let(:user) { create(:partner_organisation_user) }
 
     context "when the matched effort belongs to an activity owned by the user" do
-      let(:activity) { build_stubbed(:project_activity, organisation: user.organisation) }
+      let(:activity) { create(:project_activity, organisation: user.organisation) }
 
       context "when there is an editable report for the organisation" do
         before do
@@ -58,7 +58,7 @@ RSpec.describe MatchedEffortPolicy do
     end
 
     context "when the matched effort does not belong to an activity owned by the user" do
-      let(:activity) { build_stubbed(:project_activity) }
+      let(:activity) { create(:project_activity) }
 
       context "when there is an editable report for the organisation" do
         before do
