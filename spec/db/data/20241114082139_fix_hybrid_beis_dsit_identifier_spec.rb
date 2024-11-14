bf474b9721d991ffc0b045cdadbe6d7f92063885
meyric <meyric@dxw.com>
2024-11-14 15:27:19 +0000

    WIP: Data migration

---
+++ spec/db/data/20241114082139_fix_hybrid_beis_dsit_identifier_spec.rb
@@ -0,0 +1,71 @@
+require Rails.root + "db/data/20241114082139_fix_hybrid_beis_dsit_identifier.rb"
+
+RSpec.describe FixHybridBeisDsitIdentifier do
+  describe "#migrate!" do
+    it "updates only the appropriate activities" do
+      activity_to_update = create(
+        :programme_activity,
+        transparency_identifier: "GB-GOV-26-AAAA-BBBB-CCC-DDDDDDD",
+        previous_identifier: "GB-GOV-13-AAAA-BBBB-CCC-DDDDDDD",
+        hybrid_beis_dsit_activity: false
+      )
+      activity_to_ignore = create(
+        :programme_activity,
+        transparency_identifier: "GB-GOV-26-EEEE-FFFF-GGG-HHHHHHH",
+        previous_identifier: nil,
+        hybrid_beis_dsit_activity: false
+      )
+
+      described_class.new.migrate!
+
+      expect(activity_to_update.reload.transparency_identifier).to eql "GB-GOV-13-AAAA-BBBB-CCC-DDDDDDD"
+      expect(activity_to_update.hybrid_beis_dsit_activity).to be true
+
+      expect(activity_to_ignore.reload.transparency_identifier).to eql "GB-GOV-26-EEEE-FFFF-GGG-HHHHHHH"
+      expect(activity_to_ignore.hybrid_beis_dsit_activity).to be false
+    end
+  end
+
+  describe "#other_identifier_starts_with_beis" do
+    it "returns true for identifiers that start GB-GOV-13" do
+      valid_identifier = "GB-GOV-13-1234-5678-91011"
+
+      expect(described_class.new.other_identifier_starts_with_beis(valid_identifier)).to be true
+    end
+
+    it "returns false for identifiers that do not start GB-GOV-13" do
+      valid_identifier = "GB-GOV-10-1234-5678-91011"
+
+      expect(described_class.new.other_identifier_starts_with_beis(valid_identifier)).to be false
+    end
+  end
+
+  describe "#fix_activity" do
+    let(:activity) do
+      create(
+        :programme_activity,
+        transparency_identifier: "GB-GOV-26-AAAA-BBBB-CCC-DDDDDDD",
+        previous_identifier: "GB-GOV-13-AAAA-BBBB-CCC-DDDDDDD",
+        hybrid_beis_dsit_activity: false
+      )
+    end
+
+    it "updates the transparency_identifier to the value of previous_identifier" do
+      described_class.new.fix_activity(activity)
+
+      expect(activity.transparency_identifier).to eql "GB-GOV-13-AAAA-BBBB-CCC-DDDDDDD"
+    end
+
+    it "updates the previous_identifier to nil" do
+      described_class.new.fix_activity(activity)
+
+      expect(activity.previous_identifier).to be_nil
+    end
+
+    it "updates the hybrid BEIS DSIT state to true" do
+      described_class.new.fix_activity(activity)
+
+      expect(activity.hybrid_beis_dsit_activity).to be true
+    end
+  end
+end
