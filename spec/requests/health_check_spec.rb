5e6ccd612a1233132888a91e4b9388d02a16a8e8
Russell Garner <rgarner@zephyros-systems.co.uk>
2025-01-06 14:21:53 +0000

    Fix flaky healthcheck spec
    When run on a box with another project that had left unconsumed jobs
    in the Sidekiq retry queue, the health check hash matching spec would
    fail. In reality we don't care about the contents of the local Sidekiq
    Redis DB, so we can just match on the keys we do care about and not
    worry about the state of the world with Redis.

--- spec/requests/health_check_spec.rb
+++ spec/requests/health_check_spec.rb
@@ -6,14 +6,13 @@ RSpec.describe "Health Check", type: :request do
     ClimateControl.modify CURRENT_SHA: "b9c73f88", TIME_OF_BUILD: "2020-01-01T00:00:00Z" do
       get "/health_check", headers: {"CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"}
       expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body)).to eql(
-        "rails" => "OK",
-        "git_sha" => "b9c73f88",
-        "built_at" => "2020-01-01T00:00:00Z",
-        "sidekiq" => {
-          "enqueued" => 0,
-          "retry_size" => 0
-        }
+      expect(JSON.parse(response.body)).to match(
+        a_hash_including(
+          "rails" => "OK",
+          "git_sha" => "b9c73f88",
+          "built_at" => "2020-01-01T00:00:00Z",
+          "sidekiq" => anything,
+        )
       )
     end
   end
