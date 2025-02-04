bdde12542db201f19493804b709a7de519f6cc67
Russell Garner <rgarner@zephyros-systems.co.uk>
2025-02-04 09:55:12 +0000

    devise-two-factor 5.x upgrade phase 2
    Clean up legacy columns and legacy OTP accessor method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_16_172647) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_04_092602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -361,9 +361,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_16_172647) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: nil
     t.datetime "remember_created_at", precision: nil
-    t.string "encrypted_otp_secret"
-    t.string "encrypted_otp_secret_iv"
-    t.string "encrypted_otp_secret_salt"
     t.integer "consumed_timestep"
     t.boolean "otp_required_for_login", default: true
     t.string "mobile_number"
