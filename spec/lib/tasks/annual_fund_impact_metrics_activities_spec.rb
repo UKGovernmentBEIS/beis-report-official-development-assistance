c99203a9f3c5f516b94eb8b90c2a76f3c1f6505f
George Eaton <george@dxw.com>
2025-07-23 15:21:50 +0100

    Refactor tests to do less upfront setup
    We're about to make some changes to the KPI rake task, but the test
    setup makes it quite difficult to ascertain what setup is required for
    each test. Here we perform the necessary setup inside each test to make
    it easier to see what's going on.

--- spec/lib/tasks/annual_fund_impact_metrics_activities_spec.rb
+++ spec/lib/tasks/annual_fund_impact_metrics_activities_spec.rb
@@ -1,23 +1,6 @@
 require "tempfile"
 
 RSpec.describe "rake activities:annual_fund_impact_metrics", type: :task do
-  let!(:completed_activity) { create(:programme_activity, programme_status: "completed") }
-  let!(:actual_2_years_ago) { create(:actual, date: 2.years.ago, parent_activity: completed_activity, report: nil) }
-  let!(:beis_organisation) { completed_activity.organisation }
-  let!(:completed_activity_no_actuals) { create(:programme_activity, programme_status: "completed") }
-  let!(:decided_activity) { create(:programme_activity, organisation: aardvark_organisation, programme_status: "decided") }
-  let!(:review_activity) { create(:programme_activity, organisation: aardvark_organisation, programme_status: "review") }
-  let!(:aardvark_organisation) { create(:beis_organisation, name: "Department for Aardvarks", iati_reference: "CZH-COH-111") }
-
-  let!(:delivery_activity) { create(:programme_activity, programme_status: "delivery") }
-  let!(:agreement_in_place_activity) { create(:programme_activity, programme_status: "agreement_in_place") }
-  let!(:open_for_applications_activity) { create(:programme_activity, programme_status: "open_for_applications") }
-  let!(:stopped_activity) { create(:programme_activity, programme_status: "stopped") }
-  let!(:planned_activity) { create(:programme_activity, programme_status: "planned") }
-
-  let!(:completed_over_2_years_ago_activity) { create(:programme_activity, programme_status: "completed") }
-  let!(:actual_over_2_years_ago) { create(:actual, date: 2.years.ago - 1.day, parent_activity: completed_over_2_years_ago_activity, report: nil) }
-
   let(:test_csv) { Tempfile.new(["fake", ".csv"]) }
 
   before { freeze_time }
@@ -40,51 +23,81 @@ RSpec.describe "rake activities:annual_fund_impact_metrics", type: :task do
   end
 
   it "excludes Activities with `delivery`, `agreement_in_place`, `open_for_applications`, `stopped`, or `planned` statuses" do
-    excluded_activity_titles = [
-      delivery_activity.title,
-      agreement_in_place_activity.title,
-      open_for_applications_activity.title,
-      stopped_activity.title,
-      planned_activity.title
+    completed_activity = create(:programme_activity, programme_status: "completed")
+    delivery_activity = create(:programme_activity, programme_status: "delivery")
+    agreement_in_place_activity = create(:programme_activity, programme_status: "agreement_in_place")
+    open_for_applications_activity = create(:programme_activity, programme_status: "open_for_applications")
+    stopped_activity = create(:programme_activity, programme_status: "stopped")
+    planned_activity = create(:programme_activity, programme_status: "planned")
+
+    excluded_activities = [
+      delivery_activity,
+      agreement_in_place_activity,
+      open_for_applications_activity,
+      stopped_activity,
+      planned_activity
     ]
+
+    (excluded_activities + [completed_activity]).each do |activity|
+      create(:actual, parent_activity: activity)
+    end
 
     task.invoke(test_csv.path)
     result = test_csv.readlines
 
-    excluded_activities_present = result.any? do |activity|
-      excluded_activity_titles.any? { |title| activity.include?(title) }
-    end
+    expect(result.second).to include(completed_activity.roda_identifier)
 
+    excluded_activities_present = result.any? do |activity|
+      excluded_activities.map(&:roda_identifier).any? { |id| activity.include?(id) }
+    end
     expect(excluded_activities_present).to be(false)
   end
 
-  it "excludes Activities with `completed` statuses if there are no Actuals reported in the last 2 years" do
-    excluded_activity_titles = [completed_over_2_years_ago_activity.title]
+  it "includes Activities with `completed` statuses if there are no Actuals reported in the last 2 years" do
+    completed_activity = create(:programme_activity, programme_status: "completed")
+    create(:actual, date: Date.today, parent_activity: completed_activity, report: nil)
+
+    completed_over_2_years_ago_activity = create(:programme_activity, programme_status: "completed")
+    create(:actual, date: 2.years.ago - 1.day, parent_activity: completed_over_2_years_ago_activity, report: nil)
 
     task.invoke(test_csv.path)
     result = test_csv.readlines
 
+    expect(result.second).to include(completed_activity.roda_identifier)
+
     excluded_activities_present = result.any? do |activity|
-      excluded_activity_titles.any? { |title| activity.include?(title) }
+      activity.include?(completed_over_2_years_ago_activity.roda_identifier)
     end
 
     expect(excluded_activities_present).to be(false)
   end
 
   it "excludes Activities with `completed` statuses if it has no Actuals" do
-    excluded_activity_titles = [completed_activity_no_actuals.title]
+    completed_activity = create(:programme_activity, programme_status: "completed")
+    create(:actual, date: Date.today, parent_activity: completed_activity, report: nil)
+
+    completed_activity_with_no_actual = create(:programme_activity, programme_status: "completed")
 
     task.invoke(test_csv.path)
     result = test_csv.readlines
 
+    expect(result.second).to include(completed_activity.roda_identifier)
+
     excluded_activities_present = result.any? do |activity|
-      excluded_activity_titles.any? { |title| activity.include?(title) }
+      activity.include?(completed_activity_with_no_actual.roda_identifier)
     end
 
     expect(excluded_activities_present).to be(false)
   end
 
   it "generates a CSV file of Activities ordered by organisation name and status" do
+    completed_activity = create(:programme_activity, programme_status: "completed")
+    create(:actual, date: Date.today, parent_activity: completed_activity, report: nil)
+    beis_organisation = completed_activity.organisation
+    aardvark_organisation = create(:beis_organisation, name: "Department for Aardvarks", iati_reference: "CZH-COH-111")
+    decided_activity = create(:programme_activity, organisation: aardvark_organisation, programme_status: "decided")
+    review_activity = create(:programme_activity, organisation: aardvark_organisation, programme_status: "review")
+
     task.invoke(test_csv.path)
     result = test_csv.readlines
 
