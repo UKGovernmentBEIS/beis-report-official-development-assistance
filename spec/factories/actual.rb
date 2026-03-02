64f85a629a31b617e6f7180431652d46346c34fa
Ed Davey <ed@dxw.com>
2026-03-02 13:23:05 +0000

    Ensure Actual has compatible Report and Activity

--- spec/factories/actual.rb
+++ spec/factories/actual.rb
@@ -18,7 +18,7 @@ FactoryBot.define do
     receiving_organisation_reference { "GB-COH-{#{Faker::Number.number(digits: 6)}}" }
     receiving_organisation_type { "70" }
 
-    association :parent_activity, factory: :project_activity
+    association :parent_activity, factory: :project_activity, is_oda: true
     association :report
 
     trait :with_comment do
